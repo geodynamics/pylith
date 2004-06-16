8170f055619edde905f784c7bfa941b7fd7beddf
Charles Williams <C.Williams@gns.cri.nz>
2004-06-16 23:11:23 +0000

    Driver routine to obtain elastic material matrix.  Material histories are used, and the appropriate routine name is passed to elas_mat_cmp. The number of allowed material models is presently set to 20, but this can easily be changed.  Eventually, this routine should be replaced by a python/C++ routine that can dynamically create and pass the appropriate routine name.

---
+++ lithomop3d/liblithomop3d/elas_mat_drv.f
@@ -0,0 +1,195 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2004  All Rights Reserved
+c
+c  Copyright 2004 Rensselaer Polytechnic Institute.
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine elas_mat_drv(
+     & dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,                   ! elemnt
+     & prop,mhist,infmat,numat,npropsz,                                 ! materl
+     & infetype,netypes,                                                ! eltype
+     & histry,nhist,nstep,lastep,                                       ! timdat
+     & idout,kto,kw,ierr)                                               ! ioinf
+c
+c...  driver subroutine to form the d-matrix for the elastic solution
+c
+      include "implicit.inc"
+c
+c...  subroutine arguments
+c
+      integer nstr,nddmat,ndmatsz,numelt,numat,npropsz,netypes
+      integer nhist,nstep,lastep,idout,kto,kw,ierr
+      integer infiel(6,numelt),iddmat(nstr,nstr),mhist(npropsz)
+      integer infmat(6,numat),infetype(4,netypes)
+      double precision dmat(nddmat,ndmatsz),prop(npropsz)
+      double precision histry(nhist,lastep+1)
+c
+c...  defined constants
+c
+      include "nconsts.inc"
+      include "rconsts.inc"
+c
+c...  external routines
+c
+      include "elas_mat_ext.inc"
+c
+c...  local variables
+c
+      integer imat,matmodel,nmatel,nprop,indprop,matgpt
+      double precision ptmp(100)
+      logical matchg
+c
+cdebug      write(6,*) "Hello from elas_mat_drv_f!"
+c
+      call fill(dmat,zero,nddmat*ndmatsz)
+      matgpt=1
+c
+c...  loop over material groups and then select appropriate material
+c     model routine
+c
+      do imat=1,numat
+        matmodel=infmat(1,imat)
+        nmatel=infmat(2,imat)
+        nprop=infmat(5,imat)
+        indprop=infmat(6,imat)
+        matchg=.false.
+        call mathist(ptmp,prop(indprop),mhist(indprop),histry,nprop,
+     &   imat,nstep,nhist,lastep,matchg,ierr)
+        if(ierr.ne.izero) return
+        if(matmodel.eq.1) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_1,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.2) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_2,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.3) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_3,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.4) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_4,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.5) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_5,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.6) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_6,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.7) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_7,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.8) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_8,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.9) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_9,                 ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.10) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_10,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.11) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_11,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.12) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_12,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.13) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_13,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.14) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_14,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.15) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_15,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.16) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_16,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.17) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_17,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.18) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_18,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.19) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_19,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        else if(matmodel.eq.20) then
+          call elas_mat_cmp(
+     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_20,                ! materl
+     &     infetype,netypes)                                            ! eltype
+        end if
+        matgpt=matgpt+nmatel
+      end do
+      return
+      end
+c
+c version
+c $Id: elas_mat_drv.f,v 1.1 2004/06/16 16:11:23 willic3 Exp $
+c
+c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
+c
+c End of file 
