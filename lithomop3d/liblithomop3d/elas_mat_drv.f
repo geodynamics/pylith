6597f9dcd446fa5b8da642bf0d47c4a2317920a0
Charles Williams <willic3>
2004-06-22 03:24:40 +0000

    Updated routine for new usage of parameter statements and for use of error codes and strings.

--- lithomop3d/liblithomop3d/elas_mat_drv.f
+++ lithomop3d/liblithomop3d/elas_mat_drv.f
@@ -30,29 +30,32 @@ c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c
       subroutine elas_mat_drv(
-     & dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,                   ! elemnt
+     & dmat,infiel,iddmat,ndmatsz,numelt,                               ! elemnt
      & prop,mhist,infmat,numat,npropsz,                                 ! materl
-     & infetype,netypes,                                                ! eltype
+     & infetype,                                                        ! eltype
      & histry,nhist,nstep,lastep,                                       ! timdat
-     & ierr)                                                            ! errcode
+     & ierr,errstrng)                                                   ! errcode
 c
 c...  driver subroutine to form the d-matrix for the elastic solution
 c
       include "implicit.inc"
 c
+c...  parameter definitions
+c
+      include "ndimens.inc"
+      include "nshape.inc"
+      include "nconsts.inc"
+      include "rconsts.inc"
+c
 c...  subroutine arguments
 c
-      integer nstr,nddmat,ndmatsz,numelt,numat,npropsz,netypes
+      integer ndmatsz,numelt,numat,npropsz
       integer nhist,nstep,lastep,ierr
       integer infiel(6,numelt),iddmat(nstr,nstr),mhist(npropsz)
       integer infmat(6,numat),infetype(4,netypes)
+      character errstrng*(*)
       double precision dmat(nddmat,ndmatsz),prop(npropsz)
       double precision histry(nhist,lastep+1)
-c
-c...  defined constants
-c
-      include "nconsts.inc"
-      include "rconsts.inc"
 c
 c...  external routines
 c
@@ -79,116 +82,140 @@ c
         indprop=infmat(6,imat)
         matchg=.false.
         call mathist(ptmp,prop(indprop),mhist(indprop),histry,nprop,
-     &   imat,nstep,nhist,lastep,matchg,ierr)
+     &   imat,nstep,nhist,lastep,matchg,ierr,errstrng)
         if(ierr.ne.izero) return
         if(matmodel.eq.1) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_1,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.2) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_2,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.3) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_3,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.4) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_4,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.5) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_5,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.6) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_6,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.7) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_7,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.8) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_8,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.9) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_9,                 ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.10) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_10,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.11) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_11,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.12) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_12,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.13) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_13,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.14) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_14,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.15) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_15,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.16) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_16,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.17) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_17,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.18) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_18,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.19) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_19,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
         else if(matmodel.eq.20) then
           call elas_mat_cmp(
-     &     dmat,infiel,iddmat,nstr,nddmat,ndmatsz,numelt,               ! elemnt
+     &     dmat,infiel,iddmat,ndmatsz,numelt,                           ! elemnt
      &     ptmp,infmat(1,imat),nprop,matgpt,elas_mat_20,                ! materl
-     &     infetype,netypes)                                            ! eltype
+     &     infetype,                                                    ! eltype
+     &     ierr,errstrng)                                               ! errcode
+        else
+          ierr=101
+          errstrng="elas_mat_drv"
         end if
+        if(ierr.ne.izero) return
         matgpt=matgpt+nmatel
       end do
       return
       end
 c
 c version
-c $Id: elas_mat_drv.f,v 1.2 2004/06/17 18:44:04 willic3 Exp $
+c $Id: elas_mat_drv.f,v 1.3 2004/06/21 20:24:40 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
