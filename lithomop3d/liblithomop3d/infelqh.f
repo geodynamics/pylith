763b5e65a495f780244f60a69f097fcc0cbe57e4
Charles Williams <C.Williams@gns.cri.nz>
2004-07-07 03:07:52 +0000

    Placeholder for quadratic infinite element shape function generation. This is not yet correct, as I haven't finished working out the equations for quadratic infinite elements.  It may actually be easier to generate the entire set of shape functions for each case, using a routine based on pquadhex.

---
+++ lithomop3d/liblithomop3d/infelqh.f
@@ -0,0 +1,76 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2004  All Rights Reserved
+c
+c  Copyright 2004 Rensselaer Polytechnic Institute.
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine infelqh(v,va,vv,dvv,io)
+c
+c...  subroutine to compute local shape functions and derivatives for
+c     infinite elements
+c
+      include "implicit.inc"
+c
+c...  parameter definitions
+c
+      include "nconsts.inc"
+      include "rconsts.inc"
+c
+c...  subroutine arguments
+c
+      integer io
+      double precision v,va,vv,dvv
+c
+c...  intrinsic functions
+c
+      intrinsic abs
+c
+c...  local variables
+c
+      double precision sgn,fac,fac1
+c
+c*      write(6,*) "Hello from infelqh_f!"
+c
+      vv=half*(one+va*v)
+      dvv=half*va
+      if(io.eq.izero) return
+      sgn=one
+      if(io.eq.1) sgn=-one
+      fac=one-sgn*v
+      fac1=half*abs(va+sgn)
+      vv=fac1+two*v*va/fac
+      dvv=two*va/(fac*fac)
+      return
+      end
+c
+c version
+c $Id: infelqh.f,v 1.1 2004/07/06 20:07:52 willic3 Exp $
+c
+c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
+c
+c End of file 
