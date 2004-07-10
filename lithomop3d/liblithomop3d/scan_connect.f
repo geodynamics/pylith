e8531a9f9b032888ac1ebdca34398acf6b4c64af
Charles Williams <willic3>
2004-07-10 00:01:13 +0000

    Completely revised routine for new indexing methods.  Routine now returns the total number of elements, the connectivity size, and completely sets the infmat (material info) array, using some information determined by the python material property input routines.

--- lithomop3d/liblithomop3d/scan_connect.f
+++ lithomop3d/liblithomop3d/scan_connect.f
@@ -29,54 +29,64 @@ c
 c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c
-      subroutine scan_connect(neni,numeli,netypesi,numelt,nconsize,kr,
-     & ierr,cfile)
+      subroutine scan_connect(neni,infmat,infmatmod,matmod,
+     & indprop,numat,numelt,nconsz,kr,cfile,ierr,errstrng)
 c
 c...  subroutine to perform an initial scan of the element connectivity
-c     file to determine the number of each type of element.
+c     file to determine the total number of elements and the number of
+c     elements of each material type.
 c
 c     Note that the variable netypesi (and the associated arrays) only
 c     defines the number of 'primitive' element types, and does not
 c     include the infinite element permutations.  This makes input much
 c     simpler, since there are then only 10 element types to define.
 c
-c     Exceptions should be raised for all errors except code 2, which is
-c     not applicable.
-c
 c     Error codes:
 c         0:  No error
 c         1:  Error opening input file
-c         2:  Units not specified (not applicable for this routine)
 c         3:  Read error
-c         4:  Illegal element type
+c       106:  Illegal element type
+c       101:  Attempt to use undefined material model
+c       107:  Illegal material type
 c
       include "implicit.inc"
 c
+c...  parameter definitions
+c
+      include "nshape.inc"
+      include "materials.inc"
+      include "nconsts.inc"
+c
 c...  subroutine arguments
 c
-      integer netypesi,numelt,nconsize,kr,ierr
-      integer neni(netypesi),numeli(netypesi)
-      character cfile*(*)
-c
-c...  defined constants
-c
-      include "nconsts.inc"
+      integer numat,numelt,nconsz,kr,ierr
+      integer neni(netypesi),infmat(3,numat),infmatmod(5,nmatmodmax)
+      integer matmod(numat),indprop(numat)
+      character cfile*(*),errstrng*(*)
 c
 c...  local variables
 c
-      integer j,n,ietypei,mat,infin
+      integer i,j,n,ietypei,imat,infin,matm
       integer ien(20)
 c
 c...  open input file
 c
       ierr=izero
       numelt=izero
-      nconsize=izero
-      call ifill(numeli,izero,netypesi)
+      nconsz=izero
+      call ifill(infmat,izero,ithree*numat)
       open(kr,file=cfile,status="old",err=20)
 c
+c...  set up part of the infmat array that has already been determined
+c     by python material property input routine.
+c
+      do i=1,numat
+        infmat(1,i)=matmod(i)
+        infmat(3,i)=indprop(i)
+      end do
+c
 c... scan the file, counting the number of entries for each type of
-c    element
+c    material.
 c    Note:  Due to speed considerations, we are not allowing the option
 c    of putting comments within the list.  To do this, we
 c    would need to scan each line twice to determine whether it was a
@@ -84,22 +94,38 @@ c    comment.
 c
       call pskip(kr)
  40   continue
-        read(kr,*,end=10,err=30) n,ietypei,mat,infin,
+        read(kr,*,end=10,err=30) n,ietypei,imat,infin,
      &   (ien(j),j=1,neni(ietypei))
+c
+        matm=infmat(1,imat)
+c
         if(ietypei.le.izero.or.ietypei.gt.netypesi) then
-          ierr=4
+          ierr=106
+          errstrng="scan_connect"
           return
         end if
-        numeli(ietypei)=numeli(ietypei)+1
+c
+        if(imat.le.izero.or.imat.gt.numat) then
+          ierr=107
+          errstrng="scan_connect"
+          return
+        end if
+c
+        if(matm.gt.nmatmodmax.or.infmatmod(1,matm).eq.izero) then
+          ierr=101
+          errstrng="scan_connect"
+          return
+        end if
+c
+        numelt=numelt+ione
+        nconsz=nconsz+neni(ietypei)
+        infmat(2,imat)=infmat(2,imat)+ione
+c
         go to 40
 c
 c...  normal return
 c
  10   continue
-        do j=1,netypesi
-          numelt=numelt+numeli(j)
-          nconsize=nconsize+neni(j)*numeli(j)
-        end do
         close(kr)
         return
 c
@@ -120,7 +146,7 @@ c
       end
 c
 c version
-c $Id: scan_connect.f,v 1.1 2004/04/14 21:18:30 willic3 Exp $
+c $Id: scan_connect.f,v 1.2 2004/07/09 17:01:13 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
