d1754a56591719baf143cf3ee1aa6d51dc5f5356
Charles Williams <willic3>
2004-07-15 04:17:37 +0000

    Initial versions of files needed to define material models for pyre.

---
+++ lithomop3d/lithomop3d/MaterialModel/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# 
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2003 All Rights Reserved
+# 
+#  <LicenseText>
+# 
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+
+# Modules to include.  This should probably include everything in this
+# directory.
+
+__all__ = ["MaterialModel",
+	   "IsotropicLinearElastic",
+	   "IsotropicLinearMaxwellViscoelastic"]
+
+def copyright():
+    return "testpy pyre module: Copyright (c) 1998-2003 California Institute of Technology";
+
+
+# version
+__id__ = "$Id: __init__.py,v 1.1 2004/07/14 21:17:37 willic3 Exp $"
+
+#  End of file 
