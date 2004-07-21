51d0d28694f9f27d759986d1c671069bddb38a0c
Charles Williams <C.Williams@gns.cri.nz>
2004-07-21 03:49:11 +0000

    Updated Make.mm from top-level pyre directory.

--- lithomop3d/lithomop3d/Make.mm
+++ lithomop3d/lithomop3d/Make.mm
@@ -12,6 +12,15 @@
 #
 
 PROJECT = lithomop3d
+PACKAGE = lithomop3d
+
+PROJ_TMPDIR = $(BLD_TMPDIR)/$(PROJECT)/$(PACKAGE)
+
+BLD_DIRS = \
+	MaterialModel
+
+RECURSE_DIRS = $(BLD_DIRS)
+
 
 #--------------------------------------------------------------------------
 #
@@ -30,17 +39,20 @@ update: clean
 
 EXPORT_PYTHON_MODULES = \
 	Application.py \
+	KeywordValueParse.py \
 	Lithomop3d_run.py \
 	Lithomop3d_scan.py \
 	Lithomop3d_setup.py \
+	Materials.py \
 	__init__.py
 
 
 export:: export-python-modules
+	BLD_ACTION="export" $(MM) recurse
 
 
 # version
-# $Id: Make.mm,v 1.1 2004/04/14 21:22:47 willic3 Exp $
+# $Id: Make.mm,v 1.2 2004/07/20 20:49:11 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
