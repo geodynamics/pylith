3494dc8ba13012de6508ac5e64e062362e39f3f3
Charles Williams <willic3>
2004-08-12 09:19:36 +0000

    Checking in this version of pquadtet, although there still appear to be some problems with quadratic tets. Reordered local coordinates to be consistent with right-handed tet. Reordered midside node shape functions to be consistent with web resource. Each function and its local coordinate derivatives are defined separately rather than within a loop.

--- lithomop3d/liblithomop3d/pquadtet.f
+++ lithomop3d/liblithomop3d/pquadtet.f
@@ -81,6 +81,7 @@ c
 c...  definitions
 c
       tetvol=sixth
+cdebug      tetvol=one
       nshsize=(nsd+1)*nenmax*ngaussmax
       ngssize=(nsd+1)*ngaussmax
 c
@@ -95,6 +96,9 @@ c
         gauss(2,1)=fourth
         gauss(3,1)=fourth
         gauss(4,1)=tetvol
+cdebug        gauss(1,1)=fourth-0.01d0
+cdebug        gauss(2,1)=fourth-0.01d0
+cdebug        gauss(3,1)=fourth-0.01d0
       else
         ngauss=ifour
         g1=(five-sqrt(five))/20.0d0
@@ -103,7 +107,7 @@ c
           gauss(1,l)=g1
           gauss(2,l)=g1
           gauss(3,l)=g1
-          if(l.ne.ngauss) gauss(l,l)=g2
+          if(l.ne.ione) gauss(l-ione,l)=g2
           gauss(4,l)=fourth*tetvol
         end do
       end if
@@ -117,68 +121,52 @@ c
         rr=gauss(1,l)
         ss=gauss(2,l)
         tt=gauss(3,l)
-        uu=g1
-        if(l.eq.4) uu=g2
-        if(intord.eq.itwo) uu=fourth
+c*        uu=g1
+c*        if(l.eq.ione) uu=g2
+c*        if(intord.eq.itwo) uu=fourth
+        uu=one-rr-ss-tt
 cdebug        write(6,*) "l:",l
 cdebug        write(6,*) "rr,ss,tt,uu:",rr,ss,tt,uu
-        sh(4,1,l)=rr*(two*rr-one)
-        sh(1,1,l)=four*rr-one
-        sh(2,1,l)=zero
-        sh(3,1,l)=zero
-        sh(4,2,l)=ss*(two*ss-one)
-        sh(1,2,l)=zero
-        sh(2,2,l)=four*ss-one
+        sh(4,1,l)=uu*(two*uu-one)
+        sh(1,1,l)=one-four*uu
+        sh(2,1,l)=one-four*uu
+        sh(3,1,l)=one-four*uu
+        sh(4,2,l)=rr*(two*rr-one)
+        sh(1,2,l)=four*rr-one
+        sh(2,2,l)=zero
         sh(3,2,l)=zero
-        sh(4,3,l)=tt*(two*tt-one)
+        sh(4,3,l)=ss*(two*ss-one)
         sh(1,3,l)=zero
-        sh(2,3,l)=zero
-        sh(3,3,l)=four*tt-one
-        sh(4,4,l)=uu*(two*uu-one)
-        sh(1,4,l)=-(four*uu-one)
-        sh(2,4,l)=-(four*uu-one)
-        sh(3,4,l)=-(four*uu-one)
-        sh(4,5,l)=four*rr*ss
-        sh(1,5,l)=four*ss
-        sh(2,5,l)=four*rr
-        sh(3,5,l)=zero
-        sh(4,6,l)=four*ss*tt
-        sh(1,6,l)=zero
-        sh(2,6,l)=four*tt
-        sh(3,6,l)=four*ss
-        sh(4,7,l)=four*rr*tt
-        sh(1,7,l)=four*tt
-        sh(2,7,l)=zero
-        sh(3,7,l)=four*rr
-        sh(4,8,l)=four*rr*uu
-        sh(1,8,l)=four*(uu-rr)
-        sh(2,8,l)=-(four*rr)
-        sh(3,8,l)=-(four*rr)
-        sh(4,9,l)=four*ss*uu
-        sh(1,9,l)=-(four*ss)
-        sh(2,9,l)=four*(uu-ss)
-        sh(3,9,l)=-(four*ss)
-        sh(4,10,l)=four*tt*uu
-        sh(1,10,l)=-(four*tt)
-        sh(2,10,l)=-(four*tt)
-        sh(3,10,l)=four*(uu-tt)
-c*          if(i.le.4) then
-c*            sh(4,i,l)=rr*(two*rr-one)+ss*(two*ss-one)+tt*(two*tt-one)+
-c*     &       uu*(two*uu-one)
-c*            sh(1,i,l)=r(i)*(four*rr-one)-u(i)*(four*uu-one)
-c*            sh(2,i,l)=s(i)*(four*ss-one)-u(i)*(four*uu-one)
-c*            sh(3,i,l)=t(i)*(four*tt-one)-u(i)*(four*uu-one)
-c*          else
-c*            sh(4,i,l)=max(rr,one)*max(ss,one)*max(tt,one)*max(uu,one)
-c*            drr=eight*r(i)*max(ss,one)*max(tt,one)*max(uu,one)
-c*            dss=eight*s(i)*max(rr,one)*max(tt,one)*max(uu,one)
-c*            dtt=eight*t(i)*max(rr,one)*max(ss,one)*max(uu,one)
-c*            duu=(-eight)*u(i)*max(rr,one)*max(ss,one)*max(tt,one)
-c*            sh(1,i,l)=drr-duu
-c*            sh(2,i,l)=dss-duu
-c*            sh(3,i,l)=dtt-duu
-c*          end if
-c*        end do
+        sh(2,3,l)=four*ss-one
+        sh(3,3,l)=zero
+        sh(4,4,l)=tt*(two*tt-one)
+        sh(1,4,l)=zero
+        sh(2,4,l)=zero
+        sh(3,4,l)=four*tt-one
+        sh(4,5,l)=four*rr*uu
+        sh(1,5,l)=four*(uu-rr)
+        sh(2,5,l)=-(four*rr)
+        sh(3,5,l)=-(four*rr)
+        sh(4,6,l)=four*rr*ss
+        sh(1,6,l)=four*ss
+        sh(2,6,l)=four*rr
+        sh(3,6,l)=zero
+        sh(4,7,l)=four*ss*uu
+        sh(1,7,l)=-(four*ss)
+        sh(2,7,l)=four*(uu-ss)
+        sh(3,7,l)=-(four*ss)
+        sh(4,8,l)=four*tt*uu
+        sh(1,8,l)=-(four*tt)
+        sh(2,8,l)=-(four*tt)
+        sh(3,8,l)=four*(uu-tt)
+        sh(4,9,l)=four*rr*tt
+        sh(1,9,l)=four*tt
+        sh(2,9,l)=zero
+        sh(3,9,l)=four*rr
+        sh(4,10,l)=four*ss*tt
+        sh(1,10,l)=zero
+        sh(2,10,l)=four*tt
+        sh(3,10,l)=four*ss
 cdebug        dbsum=zero
 cdebug        do idb=1,nen
 cdebug          write(6,*) "sh:",(sh(jdb,idb,l),jdb=1,4)
@@ -193,7 +181,7 @@ c
       end
 c
 c version
-c $Id: pquadtet.f,v 1.3 2004/08/02 21:21:54 willic3 Exp $
+c $Id: pquadtet.f,v 1.4 2004/08/12 02:19:36 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
