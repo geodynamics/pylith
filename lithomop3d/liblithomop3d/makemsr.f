aa0ee8a09ee6f18d522951a6d29760122562b015
Charles Williams <C.Williams@gns.cri.nz>
2004-08-12 08:54:17 +0000

    Fixed incorrect computation of the minimum nonzero terms per row. Routine now contains commented-out debugging statements.

--- lithomop3d/liblithomop3d/makemsr.f
+++ lithomop3d/liblithomop3d/makemsr.f
@@ -31,6 +31,7 @@ c
 c
       subroutine makemsr(ja,indx,link,nbrs,neq,nnz,iwork,nmin,nmax,
      & wavg)
+cdebug      subroutine makemsr(ja,indx,link,nbrs,neq,nnz,iwork)
 c
 c      program to transform linked list into modified sparse row format
 c
@@ -43,6 +44,7 @@ c
 c...  subroutine arguments
 c
       integer neq,nnz,iwork,nmin,nmax
+cdebug      integer neq,nnz,iwork
       integer ja(nnz),indx(neq),link(iwork),nbrs(iwork)
       double precision wavg
 c
@@ -53,12 +55,20 @@ c
 c...  local variables
 c
       integer i,loc,ncol
+cdebug      integer idb
+cdebug      integer nmin,nmax
+cdebug      double precision wavg
 c
+cdebug      write(6,*) "Hello from makemsr_f!"
+c
+cdebug      write(6,*) "neq,nnz,iwork,nmin,nmax,wavg:",
+cdebug     & neq,nnz,iwork,nmin,nmax,wavg
       call ifill(ja,izero,nnz)
-      ja(1)=neq+2
-      nmin=ione
-      nmax=ione
-      wavg=neq
+cdebug      if(neq.ne.izero) stop
+      ja(1)=neq+itwo
+      nmin=10000
+      nmax=izero
+      wavg=dble(neq)
       do i=1,neq
         loc=indx(i)
         ncol=izero
@@ -76,14 +86,22 @@ c...    sort entries in each row
 c
         call isort(ja(i+1)-ja(i),ja(ja(i)))
       end do
-      nmin=nmin+1
-      nmax=nmax+1
+cdebug      write(6,*) "After end of loop!"
+cdebug      call flush(6)
+cdebug      if(neq.ne.izero) stop
+      nmin=nmin+ione
+      nmax=nmax+ione
       if(neq.ne.izero) wavg=wavg/dble(neq)
+cdebug      write(6,*) "nmin,nmax,wavg,neq:",nmin,nmax,wavg,neq
+cdebug      call flush(6)
+cdebug      write(6,*) "ja:",(ja(idb),idb=1,nnz)
+cdebug      call flush(6)
+cdebug      if(neq.ne.izero) stop
       return
       end
 c
 c version
-c $Id: makemsr.f,v 1.2 2004/07/07 18:38:30 willic3 Exp $
+c $Id: makemsr.f,v 1.3 2004/08/12 01:54:17 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
