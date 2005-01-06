5dfa63ce7e2dafb576db615152a3033cb10fe803
Charles Williams <C.Williams@gns.cri.nz>
2005-01-06 09:22:38 +0000

    Moved obsolete routines to 'unused'.

---
+++ lithomop3d/liblithomop3d/unused/bdiff.f
@@ -0,0 +1,66 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2004  All Rights Reserved
+c
+c  Copyright 2004 Rensselaer Polytechnic Institute.
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine bdiff(b,btot,bres,neq)
+c
+c...subroutine to subtract stress-equivalent nodal loads from total
+c   applied loads
+c
+      include "implicit.inc"
+c
+c...  parameter definitions
+c
+      include "nconsts.inc"
+      double precision alpha
+      parameter(alpha=-1.0d0)
+c
+c...  subroutine arguments
+c
+      integer neq
+      double precision b(neq),btot(neq),bres(neq)
+cdebug      integer idb
+c
+cdebug      write(6,*) "Hello from bdiff_f!"
+c
+      call dcopy(neq,btot,ione,bres,ione)
+      call daxpy(neq,alpha,b,ione,bres,ione)
+cdebug      do idb=1,neq
+cdebug        write(6,*) b(idb),btot(idb),bres(idb)
+cdebug      end do
+      return
+      end
+c
+c version
+c $Id: bdiff.f,v 1.1 2005/01/06 01:22:38 willic3 Exp $
+c
+c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
+c
+c End of file 
