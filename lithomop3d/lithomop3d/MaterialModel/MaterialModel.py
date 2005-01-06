a987ef2001301b07d10de4a8d45aa424ea551f02
Charles Williams <willic3>
2005-01-06 10:04:21 +0000

    Updated routine for new parsing method.

--- lithomop3d/lithomop3d/MaterialModel/MaterialModel.py
+++ lithomop3d/lithomop3d/MaterialModel/MaterialModel.py
@@ -2,11 +2,29 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-#                             Michael A.G. Aivazis
-#                      California Institute of Technology
-#                      (C) 1998-2003  All Rights Reserved
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#                        (C) 2004  All Rights Reserved
 #
-# <LicenseText>
+#  Copyright 2004 Rensselaer Polytechnic Institute.
+#  All worldwide rights reserved.  A license to use, copy, modify and
+#  distribute this software for non-commercial research purposes only
+#  is hereby granted, provided that this copyright notice and
+#  accompanying disclaimer is not modified or removed from the software.
+#
+#  DISCLAIMER:  The software is distributed "AS IS" without any express
+#  or implied warranty, including but not limited to, any implied
+#  warranties of merchantability or fitness for a particular purpose
+#  or any warranty of non-infringement of any current or pending patent
+#  rights.  The authors of the software make no representations about
+#  the suitability of this software for any particular purpose.  The
+#  entire risk as to the quality and performance of the software is with
+#  the user.  Should the software prove defective, the user assumes the
+#  cost of all necessary servicing, repair or correction.  In
+#  particular, neither Rensselaer Polytechnic Institute, nor the authors
+#  of the software are liable for any indirect, special, consequential,
+#  or incidental damages related to the software, to the maximum extent
+#  the law permits.
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
@@ -28,7 +46,7 @@ class MaterialModel:
             line = file.readline()
             if not line: break
             keyvals = lineparse.parseline(line)
-            if keyvals[2]:
+            if keyvals[3]:
                 exec keyvals[0] + '=' + `keyvals[1]`
 
         for propertyIndex in range(len(self.propertyPosition)):
@@ -51,6 +69,6 @@ class MaterialModel:
         return
 
 # version
-# $Id: MaterialModel.py,v 1.3 2004/08/12 17:11:17 willic3 Exp $
+# $Id: MaterialModel.py,v 1.4 2005/01/06 02:04:21 willic3 Exp $
 
 # End of file 
