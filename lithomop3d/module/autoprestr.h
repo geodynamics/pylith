c8da60a4bf2b8edc7d303f1e3781efe7f695358c
Charles Williams <willic3>
2005-01-19 04:09:13 +0000

    Bindings for autoprestr routine.

---
+++ lithomop3d/module/autoprestr.h
@@ -0,0 +1,46 @@
+// -*- C++ -*-
+// 
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+//                               Charles A. Williams
+//                        Rensselaer Polytechnic Institute
+//                        (C) 2004 All Rights Reserved
+// 
+//  Copyright 2004 Rensselaer Polytechnic Institute.
+//  All worldwide rights reserved.  A license to use, copy, modify and
+//  distribute this software for non-commercial research purposes only
+//  is hereby granted, provided that this copyright notice and
+//  accompanying disclaimer is not modified or removed from the software.
+//
+//  DISCLAIMER:  The software is distributed "AS IS" without any express
+//  or implied warranty, including but not limited to, any implied
+//  warranties of merchantability or fitness for a particular purpose
+//  or any warranty of non-infringement of any current or pending patent
+//  rights.  The authors of the software make no representations about
+//  the suitability of this software for any particular purpose.  The
+//  entire risk as to the quality and performance of the software is with
+//  the user.  Should the software prove defective, the user assumes the
+//  cost of all necessary servicing, repair or correction.  In
+//  particular, neither Rensselaer Polytechnic Institute, nor the authors
+//  of the software are liable for any indirect, special, consequential,
+//  or incidental damages related to the software, to the maximum extent
+//  the law permits.
+// 
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+
+#if !defined(pylithomop3d_autoprestr_h)
+#define pylithomop3d_autoprestr_h
+
+// Compute gravitational prestresses.
+extern char pylithomop3d_autoprestr__name__[];
+extern char pylithomop3d_autoprestr__doc__[];
+extern "C"
+PyObject * pylithomop3d_autoprestr(PyObject *, PyObject *);
+
+#endif
+
+// version
+// $Id: autoprestr.h,v 1.1 2005/01/18 20:09:13 willic3 Exp $
+
+// End of file
