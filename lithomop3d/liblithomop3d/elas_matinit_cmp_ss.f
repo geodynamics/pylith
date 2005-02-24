952381bd2aff7685308a24915d2344dd70018d6e
Charles Williams <C.Williams@gns.cri.nz>
2005-02-24 07:53:04 +0000

    Eliminated bprestress array and now use state0 array for initial stresses.

--- lithomop3d/liblithomop3d/elas_matinit_cmp_ss.f
+++ lithomop3d/liblithomop3d/elas_matinit_cmp_ss.f
@@ -35,8 +35,9 @@ c
      & dx,numslp,numsn,                                                 ! slip
      & tfault,numfn,                                                    ! fault
      & s,stemp,                                                         ! stiff
-     & state,dstate,dmat,ien,lm,lmx,lmf,infiel,iddmat,nstatesz,         ! elemnt
-     & ndmatsz,numelt,nconsz,                                           ! elemnt
+     & state,dstate,state0,dmat,ien,lm,lmx,lmf,infiel,iddmat,nstatesz,  ! elemnt
+     & nstatesz0,ndmatsz,numelt,nconsz,nprestrflag,ipstrs,ipauto,       ! elemnt
+     & nstate0,                                                         ! elemnt
      & prop,nstate,nprop,matgpt,imatvar,nmatel,elas_matinit,td_matinit, ! materl
      & prestr_matinit,matchg,tminmax,                                   ! materl
      & gauss,sh,shj,infetype,                                           ! eltype
@@ -58,17 +59,19 @@ c
 c
 c...  subroutine arguments
 c
-      integer nnz,neq,numnp,numslp,numsn,numfn,nstatesz,ndmatsz,numelt
-      integer nconsz,nprop,nstate,matgpt,imatvar,nmatel,numrot,ierr
+      integer nnz,neq,numnp,numslp,numsn,numfn,nstatesz,nstatesz0
+      integer ndmatsz,numelt,nconsz,nprestrflag,ipstrs,ipauto,nprop
+      integer nstate0,nstate,matgpt,imatvar,nmatel,numrot,ierr
       logical matchg
       integer ja(nnz),ien(nconsz),lm(ndof,nconsz),lmx(ndof,nconsz)
-      integer lmf(nconsz),infiel(6,numelt),iddmat(nstr,nstr)
+      integer lmf(nconsz),infiel(7,numelt),iddmat(nstr,nstr)
       integer infetype(4,netypes)
       character errstrng*(*)
       double precision alnz(nnz),x(nsd,numnp),d(ndof,numnp)
       double precision dx(ndof,numnp),tfault(ndof,numfn)
       double precision s(neemax*neemax),stemp(neemax*neemax)
       double precision state(nstr,nstatesz),dstate(nstr,nstatesz)
+      double precision state0(nstate0,nstatesz0)
       double precision dmat(nddmat,ndmatsz),prop(nprop),tminmax
       double precision gauss(nsd+1,ngaussmax,netypes)
       double precision sh(nsd+1,nenmax,ngaussmax,netypes)
@@ -176,7 +179,7 @@ c
       end
 c
 c version
-c $Id: elas_matinit_cmp_ss.f,v 1.6 2005/01/26 15:35:08 willic3 Exp $
+c $Id: elas_matinit_cmp_ss.f,v 1.7 2005/02/23 23:53:04 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
