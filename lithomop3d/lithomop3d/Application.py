8e7f2b0483c5f7445518ece3a2c07debfddd1a09
Charles Williams <willic3>
2005-03-11 10:15:07 +0000

    Updated for pythia-0.8, and commented out petsc-specific inventory.

--- lithomop3d/lithomop3d/Application.py
+++ lithomop3d/lithomop3d/Application.py
@@ -30,13 +30,13 @@
 #
 
 
-from pyre.applications.Application import Application as BaseApplication
+from pyre.applications.Script import Script as BaseScript
 
 
-class Application(BaseApplication):
+class Application(BaseScript):
 
 
-    def run(self, *args, **kwds):
+    def main(self, *args, **kwds):
 #        from time import clock as now
 #        start = now()
         lm3dsetup = self.inventory.setup
@@ -52,32 +52,30 @@ class Application(BaseApplication):
 
 
     def __init__(self, name="lithomop3d"):
-        BaseApplication.__init__(self, name)
+        BaseScript.__init__(self, name)
         return
 
 
-    class Inventory(BaseApplication.Inventory):
+    class Inventory(BaseScript.Inventory):
 
-        import pyre.facilities
+        import pyre.inventory
         from Lithomop3d_scan import Lithomop3d_scan
         from Lithomop3d_setup import Lithomop3d_setup
         from Lithomop3d_run import Lithomop3d_run
 
-        inventory = [
-            pyre.facilities.facility("scanner", default=Lithomop3d_scan()),
-            pyre.facilities.facility("setup", default=Lithomop3d_setup()),
-            pyre.facilities.facility("solver", default=Lithomop3d_run()),
-            pyre.properties.str("ksp_monitor",default="1"),
-            pyre.properties.str("ksp_view",default="1"),
-            pyre.properties.str("log_summary",default="1"),
-            pyre.properties.str("log_info",default="0"),
-            pyre.properties.str("pc_type",default="jacobi"),
-            pyre.properties.str("start_in_debugger",default="0"),
-            pyre.properties.str("petsc_solver",default="0"),
-            ]
+        scanner = pyre.inventory.facility("scanner", factory=Lithomop3d_scan)
+        setup = pyre.inventory.facility("setup", factory=Lithomop3d_setup)
+        solver = pyre.inventory.facility("solver", factory=Lithomop3d_run)
+        # ksp_monitor = pyre.inventory.str("ksp_monitor",default="1")
+        # ksp_view = pyre.inventory.str("ksp_view",default="1")
+        # log_summary = pyre.inventory.str("log_summary",default="1")
+        # log_info = pyre.inventory.str("log_info",default="0")
+        # pc_type = pyre.inventory.str("pc_type",default="1")
+        # start_in_debugger = pyre.inventory.str("start_in_debugger",default="0")
+        # petsc_solver = pyre.inventory.str("petsc_solver",default="1")
 
 
 # version
-# $Id: Application.py,v 1.3 2005/03/10 01:10:37 knepley Exp $
+# $Id: Application.py,v 1.4 2005/03/11 02:15:07 willic3 Exp $
 
 # End of file 
