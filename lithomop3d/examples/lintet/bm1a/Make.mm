afc2b4fb1d35ea63927e82b4385ad08519e9ead9
Charles Williams <C.Williams@gns.cri.nz>
2005-03-11 10:51:41 +0000

    Added -typos=relaxed so command-line arguments do not need to be in inventory.

--- lithomop3d/examples/lintet/bm1a/Make.mm
+++ lithomop3d/examples/lintet/bm1a/Make.mm
@@ -12,14 +12,14 @@
 #
 
 test: clean
-	lithomop3dapp.py -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view
+	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view
 
 clean::
 	$(RM_F) bm1a.ascii bm1a.plot bm1a*.inp
 
 
 # version
-# $Id: Make.mm,v 1.3 2005/03/08 02:14:25 knepley Exp $
+# $Id: Make.mm,v 1.4 2005/03/11 02:51:41 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
