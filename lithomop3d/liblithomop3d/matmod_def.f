13265d34cde69d25a7f2bbf048ea4718d2967791
Charles Williams <willic3>
2005-03-22 07:19:11 +0000

    Updated routine to reflect revised method of tracking state variables.

--- lithomop3d/liblithomop3d/matmod_def.f
+++ lithomop3d/liblithomop3d/matmod_def.f
@@ -36,16 +36,17 @@ c     material models.
 c
 c     At present, the maximum number of material models is 20, but this
 c     can easily be changed by editing materials.inc.  This routine
-c     defines 5 parameters for each material model:
+c     defines 6 parameters for each material model:
 c
 c         infmatmod(1,matmod) = definition flag
 c                               0 = material is not defined
 c                               1 = material is defined
 c         infmatmod(2,matmod) = number of state variables.  If a
 c                               material is defined, it will have at
-c                               least 2 state variables (stress and
-c                               strain), up to a maximum of 4, if
-c                               viscous or plastic strain are required.
+c                               least 12 state variables (stress and
+c                               strain).  At present the maximum number
+c                               of state variables is 24, if both
+c                               viscous and plastic strain are required.
 c         infmatmod(3,matmod) = number of material properties needed to
 c                               define the material.  The minimum value
 c                               should be 3 for an isotropic linear
@@ -65,6 +66,12 @@ c         infmatmod(5,matmod) = material property behavior flag.
 c                               0 = time response of material is linear.
 c                               1 = time response of material is
 c                                   nonlinear.
+c         infmatmod(6,matmod) = number of initial state variables.  At
+c                               present, this should always be 6, since
+c                               initial state variables are always
+c                               assumed to be stresses, but this may
+c                               change in the future.
+c                 
 c
 c     In addition, this routine generates a simple integer (binary)
 c     matrix (ismatmod) indicating whether a state variable is used for
@@ -79,7 +86,7 @@ c
 c
 c...  subroutine arguments
 c
-      integer infmatmod(5,nmatmodmax),ismatmod(nstatesmax,nmatmodmax)
+      integer infmatmod(6,nmatmodmax),ismatmod(nstatesmax,nmatmodmax)
 c
 c...  local variables
 c
@@ -87,44 +94,49 @@ c
 c
 cdebug      write(6,*) "Hello from matmod_def_f!"
 c
-      call ifill(infmatmod,izero,5*nmatmodmax)
+      call ifill(infmatmod,izero,6*nmatmodmax)
       call ifill(ismatmod,izero,nstatesmax*nmatmodmax)
 c
 c...  Definitions for isotropic elastic material
 c
       infmatmod(1,1) =ione
-      infmatmod(2,1) =itwo
+      infmatmod(2,1) =12
       infmatmod(3,1) =ithree
-      ismatmod(1,1) =ione
-      ismatmod(2,1) =ione
+      infmatmod(6,1) =6
+      do i=1,infmatmod(2,1)
+        ismatmod(i,1) =ione
+      end do
 c
 c...  Dummy definitions for additional elastic material models
 c
-      infmatmod(2,2)=itwo
-      infmatmod(2,3)=itwo
-      infmatmod(2,4)=itwo
-      ismatmod(1,2) =ione
-      ismatmod(2,2) =ione
-      ismatmod(1,3) =ione
-      ismatmod(2,3) =ione
-      ismatmod(1,4) =ione
-      ismatmod(2,4) =ione
+      infmatmod(2,2)=12
+      infmatmod(2,3)=12
+      infmatmod(2,4)=12
+      infmatmod(6,2) =6
+      infmatmod(6,3) =6
+      infmatmod(6,4) =6
+      do i=1,infmatmod(2,2)
+        ismatmod(i,2) =ione
+        ismatmod(i,3) =ione
+        ismatmod(i,4) =ione
+      end do
 c
 c...  Definition for Maxwell viscoelastic material
 c
       infmatmod(1,5) = ione
-      infmatmod(2,5) = ithree
+      infmatmod(2,5) = 18
       infmatmod(3,5) = ifour
       infmatmod(4,5) = ione
       infmatmod(5,5) = izero
-      ismatmod(1,5) =ione
-      ismatmod(2,5) =ione
-      ismatmod(3,5) =ione
+      infmatmod(6,5) = 6
+      do i=1,infmatmod(2,5)
+        ismatmod(i,1) =ione
+      end do
 c
 c...  Dummy definitions for remaining materials
 c
       do i=6,nmatmodmax
-        do j=2,5
+        do j=2,6
           infmatmod(j,i)=infmatmod(j,5)
         end do
         do j=1,nstatesmax
@@ -136,7 +148,7 @@ c
       end
 c
 c version
-c $Id: matmod_def.f,v 1.3 2004/08/12 02:08:44 willic3 Exp $
+c $Id: matmod_def.f,v 1.4 2005/03/21 23:19:11 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
