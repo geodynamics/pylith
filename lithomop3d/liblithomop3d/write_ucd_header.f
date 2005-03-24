ebd70dbf9e8e130982517ffd54bda884e163d364
Charles Williams <C.Williams@gns.cri.nz>
2005-03-24 02:30:02 +0000

    Simple routine to write the UCD header for state variable output.

---
+++ lithomop3d/liblithomop3d/write_ucd_header.f
@@ -0,0 +1,73 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2005  All Rights Reserved
+c
+c  Copyright 2005 Rensselaer Polytechnic Institute.
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine write_ucd_header(istatoutc,nstatestot,kucd)
+c
+c...subroutine to write the header for a UCD file containing state
+c   variables
+c
+      include "implicit.inc"
+c
+c...  parameter definitions
+c
+      include "materials.inc"
+      include "nconsts.inc"
+c
+c...  subroutine arguments
+c
+      integer nstatestot,kucd
+      integer istatoutc(nstatestot)
+c
+c... included dimension and type statements
+c
+      include "labels_dim.inc"
+c
+c...  local variables
+c
+      integer i
+      integer iout(3*nstatesmax)
+c
+c...  included variable definitions
+c
+      include "labels_def.inc"
+c
+      do i=1,3*nstatesmax
+        iout(i)=ione
+      end do
+c
+      write(kucd,"(100i5)") nstatestot,(iout(i),i=1,nstatestot)
+c
+      do i=1,nstatestot
+        write(kucd,"(a11)") labels(istatoutc(i))
+      end do
+c
+      return
+      end
