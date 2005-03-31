273140f9348f393fc88fc55b2982e31d12a199a9
Charles Williams <C.Williams@gns.cri.nz>
2005-03-31 03:43:51 +0000

    This routine presently only outputs the flag determining the initial guess for the linear solution.  All other additional information that was output previously is now handled by PETSc.

--- lithomop3d/liblithomop3d/write_subiter.f
+++ lithomop3d/liblithomop3d/write_subiter.f
@@ -4,9 +4,8 @@ c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c                             Charles A. Williams
 c                       Rensselaer Polytechnic Institute
-c                        (C) 2004  All Rights Reserved
+c                        (C) 2005  All Rights Reserved
 c
-c  Copyright 2004 Rensselaer Polytechnic Institute.
 c  All worldwide rights reserved.  A license to use, copy, modify and
 c  distribute this software for non-commercial research purposes only
 c  is hereby granted, provided that this copyright notice and
@@ -29,7 +28,7 @@ c
 c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c
-      subroutine write_subiter(rmult,rmin,nsol,maxcg,kw,idout,ofile)
+      subroutine write_subiter(nprevdflag,kw,idout,ofile)
 c
 c...subroutine to write parameters controlling the subiteration
 c   process
@@ -38,18 +37,14 @@ c
 c
 c...  subroutine arguments
 c
-      integer nsol,maxcg,kw,idout
-      include "rmult_dim.inc"
-      include "rmin_dim.inc"
+      integer nprevdflag,kw,idout
       character ofile*(*)
 c
 c...  local variables
 c
-      integer i
-c
       if(idout.gt.0) then
         open(kw,file=ofile,status="old",access="append")
-        write(kw,700) nsol,maxcg,(rmult(i),i=1,3),(rmin(i),i=1,3)
+        write(kw,700) nprevdflag
         close(kw)
       end if
 c
@@ -57,25 +52,7 @@ c
 c
 700   format(///' l i n e a r   s o l u t i o n   ',
      & 'i n f o r m a t i o n'//,5x,
-     & ' PCG solution method  . . . . . . . . . . . (nsol)  =',i5,//,5x,
-     & '    eq.1,  diagonal preconditioner,                  ',/,5x,
-     & '           use zero initial guess                    ',/,5x,
-     & '    eq.2,  diagonal preconditioner,                  ',/,5x,
-     & '           use displacements from previous step      ',/,5x,
-     & '    eq.3,  symmetrized Gauss-Seidel preconditioner   ',/,5x,
-     & '           use zero initial guess                    ',/,5x,
-     & '    eq.4,  symmetrized Gauss-Seidel preconditioner   ',/,5x,
-     & '           use displacements from previous step      ',//,5x,
-     & ' maximum number of PCG iterations . . . . . (maxcg) =',i5,//,5x,
-     & ' accuracy multiplier for displacement . . . (rmult1)=',1pe15.8,
-     & /,5x,
-     & ' accuracy multiplier for force. . . . . . . (rmult2)=',1pe15.8,
-     & /,5x,
-     & ' accuracy multiplier for energy . . . . . . (rmult3)=',1pe15.8,
-     & /,5x,
-     & ' minimum displacement accuracy. . . . . . . (rmin1) =',1pe15.8,
-     & /,5x,
-     & ' minimum force accuracy . . . . . . . . . . (rmin2) =',1pe15.8,
-     & /,5x,
-     & ' minimum energy accuracy. . . . . . . . . . (rmin3) =',1pe15.8)
+     & ' initial solution flag  . . . . . . . .(nprevdflag) =',i5,//,5x,
+     & '    eq.0,  use zero initial guess                    ',/,5x,
+     & '    eq.1,  use displacements from previous step      ')
       end
