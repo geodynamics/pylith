57b2d0c0bf42ee60c5f272f3dbb12deff46dce44
Charles Williams <C.Williams@gns.cri.nz>
2005-04-02 07:58:39 +0000

    Added commented-out line for PETSc debugging when desired. This is presently not working.

--- lithomop3d/examples/lintet/bm1a/Make.mm
+++ lithomop3d/examples/lintet/bm1a/Make.mm
@@ -14,12 +14,14 @@
 test: clean
 	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view
 
+	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -start_in_debugger
+
 clean::
 	$(RM_F) bm1a.ascii bm1a.plot bm1a*.inp
 
 
 # version
-# $Id: Make.mm,v 1.4 2005/03/11 02:51:41 willic3 Exp $
+# $Id: Make.mm,v 1.5 2005/04/01 23:58:39 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
