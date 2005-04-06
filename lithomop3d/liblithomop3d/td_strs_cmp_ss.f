ab749f17676eb71e64cd49096f26e1fc8cf3dcaa
Charles Williams <C.Williams@gns.cri.nz>
2005-04-06 06:03:46 +0000

    Cosmetic changes.

--- lithomop3d/liblithomop3d/td_strs_cmp_ss.f
+++ lithomop3d/liblithomop3d/td_strs_cmp_ss.f
@@ -4,9 +4,8 @@ c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c                             Charles A. Williams
 c                       Rensselaer Polytechnic Institute
-c                        (C) 2004  All Rights Reserved
+c                        (C) 2005  All Rights Reserved
 c
-c  Copyright 2004 Rensselaer Polytechnic Institute.
 c  All worldwide rights reserved.  A license to use, copy, modify and
 c  distribute this software for non-commercial research purposes only
 c  is hereby granted, provided that this copyright notice and
@@ -162,7 +161,7 @@ c
       end
 c
 c version
-c $Id: td_strs_cmp_ss.f,v 1.8 2005/03/19 01:49:49 willic3 Exp $
+c $Id: td_strs_cmp_ss.f,v 1.9 2005/04/05 23:03:46 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
