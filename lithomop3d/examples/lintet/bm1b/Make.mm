f420cc3f83aa6b0b894eb9862f92df81c2a56e59
Charles Williams <C.Williams@gns.cri.nz>
2005-04-08 07:47:49 +0000

    Added '-ksp_rtol 1e-9' to the command line arguments. This dramatically improves the accuracy for slippery node tests, and likely will help for the other test problems. I need to change all of the Make.mm files for example problems to be similar to these, and also need to modify all input files for current formats.

--- lithomop3d/examples/lintet/bm1b/Make.mm
+++ lithomop3d/examples/lintet/bm1b/Make.mm
@@ -12,15 +12,15 @@
 #
 
 test: clean
-	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1b -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view
-	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1b -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -start_in_debugger
+	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1b -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
+	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1b -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9 -start_in_debugger
 
 clean::
 	$(RM_F) bm1b.ascii bm1b.plot bm1b*.inp
 
 
 # version
-# $Id: Make.mm,v 1.1 2005/04/01 22:55:27 willic3 Exp $
+# $Id: Make.mm,v 1.2 2005/04/08 00:47:50 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
