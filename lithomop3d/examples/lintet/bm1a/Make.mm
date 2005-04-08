73b89866aa0e789baffbb932eaf15f6b4359a658
Charles Williams <willic3>
2005-04-08 07:47:49 +0000

    Added '-ksp_rtol 1e-9' to the command line arguments. This dramatically improves the accuracy for slippery node tests, and likely will help for the other test problems. I need to change all of the Make.mm files for example problems to be similar to these, and also need to modify all input files for current formats.

--- lithomop3d/examples/lintet/bm1a/Make.mm
+++ lithomop3d/examples/lintet/bm1a/Make.mm
@@ -12,16 +12,16 @@
 #
 
 test: clean
-	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view
+	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
 
-	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -start_in_debugger
+	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1a -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9 -start_in_debugger
 
 clean::
 	$(RM_F) bm1a.ascii bm1a.plot bm1a*.inp
 
 
 # version
-# $Id: Make.mm,v 1.5 2005/04/01 23:58:39 willic3 Exp $
+# $Id: Make.mm,v 1.6 2005/04/08 00:47:49 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
--- lithomop3d/examples/lintet/bm5/Make.mm
+++ lithomop3d/examples/lintet/bm5/Make.mm
@@ -12,15 +12,15 @@
 #
 
 test: clean
-	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view
-	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -start_in_debugger noxterm
+	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
+	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9 -start_in_debugger noxterm
 
 clean::
 	$(RM_F) bm5.ascii bm5.plot bm5*.inp
 
 
 # version
-# $Id: Make.mm,v 1.1 2005/04/05 23:14:54 willic3 Exp $
+# $Id: Make.mm,v 1.2 2005/04/08 00:47:50 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
