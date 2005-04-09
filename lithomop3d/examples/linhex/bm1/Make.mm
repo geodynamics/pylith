2d54594e9f3a2af7516b8de43681f80cf5d72e3d
Charles Williams <willic3>
2005-04-09 01:29:06 +0000

    Updated these input files for current code version.

--- lithomop3d/examples/linhex/bm1/Make.mm
+++ lithomop3d/examples/linhex/bm1/Make.mm
@@ -4,22 +4,41 @@
 #
 #                             Charles A. Williams
 #                       Rensselaer Polytechnic Institute
-#                        (C) 2003  All Rights Reserved
+#                        (C) 2005  All Rights Reserved
 #
-# <LicenseText>
+#  All worldwide rights reserved.  A license to use, copy, modify and
+#  distribute this software for non-commercial research purposes only
+#  is hereby granted, provided that this copyright notice and
+#  accompanying disclaimer is not modified or removed from the software.
+#
+#  DISCLAIMER:  The software is distributed "AS IS" without any express
+#  or implied warranty, including but not limited to, any implied
+#  warranties of merchantability or fitness for a particular purpose
+#  or any warranty of non-infringement of any current or pending patent
+#  rights.  The authors of the software make no representations about
+#  the suitability of this software for any particular purpose.  The
+#  entire risk as to the quality and performance of the software is with
+#  the user.  Should the software prove defective, the user assumes the
+#  cost of all necessary servicing, repair or correction.  In
+#  particular, neither Rensselaer Polytechnic Institute, nor the authors
+#  of the software are liable for any indirect, special, consequential,
+#  or incidental damages related to the software, to the maximum extent
+#  the law permits.
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
 test: clean
-	lithomop3dapp.py -lm3dscan.fileRoot=bm1
+        lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1 -lm3dscan.ascii Output=full -lm3dscan.autoRotateSlipperyNodes=False -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
+        # lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm1 -lm3dscan.ascii Output=full -lm3dscan.autoRotateSlipperyNodes=False -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9 -start_in_debugger noxterm
+
 
 clean::
 	$(RM_F) bm1.ascii bm1.plot bm1*.inp
 
 
 # version
-# $Id: Make.mm,v 1.1 2004/08/25 16:56:13 willic3 Exp $
+# $Id: Make.mm,v 1.2 2005/04/08 18:29:06 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
