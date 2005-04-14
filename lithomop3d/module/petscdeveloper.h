03324705ecad63e5c6c4c8ab66880ff27c93b658
Charles Williams <willic3>
2005-04-14 05:04:17 +0000

    Added this file as a kludgy fix to let the code know whether the user is using the developer's version of PETSc or not.  If so, the tag PETSC_DEVELOPER_VERSION needs to be set to 1.

---
+++ lithomop3d/module/petscdeveloper.h
@@ -0,0 +1,43 @@
+// -*- C++ -*-
+// 
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+//                               Charles A. Williams
+//                        Rensselaer Polytechnic Institute
+//                        (C) 2005 All Rights Reserved
+// 
+//  All worldwide rights reserved.  A license to use, copy, modify and
+//  distribute this software for non-commercial research purposes only
+//  is hereby granted, provided that this copyright notice and
+//  accompanying disclaimer is not modified or removed from the software.
+//
+//  DISCLAIMER:  The software is distributed "AS IS" without any express
+//  or implied warranty, including but not limited to, any implied
+//  warranties of merchantability or fitness for a particular purpose
+//  or any warranty of non-infringement of any current or pending patent
+//  rights.  The authors of the software make no representations about
+//  the suitability of this software for any particular purpose.  The
+//  entire risk as to the quality and performance of the software is with
+//  the user.  Should the software prove defective, the user assumes the
+//  cost of all necessary servicing, repair or correction.  In
+//  particular, neither Rensselaer Polytechnic Institute, nor the authors
+//  of the software are liable for any indirect, special, consequential,
+//  or incidental damages related to the software, to the maximum extent
+//  the law permits.
+// 
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+
+#if !defined(pylithomop3d_petscdeveloper_h)
+#define pylithomop3d_petscdeveloper_h
+//  Kludgy fix to let the bindings know whether you are using the
+//  developer's version of PETSc 2.2.1.  If you are, change the value
+//  below to 1.  Otherwise, leave this file unchanged.
+#define PETSC_DEVELOPER_VERSION 0
+
+#endif
+
+// version
+// $Id: petscdeveloper.h,v 1.1 2005/04/13 22:04:17 willic3 Exp $
+
+// End of file
