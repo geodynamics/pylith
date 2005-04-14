7d3d78b5f65d490cd6b78a8e4ff7a67d55b9c8af
Charles Williams <willic3>
2005-04-14 07:48:18 +0000

    Initial version of a simple routine that sorts split node entries according to the new element ordering.

---
+++ lithomop3d/liblithomop3d/sort_split_nodes.f
@@ -0,0 +1,75 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2005  All Rights Reserved
+c
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine sort_split_nodes(nfault,indxiel,numfn,numelv)
+c
+c...  sorts split nodes according to reordered elements
+c
+c      nfault(3,numfn)    = element, node, and history for each entry
+c      numfn              = number of split node entries
+c
+c
+
+      include "implicit.inc"
+c
+c...  parameter definitions
+c
+      include "ndimens.inc"
+      include "nconsts.inc"
+      include "rconsts.inc"
+c
+c...  subroutine arguments
+c
+      integer numfn,numelv
+      integer nfault(3,numfn),indxiel(numelv)
+c
+c...  intrinsic functions
+c
+c
+c...  local variables
+c
+      integer i
+c
+c...  open input file and read entries
+c
+      if(numfn.eq.izero) return
+      do i=1,numfn
+        nfault(1,i)=indxiel(nfault(1,i))
+      end do
+c
+      return
+      end
+c
+c version
+c $Id: sort_split_nodes.f,v 1.1 2005/04/14 00:48:18 willic3 Exp $
+c
+c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
+c
+c End of file 
