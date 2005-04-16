1aa511130027ac4461e0c8f6d9ec3ff1bc6c39e7
Charles Williams <willic3>
2005-04-16 07:33:29 +0000

    New routine to replace element numbers with reordered numbers.

---
+++ lithomop3d/liblithomop3d/sort_slip_nodes.f
@@ -0,0 +1,71 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2005  All Rights Reserved
+c
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine sort_slip_nodes(nslip,indxiel,numslp,numelv)
+c
+c...  sorts slippery nodes according to reordered elements
+c
+c       nslip(ndof+2)     = element, node, and slip weights for each
+c                           entry
+c       numslp            = number of slippery node entries
+c
+      include "implicit.inc"
+c
+c...  parameter definitions
+c
+      include "ndimens.inc"
+      include "nconsts.inc"
+c
+c...  subroutine arguments
+c
+      integer numslp,numelv
+      integer nslip(nsdim,numslp),indxiel(numelv)
+c
+c...  local variables
+c
+      integer i
+c
+      if(numslp.eq.izero) return
+c
+c...  swap element numbers to new values
+c
+      do i=1,numslp
+        nslip(1,i)=indxiel(nslip(1,i))
+      end do
+c
+      return
+      end
+c
+c version
+c $Id: sort_slip_nodes.f,v 1.1 2005/04/16 00:33:29 willic3 Exp $
+c
+c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
+c
+c End of file 
