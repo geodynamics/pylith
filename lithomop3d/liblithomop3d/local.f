d9eacef4f4c516cd60389b058bf082ff95e10647
Charles Williams <willic3>
2005-04-16 07:43:31 +0000

    Cosmetic changes.

--- lithomop3d/liblithomop3d/local.f
+++ lithomop3d/liblithomop3d/local.f
@@ -4,9 +4,8 @@ c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c                             Charles A. Williams
 c                       Rensselaer Polytechnic Institute
-c                        (C) 2004  All Rights Reserved
+c                        (C) 2005  All Rights Reserved
 c
-c  Copyright 2004 Rensselaer Polytechnic Institute.
 c  All worldwide rights reserved.  A license to use, copy, modify and
 c  distribute this software for non-commercial research purposes only
 c  is hereby granted, provided that this copyright notice and
@@ -61,7 +60,7 @@ c
       end
 c
 c version
-c $Id: local.f,v 1.4 2005/03/21 22:35:18 willic3 Exp $
+c $Id: local.f,v 1.5 2005/04/16 00:43:31 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
--- lithomop3d/liblithomop3d/read_skew.f
+++ lithomop3d/liblithomop3d/read_skew.f
@@ -4,9 +4,8 @@ c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c                             Charles A. Williams
 c                       Rensselaer Polytechnic Institute
-c                        (C) 2004  All Rights Reserved
+c                        (C) 2005  All Rights Reserved
 c
-c  Copyright 2004 Rensselaer Polytechnic Institute.
 c  All worldwide rights reserved.  A license to use, copy, modify and
 c  distribute this software for non-commercial research purposes only
 c  is hereby granted, provided that this copyright notice and
@@ -124,7 +123,7 @@ c
       end
 c
 c version
-c $Id: read_skew.f,v 1.3 2005/04/14 00:59:44 willic3 Exp $
+c $Id: read_skew.f,v 1.4 2005/04/16 00:44:19 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
