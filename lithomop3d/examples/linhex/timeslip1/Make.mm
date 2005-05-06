80cf78600cfbc23f4d22d035bcfdac1c67cfef80
Charles Williams <willic3>
2005-05-06 04:31:40 +0000

    Problem to test usage of time-varying Winkler elements for locking and unlocking a fault.

---
+++ lithomop3d/examples/linhex/timeslip1/Make.mm
@@ -0,0 +1,45 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#                        (C) 2005  All Rights Reserved
+#
+#  All worldwide rights reserved.  A license to use, copy, modify and
+#  distribute this software for non-commercial research purposes only
+#  is hereby granted, provided that this copyright notice and
+#  accompanying disclaimer is not modified or removed from the software.
+#
+#  DISCLAIMER:  The software is distributed "AS IS" without any express
+#  or implied warranty, including but not limited to, any implied
+#  warranties of merchantability or fitness for a particular purpose
+#  or any warranty of non-infringement of any current or pending patent
+#  rights.  The authors of the software make no representations about
+#  the suitability of this software for any particular purpose.  The
+#  entire risk as to the quality and performance of the software is with
+#  the user.  Should the software prove defective, the user assumes the
+#  cost of all necessary servicing, repair or correction.  In
+#  particular, neither Rensselaer Polytechnic Institute, nor the authors
+#  of the software are liable for any indirect, special, consequential,
+#  or incidental damages related to the software, to the maximum extent
+#  the law permits.
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+test: clean
+	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=timeslip1 -lm3dscan.asciiOutput=full -lm3dscan.autoRotateSlipperyNodes=False -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-09
+	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=timeslip1 -lm3dscan.asciiOutput=full -lm3dscan.autoRotateSlipperyNodes=False -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-09 -mat_view -start_in_debugger
+
+clean::
+	$(RM_F) timeslip1.ascii timeslip1.plot timeslip1*.inp
+
+
+# version
+# $Id: Make.mm,v 1.1 2005/05/05 21:31:40 willic3 Exp $
+
+# Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
+
+# End of file 
