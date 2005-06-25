cda59875f5adb3b4722d6aa200a357c1e8c0c84d
Charles Williams <C.Williams@gns.cri.nz>
2005-06-25 03:26:18 +0000

    Removed obsolete petsc_solver argument from command line. Explicitly specify binary UCD output using the lm3dscan.ucdOutput variable (even though binary is now the default).

--- lithomop3d/examples/lintet/bm5/Make.mm
+++ lithomop3d/examples/lintet/bm5/Make.mm
@@ -12,15 +12,17 @@
 #
 
 test: clean
-	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
-	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9 -start_in_debugger noxterm
+	lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -lm3dscan.ucdOutput=binary -log_summary -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
+
+# test: clean
+	# lithomop3dapp.py -typos=relaxed -lm3dscan.fileRoot=bm5 -lm3dscan.ucdOutput=ascii -log_summary -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9 -start_in_debugger
 
 clean::
 	$(RM_F) bm5.ascii bm5.plot bm5*.inp
 
 
 # version
-# $Id: Make.mm,v 1.2 2005/04/08 00:47:50 willic3 Exp $
+# $Id: Make.mm,v 1.3 2005/06/24 20:26:18 willic3 Exp $
 
 # Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
 
