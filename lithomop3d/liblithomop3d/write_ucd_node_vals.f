11d4f6c263b10535ecb62e4445f9025a7710bbd4
Charles Williams <willic3>
2005-06-25 03:13:28 +0000

    Added new section for writing binary UCD nodal values and computing nodal min/max values.

--- lithomop3d/liblithomop3d/write_ucd_node_vals.f
+++ lithomop3d/liblithomop3d/write_ucd_node_vals.f
@@ -29,7 +29,8 @@ c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 c
 c
       subroutine write_ucd_node_vals(d,deld,tfault,dfault,nfault,numfn,
-     & dx,deldx,idslp,numsn,deltp,nstep,numnp,kucd,ucdroot,iprestress)
+     & dx,deldx,idslp,numsn,deltp,nstep,numnp,kucd,iucd,ucdroot,
+     & iprestress)
 c
 c...  Specialized routine to output displacement info for SCEC
 c     benchmarks.
@@ -43,6 +44,8 @@ c     to output displacements, displacement increments, and/or
 c     velocities.
 c     Note also that the split node output is not really a UCD file,
 c     since it contains both node and element information.
+c     Either binary or ascii UCD files are written, depending on the
+c     value of the iucd parameter.
 c
       include "implicit.inc"
 c
@@ -54,7 +57,7 @@ c
 c
 c...  subroutine arguments
 c
-      integer numfn,numsn,nstep,numnp,kucd,iprestress
+      integer numfn,numsn,nstep,numnp,kucd,iucd,iprestress
       integer nfault(3,numfn),idslp(numsn)
       double precision deltp
       double precision d(ndof,numnp),deld(ndof,numnp)
@@ -80,10 +83,30 @@ c
 c...  local variables
 c
       integer i,j,i1,i2,inode
+      integer ibyte,intlen,floatlen,len
       double precision rmult
+      double precision vmin(6),vmax(6)
       character filenm*200,cstep*5
+      character nlabels*1024,nunits*1024
 c
 cdebug      write(6,*) "Hello from write_ucd_node_vals!"
+c
+      if(iucd.eq.izero) return
+c
+c...  set up labels and byte-counting info for binary output
+c
+      nlabels="X-Displacement.Y-Displacement.Z-Displacement."//
+     &        "X-Velocity.Y-Velocity.Z-Velocity."
+      nlabels(1024:1024)="0"
+      nunits="meters.meters.meters."//
+     &       "meters/second.meters/second.meters/second."
+      nunits(1024:1024)="0"
+      ibyte=ione
+      len=ione
+      intlen=ifour
+      floatlen=ifour
+c
+c...  determine filename
 c
       if(deltp.ne.zero) then
         rmult=one/deltp
@@ -97,61 +120,124 @@ c
       else
         cstep="prest"
       end if
+      filenm=ucdroot(i1:i2)//".mesh.time."//cstep//".inp"
+c
+c...  ascii UCD output
+c
+      if(iucd.eq.ione) then
 c
 c...  write mesh info
 c
-      filenm=ucdroot(i1:i2)//".mesh.time."//cstep//".inp"
-      open(kucd,file=filenm,status="new")
-      write(kucd,"(7i7)") nnvals,(ival(i),i=1,nnvals)
-      do i=1,nnvals
-        write(kucd,"(a9)") dout(i)
-      end do
+        open(kucd,file=filenm,status="new")
+        write(kucd,"(7i7)") nnvals,(ival(i),i=1,nnvals)
+        do i=1,nnvals
+          write(kucd,"(a9)") dout(i)
+        end do
 c
 c...  write nodal displacements
 c
-      do i=1,numnp
-        write(kucd,"(i7,6(2x,1pe15.8))") i,(d(j,i),j=1,ndof),
-     &   (rmult*deld(j,i),j=1,ndof)
-      end do
-      close(kucd)
+        do i=1,numnp
+          write(kucd,"(i7,6(2x,1pe15.8))") i,(d(j,i),j=1,ndof),
+     &     (rmult*deld(j,i),j=1,ndof)
+        end do
+        close(kucd)
 c
 c...  write split node displacements, if there are any
 c
-      if(numfn.ne.0) then
-        filenm=ucdroot(i1:i2)//".mesh.split.time."//cstep//".inp"
-        open(kucd,file=filenm,status="new")
-        write(kucd,"(7i7)") nnvals,(ival(i),i=1,nnvals)
-        do i=1,nnvals
-          write(kucd,"(a9)") dout(i)
-        end do
-        do i=1,numfn
-          write(kucd,"(2i7,6(2x,1pe15.8))") nfault(1,i),nfault(2,i),
-     &     (tfault(j,i),j=1,ndof),(rmult*dfault(j,i),j=1,ndof)
-        end do
-        close(kucd)
-      end if
+        if(numfn.ne.0) then
+          filenm=ucdroot(i1:i2)//".mesh.split.time."//cstep//".inp"
+          open(kucd,file=filenm,status="new")
+          write(kucd,"(7i7)") nnvals,(ival(i),i=1,nnvals)
+          do i=1,nnvals
+            write(kucd,"(a9)") dout(i)
+          end do
+          do i=1,numfn
+            write(kucd,"(2i7,6(2x,1pe15.8))") nfault(1,i),nfault(2,i),
+     &       (tfault(j,i),j=1,ndof),(rmult*dfault(j,i),j=1,ndof)
+          end do
+          close(kucd)
+        end if
 c
 c...  write slippery node displacements, if there are any
 c
-      if(numsn.ne.0) then
-        filenm=ucdroot(i1:i2)//".mesh.slip.time."//cstep//".inp"
-        open(kucd,file=filenm,status="new")
-        write(kucd,"(7i7)") nnvals,(ival(i),i=1,nnvals)
-        do i=1,nnvals
-          write(kucd,"(a9)") dout(i)
+        if(numsn.ne.0) then
+          filenm=ucdroot(i1:i2)//".mesh.slip.time."//cstep//".inp"
+          open(kucd,file=filenm,status="new")
+          write(kucd,"(7i7)") nnvals,(ival(i),i=1,nnvals)
+          do i=1,nnvals
+            write(kucd,"(a9)") dout(i)
+          end do
+          do i=1,numsn
+            inode=idslp(i)
+            write(kucd,"(i7,6(2x,1pe15.8))") idslp(inode),
+     &       (dx(j,inode),j=1,ndof),(rmult*deldx(j,inode),j=1,ndof)
+          end do
+          close(kucd)
+        end if
+c
+c...  binary UCD output
+c
+      else if(iucd.eq.itwo) then
+        open(kucd,file=filenm,status="new",access="direct",recl=len,
+     &   form="unformatted")
+c
+c...  determin min/max of data values
+c
+        do i=1,6
+          vmin(i)=1.0d30
+          vmax(i)=-1.0d30
         end do
-        do i=1,numsn
-          inode=idslp(i)
-          write(kucd,"(i7,6(2x,1pe15.8))") idslp(inode),
-     &     (dx(j,inode),j=1,ndof),(rmult*deldx(j,inode),j=1,ndof)
+        do i=1,numnp
+          vmin(1)=min(vmin(1),d(1,i))
+          vmin(2)=min(vmin(2),d(2,i))
+          vmin(3)=min(vmin(3),d(3,i))
+          vmin(4)=min(vmin(4),rmult*deld(1,i))
+          vmin(5)=min(vmin(5),rmult*deld(2,i))
+          vmin(6)=min(vmin(6),rmult*deld(3,i))
+          vmax(1)=max(vmax(1),d(1,i))
+          vmax(2)=max(vmax(2),d(2,i))
+          vmax(3)=max(vmax(3),d(3,i))
+          vmax(4)=max(vmax(4),rmult*deld(1,i))
+          vmax(5)=max(vmax(5),rmult*deld(2,i))
+          vmax(6)=max(vmax(6),rmult*deld(3,i))
         end do
+c
+c...  write header info
+c
+        write(kucd,rec=ibyte) nlabels,nunits
+        ibyte=ibyte+2048
+        write(kucd,rec=ibyte) nnvals,(ival(i),i=1,nnvals)
+        ibyte=ibyte+intlen*(1+nnvals)
+        write(kucd,rec=ibyte) (real(vmin(i)),i=1,nnvals)
+        ibyte=ibyte+floatlen*nnvals
+        write(kucd,rec=ibyte) (real(vmax(i)),i=1,nnvals)
+        ibyte=ibyte+floatlen*nnvals
+c
+c...  write node data
+c
+        do j=1,ndof
+          write(kucd,rec=ibyte) (real(d(j,i)),i=1,numnp)
+          ibyte=ibyte+numnp*floatlen
+        end do
+        do j=1,ndof
+          write(kucd,rec=ibyte) (real(rmult*deld(j,i)),i=1,numnp)
+          ibyte=ibyte+numnp*floatlen
+        end do
+c
+c...  write terminating values and close file
+c
+        write(kucd,rec=ibyte) (real(vmax(i)),i=1,nnvals)
         close(kucd)
+c
+c...  NOTE:  for now, binary UCD files are not written for split or
+c     slippery nodes, since there does not appear to be a use for them.
+c
       end if
       return
       end
 c
 c version
-c $Id: write_ucd_node_vals.f,v 1.4 2005/04/14 01:01:22 willic3 Exp $
+c $Id: write_ucd_node_vals.f,v 1.5 2005/06/24 20:13:28 willic3 Exp $
 c
 c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
 c
