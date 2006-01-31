03b70c519366a53d15cbe2094582b57917cf8219
Charles Williams <C.Williams@gns.cri.nz>
2006-01-31 17:16:23 +0000

    Updated, but still incomplete, version of code to get mesh object using Tecton-style input.

--- lithomop3d/lithomop3d/MeshImporterTecton.py
+++ lithomop3d/lithomop3d/MeshImporterTecton.py
@@ -36,41 +36,65 @@
 class MeshImporterTecton(MeshImporter):
   """Class for importing a mesh using Tecton-style (original LithoMop) input files."""
 
-  def mesh(self, coordInputFile, elemInputFile, numDims):
+  def mesh(self, coordInputFile, elemInputFile, numDims, f77FileInput):
     """Get a finite element mesh."""
 
     from lithomop3d.Mesh import Mesh
     mesh = Mesh()
 
     # Get nodes
-    self._getNodes(mesh.nodes, coordInputFile, numDims)
+    self._getNodes(mesh.nodes, coordInputFile, numDims, f77FileInput)
 
     # Get elements and define element families and materials
-    self._getElements(mesh.elements)
+    self._getElements(mesh.elements, elemInputFile, f77FileInput)
 
 
     return mesh
 
-  def _getNodes(self, nodes, coordInputFile, numDims):
+  def _getNodes(self, nodes, coordInputFile, numDims, f77FileInput):
     """Gets dimensions and nodal coordinates from Tecton-style input file."""
 
     import pyre.units
+    import lithomop3d
     
     self._nodeInfo = lithomop3d.scan_coords(
       f77FileInput,
       numDims,
       coordInputFile)
 
-    self._numNodes = self._nodeInfo[0]
+    self.numNodes = self._nodeInfo[0]
     self._coordUnits = self._nodeInfo[1]
     
     self._coordScaleString = \
                            uparser.parse(string.strip(self._coordUnits))
     self._coordScaleFactor = self._coordScaleString.value
+
+    self.ptrCoords = lithomop3d.allocateDouble(numDims*self.numNodes)
+
+    nodes['numNodes'] = self.numNodes
+    nodes['dim'] = numDims
+    nodes['ptrCoords'] = self.ptrCoords
+
+    lithomop3d.read_coords(nodes,
+                           self._coordScaleFactor,
+                           self.f77FileInput,
+                           coordInputFile)
+
+    return
+      
+    
     
   def __init__(self, name="meshimporter"):
     """Constructor."""
+
     Mesher.__init__(self, name)
+
+    self.numNodes = 0
+    self._coordUnits = 0.0
+    self._coordUnits = "coordUnitsInitial1234567890123"
+    self._coordScaleString = "coordScaleStringInitial1234567"
+    self.ptrCoords = None
+
     return
         
 
