90b39ddf61e7034cbd8c71740598eac14d7f3f5f
Charles Williams <willic3>
2006-02-14 16:54:50 +0000

    Moved current working versions of files to 'pyre' directory. That way, these will not conflict with current functioning code in 'lithomop3d' directory.

---
+++ lithomop3d/pyre/MeshImporter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#             Copyright (C) 2006 Rensselaer Polytechnic Institute
+#
+# 
+# 	Permission is hereby granted, free of charge, to any person
+# 	obtaining a copy of this software and associated documentation
+# 	files (the "Software"), to deal in the Software without
+# 	restriction, including without limitation the rights to use,
+# 	copy, modify, merge, publish, distribute, sublicense, and/or
+# 	sell copies of the Software, and to permit persons to whom the
+# 	Software is furnished to do so, subject to the following
+# 	conditions:
+# 
+# 	The above copyright notice and this permission notice shall be
+# 	included in all copies or substantial portions of the Software.
+# 
+# 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# 	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# 	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# 	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# 	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# 	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# 	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# 	OTHER DEALINGS IN THE SOFTWARE.
+#         
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Initial attempt at a module that gets a Mesh object (generic version).
+
+from Mesher import Mesher
+
+class MeshImporter(Mesher):
+  """Template for a generic MeshImporter class."""
+
+  def __init__(self, name="meshimporter"):
+    """Constructor."""
+    Mesher.__init__(self, name)
+    return
+        
+
+# version
+__id__ = "$Id$"
+
+# End of file 
---
+++ lithomop3d/pyre/Mesher.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#             Copyright (C) 2006 Rensselaer Polytechnic Institute
+#
+# 
+# 	Permission is hereby granted, free of charge, to any person
+# 	obtaining a copy of this software and associated documentation
+# 	files (the "Software"), to deal in the Software without
+# 	restriction, including without limitation the rights to use,
+# 	copy, modify, merge, publish, distribute, sublicense, and/or
+# 	sell copies of the Software, and to permit persons to whom the
+# 	Software is furnished to do so, subject to the following
+# 	conditions:
+# 
+# 	The above copyright notice and this permission notice shall be
+# 	included in all copies or substantial portions of the Software.
+# 
+# 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# 	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# 	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# 	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# 	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# 	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# 	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# 	OTHER DEALINGS IN THE SOFTWARE.
+#         
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Generic class for mesh generation or importing
+
+from pyre.components.Component import Component
+
+class Mesher(Component):
+  """Python manager for mesh generation or importing."""
+
+  def generate(self):
+    """Create finite-element mesh."""
+
+    return
+
+  def __init__(self, name="mesher"):
+    """Constructor."""
+    Component.__init__(self, name, facility="mesher")
+    return
+
+# version
+__id__ = "$Id$"
+
+# End of file 
