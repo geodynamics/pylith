eab4afe4574c6defd6a997caed21404f0755e782
Charles Williams <C.Williams@gns.cri.nz>
2006-04-11 15:42:18 +0000

    Beginnings of an ElemFamilies class.  This will essentially provide a bunch of functions for looping over families.  The present implementation that uses a dictionary to store the elemFamilies info seems a bit clumsy.

---
+++ lithomop3d/pyre/ElemFamilies.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#             Copyright (C) 2006 Rensselaer Polytechnic Institute
+#
+# 
+# 	Permission is hereby granted, free of charge, to any person
+# 	obtaining a copy of this software and associated documentation
+# 	files (the "Software"), to deal in the Software without
+# 	restriction, including without limitation the rights to use,
+# 	copy, modify, merge, publish, distribute, sublicense, and/or
+# 	sell copies of the Software, and to permit persons to whom the
+# 	Software is furnished to do so, subject to the following
+# 	conditions:
+# 
+# 	The above copyright notice and this permission notice shall be
+# 	included in all copies or substantial portions of the Software.
+# 
+# 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# 	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# 	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# 	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# 	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# 	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# 	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# 	OTHER DEALINGS IN THE SOFTWARE.
+#         
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+from pyre.components.Component import Component
+
+#ElemFamilies class
+class ElemFamilies(Component):
+  """Python manager for solid elements of a given type and material ID."""
+
+  def initialize(self, elements, elemFamilies):
+    """Puts elements into families and allocates necessary storage."""
+    from ElemFamilySolid import ElemFamilySolid
+
+    # This could be much cleaner.  I'm not sure if dictionaries are the
+    # way to go for describing families.
+    numElemFamilies = elemFamilies["numElemFamilies"]
+    elemFamilySizes = elemFamilies["elemFamilySizes"]
+    elemFamilyTypes = elemFamilies["elemFamilyTypes"]
+    elemFamilyIds = elemFamilies["elemFamilyIds"]
+    elemFamilyNames = elemFamilies["elemFamilyNames"]
+    for i in range(numElemFamilies):
+      familyName = elemFamilyNames[i]
+      familySize = elemFamilySizes[i]
+      familyType = elemFamilyTypes[i]
+      familyId = elemFamilyIds[i]
+      elemFamily = ElemFamilySolid()
+      elemFamily.initialize(familyName,
+                            familySize,
+                            familyType,
+                            familyId)
+  
+
+# version
+__id__ = "$Id$"
+
+# End of file 
