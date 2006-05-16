261e449f28b556defd3e8fba9eaf0b93a7fead8a
Brad Aagaard <baagaard@usgs.gov>
2006-05-16 16:28:53 +0000

    Added missing __init__.py

---
+++ pylith/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ----------------------------------------------------------------------
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# <LicenseText>
+#
+# ----------------------------------------------------------------------
+#
+
+## @file pylith/__init__.py
+## @brief Python top-level PyLith module initialization
+
+# version
+__id__ = "$Id$"
+
+# End of file
