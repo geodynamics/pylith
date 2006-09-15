ef602c7f49f408332b38ea331c92c074877055a6
Brad Aagaard <baagaard>
2006-09-15 02:32:57 +0000

    Removed old, obsolete pylith stuff.

--- pylith3d/examples/linhex/timeslip1/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pylith3dapp.py -typos=relaxed -pl3dscan.fileRoot=linhex/timeslip1/timeslip1 -pl3dscan.asciiOutput=full -pl3dscan.autoRotateSlipperyNodes=False -log_summary -petsc_solver 1 -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-09
-
-# end of file
