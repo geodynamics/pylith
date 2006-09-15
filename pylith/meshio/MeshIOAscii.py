e65e564befcbafa7be74b61844a649543eac110f
Brad Aagaard <baagaard>
2006-09-15 03:49:20 +0000

    Added missing filename as property.

--- pylith/meshio/MeshIOAscii.py
+++ pylith/meshio/MeshIOAscii.py
@@ -24,6 +24,26 @@ class MeshIOAscii(MeshIO):
   ASCII file.
   """
 
+  # INVENTORY //////////////////////////////////////////////////////////
+
+  class Inventory(Component.Inventory):
+    """Python object for managing MeshIOAscii facilities and properties."""
+
+    ## @class Inventory
+    ## Python object for managing MeshIOAscii facilities and properties.
+    ##
+    ## \b Properties
+    ## @li \b filename Name of mesh file
+    ##
+    ## \b Facilities
+    ## @li None
+
+    import pyre.inventory
+
+    filename = pyre.inventory.str("filename", default=False)
+    filename.meta['tip'] = "Name of mesh file"
+
+
   # PUBLIC METHODS /////////////////////////////////////////////////////
 
   def __init__(self, name="meshioascii"):
@@ -31,6 +51,15 @@ class MeshIOAscii(MeshIO):
     MeshIO.__init__(self, name)
     import pylith.meshio.meshio as bindings
     self.cppHandler = bindings.MeshIOAscii()
+    return
+
+
+  # PRIVATE METHODS ////////////////////////////////////////////////////
+
+  def _configure(self):
+    """Set members based using inventory."""
+    MeshIO.configure(self)
+    self.filename = self.inventory.filename
     return
 
 
