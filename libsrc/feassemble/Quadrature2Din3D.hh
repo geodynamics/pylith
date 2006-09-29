bfdda86a53b64ab7df09acbf13fcbbfd9019ca49
Brad Aagaard <baagaard>
2006-09-29 05:07:58 +0000

    Added Quadrature2Din3D and Quadrature3D (not tested yet).

---
+++ libsrc/feassemble/Quadrature2Din3D.hh
@@ -0,0 +1,77 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+/**
+ * @file pylith/feassemble/Quadrature2Din3D.hh
+ *
+ * @brief Quadrature for 2-D finite-elements in 3-D space.
+ */
+
+#if !defined(pylith_feassemble_quadrature2din3d_hh)
+#define pylith_feassemble_quadrature2din3d_hh
+
+#include "Quadrature.hh"
+
+namespace pylith {
+  namespace feassemble {
+    class Quadrature2Din3D;
+    class TestQuadrature2Din3D;
+  } // feassemble
+} // pylith
+
+class pylith::feassemble::Quadrature2Din3D : public Quadrature
+{ // Quadrature2D
+  friend class TestQuadrature2Din3D; // unit testing
+
+// PUBLIC MEMBERS ///////////////////////////////////////////////////////
+public :
+
+  /// Constructor
+  Quadrature2Din3D(void);
+
+  /// Destructor
+  virtual ~Quadrature2Din3D(void);
+
+  /// Create a copy of this object.
+  virtual
+  Quadrature* clone(void) const;
+
+// PROTECTED METHODS ////////////////////////////////////////////////////
+protected :
+
+  /** Copy constructor.
+   *
+   * @param q Quadrature to copy
+   */
+  Quadrature2Din3D(const Quadrature2Din3D& q);
+
+  /** Compute geometric quantities for a cell.
+   *
+   * @param coordinates Section containing vertex coordinates
+   * @param cell Finite-element cell
+   */
+  void _computeGeometry(const ALE::Obj<ALE::Mesh::section_type>& coordinates,
+			const ALE::Mesh::point_type& cell);
+
+// PRIVATE METHODS //////////////////////////////////////////////////////
+private :
+
+  /// Not implemented
+  const Quadrature2Din3D& operator=(const Quadrature2Din3D&);
+
+}; // Quadrature2Din3D
+
+#include "Quadrature2Din3D.icc" // inline methods
+
+#endif // pylith_feassemble_quadrature2din3d_hh
+
+// End of file 
---
+++ libsrc/feassemble/Quadrature3D.hh
@@ -0,0 +1,77 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+/**
+ * @file pylith/feassemble/Quadrature3D.hh
+ *
+ * @brief Quadrature for 3-D finite-elements.
+ */
+
+#if !defined(pylith_feassemble_quadrature3d_hh)
+#define pylith_feassemble_quadrature3d_hh
+
+#include "Quadrature.hh"
+
+namespace pylith {
+  namespace feassemble {
+    class Quadrature3D;
+    class TestQuadrature3D;
+  } // feassemble
+} // pylith
+
+class pylith::feassemble::Quadrature3D : public Quadrature
+{ // Quadrature1D
+  friend class TestQuadrature3D; // unit testing
+
+// PUBLIC MEMBERS ///////////////////////////////////////////////////////
+public :
+
+  /// Constructor
+  Quadrature3D(void);
+
+  /// Destructor
+  virtual ~Quadrature3D(void);
+
+  /// Create a copy of this object.
+  virtual
+  Quadrature* clone(void) const;
+
+// PROTECTED METHODS ////////////////////////////////////////////////////
+protected :
+
+  /** Copy constructor.
+   *
+   * @param q Quadrature to copy
+   */
+  Quadrature3D(const Quadrature3D& q);
+
+  /** Compute geometric quantities for a cell.
+   *
+   * @param coordinates Section containing vertex coordinates
+   * @param cell Finite-element cell
+   */
+  void _computeGeometry(const ALE::Obj<ALE::Mesh::section_type>& coordinates,
+			const ALE::Mesh::point_type& cell);
+
+// PRIVATE METHODS //////////////////////////////////////////////////////
+private :
+
+  /// Not implemented
+  const Quadrature3D& operator=(const Quadrature3D&);
+
+}; // Quadrature3D
+
+#include "Quadrature3D.icc" // inline methods
+
+#endif // pylith_feassemble_quadrature3d_hh
+
+// End of file 
