84df2b664a05c14bf432248ae086d7e57f3abcf0
Brad Aagaard <baagaard@usgs.gov>
2006-10-26 23:37:16 +0000

    Continued implementing integration of interial action.

--- libsrc/feassemble/IntegratorInertia3D.cc
+++ libsrc/feassemble/IntegratorInertia3D.cc
@@ -14,6 +14,8 @@
 
 #include "IntegratorInertia3D.hh" // implementation of class methods
 
+#include "Quadrature.hh" // USES Quadrature
+
 #include "petscmat.h" // USES PetscMat
 
 #include <assert.h> // USES assert()
@@ -40,17 +42,65 @@ pylith::feassemble::IntegratorInertia3D::IntegratorInertia3D(const IntegratorIne
 // ----------------------------------------------------------------------
 // Integrate inertial term for 3-D finite elements.
 void
-pylith::feassemble::IntegratorInertia3D::integrateAction(const ALE::Obj<ALE::Mesh::real_section_type>& fieldOut,
-		 const ALE::Obj<ALE::Mesh::real_section_type>& fieldIn,
-		 const ALE::Obj<ALE::Mesh::real_section_type>& coordinates)
+pylith::feassemble::IntegratorInertia3D::integrateAction(
+			      const ALE::Obj<real_section_type>& fieldOut,
+			      const ALE::Obj<real_section_type>& fieldIn,
+			      const ALE::Obj<real_section_type>& coordinates)
 { // integrateAction
+  assert(0 != _quadrature);
+
+  const topology_type::patch_type patch = 0;
+  const ALE::Obj<topology_type>& topology = fieldIn->getTopology();
+  const ALE::Obj<topology_type::label_sequence>& cells = 
+    topology->heightStratum(patch, 0);
+  const topology_type::label_sequence::iterator cellsEnd = cells->end();
+
+  _initCellVector();
+
+  for (topology_type::label_sequence::iterator cellIter=cells->begin();
+       cellIter != cellsEnd;
+       ++cellIter) {
+    // Compute geometry information for current cell
+    _quadrature->computeGeometry(coordinates, *cellIter);
+
+    // Reset element vector to zero
+    _resetCellVector();
+
+    // Restrict input field to cell
+    const real_section_type::value_type* fieldInCell = 
+      fieldIn->restrict(patch, *cellIter);
+
+    const int numQuadPts = _quadrature->numQuadPts();
+    const double* basis = _quadrature->basis();
+    const double* quadPts = _quadrature->quadPts();
+    const double* quadWts = _quadrature->quadWts();
+    const double* jacobianDet = _quadrature->jacobianDet();
+    const int numBasis = _quadrature->numCorners();
+    const int spaceDim = _quadrature->spaceDim();
+
+    for (int iQuad=0; iQuad < numQuadPts; ++iQuad) {
+      const double wt = quadWts[iQuad]*jacobianDet[iQuad];
+      for (int iBasis=0, iQ=iQuad*numBasis; iBasis < numBasis; ++iBasis) {
+	const int iC = iBasis*spaceDim;
+	double val = wt*basis[iQ+iBasis];
+	for (int jBasis=0; jBasis < numBasis; ++jBasis) {
+	  val *= basis[iQ+jBasis];
+	  for (int iDim=0; iDim < spaceDim; ++iDim)
+	    _cellVector[iC+iDim] += val*fieldInCell[iC+iDim];
+	} // for
+      } // for
+    } // for
+    
+    // Assemble cell contribution into field
+    fieldOut->updateAdd(patch, *cellIter, _cellVector);
+  } // for
 } // integrateAction
 
 // ----------------------------------------------------------------------
 // Compute matrix associated with operator.
 void
 pylith::feassemble::IntegratorInertia3D::integrate(PetscMat* mat,
-		    const ALE::Obj<ALE::Mesh::real_section_type>& coordinates)
+			      const ALE::Obj<real_section_type>& coordinates)
 { // integrate
 } // integrate
 
