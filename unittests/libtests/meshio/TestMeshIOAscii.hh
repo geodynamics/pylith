1d040c0c3e19f29fb19af16b476d3c2316154869
Brad Aagaard <baagaard@usgs.gov>
2006-11-07 04:47:48 +0000

    Started implementing unit tests for MeshIOAscii stuff.

---
+++ unittests/libtests/meshio/TestMeshIOAscii.hh
@@ -0,0 +1,80 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/**
+ * @file unittests/libtests/meshio/TestMeshIOAscii.hh
+ *
+ * @brief C++ TestMeshIOAscii object
+ *
+ * C++ unit testing for MeshIOAscii.
+ */
+
+#if !defined(pylith_meshio_testmeshioascii_hh)
+#define pylith_meshio_testmeshioascii_hh
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/// Namespace for spatialdata package
+namespace pylith {
+  namespace meshio {
+    class TestMeshIOAscii;
+  } // meshio
+} // pylith
+
+/// C++ unit testing for Quadrature1D
+class pylith::meshio::TestMeshIOAscii : public CppUnit::TestFixture
+{ // class TestMeshIOAscii
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestMeshIOAscii );
+  CPPUNIT_TEST( testConstructor );
+  CPPUNIT_TEST( testFilename );
+  CPPUNIT_TEST( testWriteRead1D );
+  CPPUNIT_TEST( testWriteRead1Din2D );
+  CPPUNIT_TEST( testWriteRead1Din3D );
+  CPPUNIT_TEST( testWriteRead2D );
+  CPPUNIT_TEST( testWriteRead2Din3D );
+  CPPUNIT_TEST( testWriteRead3D );
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Test constructor
+  void testConstructor(void);
+
+  /// Test filename()
+  void testFilename(void);
+
+  /// Test write() and read() for 1D mesh in 1D space.
+  void testWriteRead1D(void);
+
+  /// Test write() and read() for 1D mesh in 2D space.
+  void testWriteRead1Din2D(void);
+
+  /// Test write() and read() for 1D mesh in 3D space.
+  void testWriteRead1Din3D(void);
+
+  /// Test write() and read() for 2D mesh in 2D space.
+  void testWriteRead2D(void);
+
+  /// Test write() and read() for 2D mesh in 3D space.
+  void testWriteRead2Din3D(void);
+
+  /// Test write() and read() for 3D mesh in 3D space.
+  void testWriteRead3D(void);
+
+}; // class TestMeshIOAscii
+
+#endif // pylith_meshio_testmeshioascii_hh
+
+// End of file 
