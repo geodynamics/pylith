a5b33df72436af78f76c9c068096fbe84e0ddc1b
Leif Strand <leif>
2007-01-05 01:15:46 +0000

    Transitioned PyLith v0.8 to the egg-based "CIG Edition" of Pythia.

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from archimedes import use_merlin
+use_merlin()
+
+from merlin import setup, find_packages
+
+setup(
+    
+    name = 'PyLith', 
+    version = '0.8.2',
+
+    zip_safe = False,
+    packages = find_packages(),
+    
+    install_requires = [
+    'pythia[mpi] >= 0.8.1.0, < 0.8.2a',
+    #'numpy',
+    #'mercurial',
+    ],
+
+    author = 'Charles A. Williams, Brad Aagaard, and Matt Knepley',
+    author_email = 'cig-short@geodynamics.org',
+    description = """A finite element code for the solution of visco-elastic/plastic deformation that was designed for lithospheric modeling problems.""",
+    license = 'other',
+    url = 'http://www.geodynamics.org/cig/software/packages/short/pylith/',
+
+)
