f55e02d825def240287c34fe32d4076a6218e833
Charles Williams <C.Williams@gns.cri.nz>
2007-01-18 16:41:06 +0000

    Starting the process of revising benchmarks. Old Make.mm and test.sh files have been removed and replaced by runex.py script to run the benchmark.  Time information has been corrected to yield correct viscoelastic solution. Files originally used from bm1a have been copied over to make this a standalone example. The time info needs to be tested to see if it is overkill.

--- pylith3d/examples/linhex/bm2a/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pylith3dapp.py -pl3dscan.fileRoot=linhex/bm1a/bm1a -pl3dscan.bcInputFile=linhex/bm2a/bm2a.bc -pl3dscan.keywordEqualsValueFile=linhex/bm2a/bm2a.keyval -pl3dscan.timeStepInputFile=linhex/bm2a/bm2a.time -pl3dscan.asciiOutputFile=linhex/bm2a/bm2a.ascii -pl3dscan.ucdOutputRoot=linhex/bm2a/bm2a
-
-# end of file
