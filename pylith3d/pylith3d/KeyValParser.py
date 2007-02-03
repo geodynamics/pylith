09fa2f6f2f10e28f052457c1e4eba400f65cfc55
Leif Strand <leif@geodynamics.org>
2007-02-03 03:27:19 +0000

    Added a "keyval codec" so that *.keyval files can be processed by the Pyre framework.  Keyval parameters are now ordinary Inventory items, so they can also be set from the command line or a .cfg file.  Another advantage of this is error reporting: try inserting a bogus key or value into a '.keyval' file.
    I still haven't got the implementation exactly right, however: with
    this change, errors in .cfg files (for example) are reported twice.
    Since the filename of the .keyval file is itself a Pyre property, this
    creates order-of-initialization problems which exceed the limitations
    of the Pyre framework.  I have one or two ideas about how to fix this,
    but they are still in the oven, only half-baked...

---
+++ pylith3d/pylith3d/KeyValParser.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  PyLith by Charles A. Williams, Brad Aagaard, and Matt Knepley
+#
+#  Copyright (c) 2004-2006 Rensselaer Polytechnic Institute
+#
+#  Permission is hereby granted, free of charge, to any person obtaining
+#  a copy of this software and associated documentation files (the
+#  "Software"), to deal in the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
+#
+#  The above copyright notice and this permission notice shall be
+#  included in all copies or substantial portions of the Software.
+#
+#  THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+#  EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+#  MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#  OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
+#  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from pyre.inventory.odb.Registry import Registry
+import pyre.parsing.locators as locators
+
+
+class KeyValParser:
+
+
+    def parse(self, stream):
+        registry = Registry("root")
+        node = registry.getNode("pylith3d")
+        node = node.getNode("scanner")
+        lineNumber = 1
+        for line in stream:
+            locator = locators.file(stream.name, lineNumber)
+            self.parseLine(line, node, locator)
+            lineNumber = lineNumber + 1
+        return registry
+
+
+    def parseLine(self, line, registry, locator):
+        comment = line.find('#')
+        line = line[:comment]
+        if line:
+            split = line.split('=')
+            key = split[0].strip()
+            value = "true"
+            if len(split) > 1:
+                value = '='.join(split[1:]).strip()
+            registry.setProperty(key, value, locator)
+        return
+
+
+# End of file 
