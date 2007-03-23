b35df3cb4deab5a8cdd4f65e94b144ec020e7739
Leif Strand <leif@geodynamics.org>
2007-03-23 21:26:54 +0000

    Cleaning, step 4: Merged GreenFunctionApp with Application. (Removing meaningless Component structure... 90% complete...)

--- pylith3d/pylith3d/Application.py
+++ pylith3d/pylith3d/Application.py
@@ -46,6 +46,76 @@ class Application(PetscApplication):
     from cig.cs.petsc import PetscCommandlineParser as CommandlineParser
 
 
+    def readSamplePoints(self, filename):
+        '''Read in the sampling locations
+        - One point per line, three values per line (x,y,z)
+        - Returns a Numeric array'''
+        import Numeric
+        f = file(filename)
+        points = []
+        for line in f.readlines():
+            points.append([float(v) for v in line.strip().split(' ')])
+        f.close()
+        return Numeric.array(points)
+
+
+    def outputSampleValues(self, filename, values):
+        '''sample# sample values impluse# impulse type'''
+        # Computing normal to the fault:
+        #   Split nodes define the fault
+        #   Get all fault faces for a node
+        #   Area weighted average of normals
+        f = file(filename, 'w')
+        for v, values in enumerate(values):
+            write(f, '%d %g %g %g 1 0' % (v, values[0], values[1], values[2]))
+        f.close()
+        return
+
+
+    def greenFunction(self, points):
+        """
+        # Beginning of loop that loops over split node sets, creating
+        # an 'impulse' for each one and outputting response values.
+        # Below at present is a quasi-C version of the needed code.
+SectionReal splitField;
+
+# Need bindings for this
+ierr = MeshGetSectionPair(mesh, "split", &splitField);
+// Loop over split nodes
+for() {
+  // Loop over elements
+  for() {
+# Need bindings for this
+    ierr = SectionPairSetFiberDimension(splitField, e, 1);
+  }
+# Need bindings for this
+  ierr = SectionPairAllocate(splitField);
+  // Loop over elements
+  for() {
+    PetscPair value;
+
+    value.i = node;
+    value.x = ;
+    value.y = ;
+    value.z = ;
+# Need bindings for this
+    ierr = SectionPairUpdate(splitField, e, &value);
+# Major problem right now:  This just updates PETSc/Sieve's copy of splitField.
+# It does not change the values within PyLith, which have been read from
+# per-process input files.
+  }
+  // Solve
+        pl3drun.solveElastic()
+# Need bindings for this
+  ierr = SectionPairClear(splitField);
+}
+"""
+        scanner = self.inventory.scanner
+        values = scanner.interpolatePoints(points)
+        self.outputSampleValues(scanner.fileRoot+'.output', values)
+        return
+
+
     def main(self, *args, **kwds):
     
 #        from time import clock as now
@@ -53,6 +123,10 @@ class Application(PetscApplication):
         import pylith3d
 
         scanner = self.inventory.scanner
+        green = self.inventory.green
+        
+        if green:
+            points      = self.readSamplePoints(scanner.macroString(scanner.metainventory.sampleLocationFile))
 
         mesh = pylith3d.processMesh(scanner.macroString(scanner.metainventory.bcInputFile),
                                     scanner.macroString(scanner.metainventory.inputFileRoot),
@@ -71,7 +145,10 @@ class Application(PetscApplication):
         scanner.allocateremaining()
         scanner.meshwrite()
 
-        scanner.run()
+        if green:
+            self.greenFunction(points)
+        else:
+            scanner.run()
 #        finish = now()
 #        usertime = finish - start
 #        print "Total user time:  %g" % usertime
@@ -83,6 +160,8 @@ class Application(PetscApplication):
         import pyre.inventory
         from cig.cs.petsc import PetscProperty
         from Pylith3d_scan import Pylith3d_scan
+
+        green = pyre.inventory.bool("green")
 
         scanner = pyre.inventory.facility("scanner", factory=Pylith3d_scan)
 
