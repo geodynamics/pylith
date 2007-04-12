736722f8c29a37f5a838de69ce1fd638d1feed86
Brad Aagaard <brad>
2007-04-12 04:44:05 +0000

    Added tests for ElasticStrain1D and ElasticPlaneStrain materials.

---
+++ unittests/libtests/materials/TestElasticPlaneStrain.cc
@@ -0,0 +1,85 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestElasticPlaneStrain.hh" // Implementation of class methods
+
+#include "data/ElasticPlaneStrainData.hh" // USES ElasticPlaneStrainData
+
+#include "pylith/materials/ElasticPlaneStrain.hh" // USES ElasticPlaneStrain
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::materials::TestElasticPlaneStrain );
+
+// ----------------------------------------------------------------------
+// Test DBValues()
+void
+pylith::materials::TestElasticPlaneStrain::testDBValues(void)
+{ // testDBValues
+  ElasticPlaneStrain material;
+  ElasticPlaneStrainData data;
+  _testDBValues(&material, data);
+} // testDBValues
+
+// ----------------------------------------------------------------------
+// Test parameters()
+void
+pylith::materials::TestElasticPlaneStrain::testParameters(void)
+{ // testParameters
+  ElasticPlaneStrain material;
+  ElasticPlaneStrainData data;
+  _testParameters(&material, data);
+} // testParameters
+
+// ----------------------------------------------------------------------
+// Test _dbToParameters()
+void
+pylith::materials::TestElasticPlaneStrain::testDBToParameters(void)
+{ // testDBToParameters
+  ElasticPlaneStrain material;
+  ElasticPlaneStrainData data;
+  _testDBToParameters(&material, data);
+} // testDBToParameters
+
+// ----------------------------------------------------------------------
+// Test calcDensity()
+void
+pylith::materials::TestElasticPlaneStrain::testCalcDensity(void)
+{ // testCalcDensity
+  ElasticPlaneStrain material;
+  ElasticPlaneStrainData data;
+  _testCalcDensity(&material, data);
+} // testCalcDensity
+
+// ----------------------------------------------------------------------
+// Test calcStress()
+void
+pylith::materials::TestElasticPlaneStrain::testCalcStress(void)
+{ // testCalcStress
+  ElasticPlaneStrain material;
+  ElasticPlaneStrainData data;
+  _testCalcStress(&material, data);
+} // testCalcStress
+
+// ----------------------------------------------------------------------
+// Test calcElasticConsts()
+void
+pylith::materials::TestElasticPlaneStrain::testCalcElasticConsts(void)
+{ // testElasticConsts
+  ElasticPlaneStrain material;
+  ElasticPlaneStrainData data;
+  _testCalcElasticConsts(&material, data);
+} // testElasticConsts
+
+
+// End of file 
---
+++ unittests/libtests/materials/TestElasticPlaneStrain.hh
@@ -0,0 +1,75 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/**
+ * @file unittests/libtests/materials/TestElasticPlaneStrain.hh
+ *
+ * @brief C++ TestElasticPlaneStrain object
+ *
+ * C++ unit testing for ElasticPlaneStrain.
+ */
+
+#if !defined(pylith_materials_testelasticplanestrain_hh)
+#define pylith_materials_testelasticplanestrain_hh
+
+#include "TestElasticMaterial.hh"
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace materials {
+    class ElasticPlaneStrain;
+    class TestElasticPlaneStrain;
+    class ElasticPlaneStrainData;
+  } // materials
+} // pylith
+
+/// C++ unit testing for ElasticPlaneStrain
+class pylith::materials::TestElasticPlaneStrain : public TestElasticMaterial
+{ // class TestElasticPlaneStrain
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestElasticPlaneStrain );
+  CPPUNIT_TEST( testDBToParameters );
+  CPPUNIT_TEST( testDBValues );
+  CPPUNIT_TEST( testParameters );
+  CPPUNIT_TEST( testCalcDensity );
+  CPPUNIT_TEST( testCalcStress );
+  CPPUNIT_TEST( testCalcElasticConsts );
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Test DBValues()
+  void testDBValues(void);
+
+  /// Test parameters()
+  void testParameters(void);
+
+  /// Test _dbToParameters()
+  void testDBToParameters(void);
+
+  /// Test calcDensity()
+  void testCalcDensity(void);
+
+  /// Test calcStress()
+  void testCalcStress(void);
+
+  /// Test calcElasticConsts()
+  void testCalcElasticConsts(void);
+
+}; // class TestElasticPlaneStrain
+
+#endif // pylith_materials_testelasticplanestrain_hh
+
+
+// End of file 
