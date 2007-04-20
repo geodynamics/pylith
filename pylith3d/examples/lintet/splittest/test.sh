a90529af4839ee454a3178c09d44056ca71654ef
Charles Williams <C.Williams@gns.cri.nz>
2007-04-20 20:24:07 +0000

    Deleted old shell scripts and added a couple of new .cfg files.

--- pylith3d/examples/lintet/splittest/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pylith3dapp.py -typos=relaxed -pl3dscan.fileRoot=lintet/splittest/splittest -pl3dscan.ucdOutput=binary -log_summary -pc_type ilu -ksp_monitor -ksp_view -ksp_rtol 1e-9
-
-# end of file
