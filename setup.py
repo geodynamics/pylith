49b98b883f629b1a29cee0e43fee2bd0311ac13d
Leif Strand <leif>
2007-05-04 03:00:59 +0000

    Updated 'setup.py'.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages = find_packages("pylith3d"),
     
     install_requires = [
-    'pythia[mpi] >= 0.8.1.3b5, < 0.8.2a',
+    'pythia[mpi] >= 0.8.1.3, < 0.8.2a',
     ],
 
     author = 'Charles A. Williams, Brad Aagaard, and Matt Knepley',
@@ -22,10 +22,5 @@ setup(
     description = """A finite element code for the solution of visco-elastic/plastic deformation that was designed for lithospheric modeling problems.""",
     license = 'other',
     url = 'http://www.geodynamics.org/cig/software/packages/short/pylith/',
-
-    # temporary
-    dependency_links = [
-    'http://crust.geodynamics.org/~leif/shipping/',
-    ]
 
 )
