29755eb10dfa73a1a129a84763890e543c382b21
Brad Aagaard <brad>
2007-05-11 04:45:20 +0000

    Started work on C++ code for SolutionIO.

---
+++ libsrc/meshio/SolutionIO.cc
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+#include <portinfo>
+
+#include "SolutionIO.hh" // implementation of class methods
+
+// ----------------------------------------------------------------------
+// Constructor
+pylith::meshio::SolutionIO::SolutionIO(void)
+{ // constructor
+} // constructor
+
+// ----------------------------------------------------------------------
+// Destructor
+pylith::meshio::SolutionIO::~SolutionIO(void)
+{ // destructor
+} // destructor  
+
+
+// End of file 
