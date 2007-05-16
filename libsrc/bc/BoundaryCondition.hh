44ad6465875cde4938ec527ada7b9b4fa5f6115b
Brad Aagaard <baagaard@usgs.gov>
2007-05-16 22:09:21 +0000

    Worked on implementing Dirichlet boundary conditions.

---
+++ libsrc/bc/BoundaryCondition.hh
@@ -0,0 +1,168 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/** @file libsrc/bc/BoundaryCondition.hh
+ *
+ * @brief C++ abstract base class for BoundaryCondition object.
+ *
+ * Interface definition for boundary conditions.
+ */
+
+#if !defined(pylith_bc_boundarycondition_hh)
+#define pylith_bc_boundarycondition_hh
+
+#include "pylith/utils/sievetypes.hh" // USES PETSc Mesh, real_section_type
+#include "pylith/utils/petscfwd.h" // USES PETScMat
+
+#include <string> // HASA std::string
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace bc {
+    class BoundaryCondition;
+    class TestBoundaryCondition; // unit testing
+  } // bc
+} // pylith
+
+/// Namespace for spatialdata package
+namespace spatialdata {
+  namespace geocoords {
+    class CoordSys;
+  } // geocoords
+
+  namespace spatialdb {
+    class SpatialDB;
+  } // spatialdb
+} // spatialdata
+
+/// C++ abstract base class for BoundaryCondition object.
+class pylith::bc::BoundaryCondition
+{ // class BoundaryCondition
+  friend class TestBoundaryCondition; // unit testing
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Default constructor.
+  BoundaryCondition(void);
+
+  /// Destructor.
+  virtual
+  ~BoundaryCondition(void);
+
+  /** Set identifier of fault.
+   *
+   * @param value BoundaryCondition identifier
+   */
+  void id(const int value);
+
+  /** Get identifier of fault.
+   *
+   * @returns BoundaryCondition identifier
+   */
+  int id(void) const;
+
+  /** Set label of fault.
+   *
+   * @param value Label of fault
+   */
+  void label(const char* value);
+
+  /** Get label of fault.
+   *
+   * @returns Label of fault
+   */
+  const std::string& label(void) const;
+
+  /** Set database for boundary condition parameters.
+   *
+   * @param db Spatial database
+   */
+  void db(spatialdata::spatialdb::SpatialDB* const db);
+
+  /** Initialize boundary condition.
+   *
+   * @param mesh PETSc mesh
+   * @param cs Coordinate system for mesh
+   */
+  virtual
+  void initialize(const ALE::Obj<ALE::Mesh>& mesh,
+		  const spatialdata::geocoords::CoordSys* cs) = 0;
+
+  /** Set constrained degrees of freedom in field.
+   *
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void setConstraints(const ALE::Obj<real_section_type>& field,
+		      const ALE::Obj<ALE::Mesh>& mesh);
+
+  /** Set constrained degrees of freedom in field.
+   *
+   * @param jacobian Matrix for Jacobian of system
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void integrateJacobian(PetscMat* jacobian,
+			 const ALE::Obj<real_section_type>& field,
+			 const ALE::Obj<ALE::Mesh>& mesh);
+
+  /** Set constrained degrees of freedom in field.
+   *
+   * @param residual Residual field
+   * @param fieldT Solution field at time t
+   * @param fieldTmdt Solution field at time t-dt
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void integrateResidual(const ALE::Obj<real_section_type>& residual,
+			 const ALE::Obj<real_section_type>& fieldT,
+			 const ALE::Obj<real_section_type>& fieldTmdt,
+			 const ALE::Obj<ALE::Mesh>& mesh);
+
+  /** Set values in field.
+   *
+   * @param t Current time
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void setField(const double t,
+		const ALE::Obj<real_section_type>& field,
+		const ALE::Obj<ALE::Mesh>& mesh);
+
+  // NOT IMPLEMENTED ////////////////////////////////////////////////////
+private :
+
+  /// Not implemented
+  BoundaryCondition(const BoundaryCondition& m);
+
+  /// Not implemented
+  const BoundaryCondition& operator=(const BoundaryCondition& m);
+
+  // PROTECTED MEMBERS //////////////////////////////////////////////////
+protected :
+
+  int _id; ///< BoundaryCondition identifier
+  std::string _label; ///< Label of fault
+  spatialdata::spatialdb::SpatialDB* _db; ///< Spatial database w/parameters
+
+}; // class BoundaryCondition
+
+#include "BoundaryCondition.icc" // inline methods
+
+#endif // pylith_bc_boundarycondition_hh
+
+
+// End of file 
---
+++ libsrc/bc/Dirichlet.hh
@@ -0,0 +1,109 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/** @file libsrc/bc/Dirichlet.hh
+ *
+ * @brief C++ implementation of Dirichlet (prescribed values at
+ * degrees of freedom) boundary conditions.
+ */
+
+#if !defined(pylith_bc_dirichlet_hh)
+#define pylith_bc_dirichlet_hh
+
+#include "BoundaryCondition.hh" // ISA BoundaryCondition
+
+#include "pylith/utils/array.hh" // USES std::vector, double_array, int_array
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace bc {
+    class Dirichlet;
+    class TestDirichlet; // unit testing
+  } // bc
+} // pylith
+
+
+/// C++ implementation of Dirichlet boundary conditions.
+class pylith::bc::Dirichlet : public BoundaryCondition
+{ // class Dirichlet
+  friend class TestDirichlet; // unit testing
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Default constructor.
+  Dirichlet(void);
+
+  /// Destructor.
+  ~Dirichlet(void);
+
+  /** Set indices of fixed degrees of freedom. 
+   *
+   * Note: all points associated with boundary condition has same
+   * degrees of freedom fixed.
+   *
+   * Example: [0, 1] to fix x and y degrees of freedom in Cartesian system.
+   *
+   * @param flags Indices of fixed degrees of freedom.
+   */
+  void fixedDOF(const int_array& flags);
+
+  /** Initialize boundary condition.
+   *
+   * @param mesh PETSc mesh
+   * @param cs Coordinate system for mesh
+   */
+  void initialize(const ALE::Obj<ALE::Mesh>& mesh,
+		  const spatialdata::geocoords::CoordSys* cs);
+
+  /** Set constrained degrees of freedom in field.
+   *
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  void setConstraints(const ALE::Obj<real_section_type>& field,
+		      const ALE::Obj<ALE::Mesh>& mesh);
+
+  /** Set values in field.
+   *
+   * @param t Current time
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  void setField(const double t,
+		const ALE::Obj<real_section_type>& field,
+		const ALE::Obj<ALE::Mesh>& mesh);
+
+  // NOT IMPLEMENTED ////////////////////////////////////////////////////
+private :
+
+  /// Not implemented
+  Dirichlet(const Dirichlet& m);
+
+  /// Not implemented
+  const Dirichlet& operator=(const Dirichlet& m);
+
+  // PRIVATE MEMBERS ////////////////////////////////////////////////////
+private :
+
+  std::vector<Mesh::point_type> _points; ///< Locations of boundary condition
+  double_array _values; ///< Values at degrees of freedom
+  int_array _fixedDOF; ///< Indices of fixed degrees of freedom
+
+}; // class Dirichlet
+
+#include "Dirichlet.icc" // inline methods
+
+#endif // pylith_bc_fault_hh
+
+
+// End of file 
