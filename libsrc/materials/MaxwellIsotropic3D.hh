c323542cc91f081219934fec910bc09483dddd2a
Charles Williams <willic3>
2007-05-22 19:11:06 +0000

    Started work on libraries for Maxwell viscoelastic material.

---
+++ libsrc/materials/MaxwellIsotropic3D.hh
@@ -0,0 +1,165 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/** @file libsrc/materials/ElasticIsotropic3D.h
+ *
+ * @brief C++ ElasticIsotropic3D object
+ *
+ * 3-D, isotropic, linear elastic material. The physical properties
+ * are specified using density, shear-wave speed, and
+ * compressional-wave speed. The physical properties are stored
+ * internally using density, lambda, and mu, which are directly
+ * related to the elasticity constants used in the finite-element
+ * integration.
+ */
+
+#if !defined(pylith_materials_elasticisotropic3d_hh)
+#define pylith_materials_elasticisotropic3d_hh
+
+#include "ElasticMaterial.hh"
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace materials {
+    class ElasticIsotropic3D;
+    class TestElasticIsotropic3D; // unit testing
+  } // materials
+} // pylith
+
+/// 3-D, isotropic, linear elastic material.
+class pylith::materials::ElasticIsotropic3D : public ElasticMaterial
+{ // class ElasticIsotropic3D
+  friend class TestElasticIsotropic3D; // unit testing
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Default constructor
+  ElasticIsotropic3D(void);
+
+  /// Destructor
+  ~ElasticIsotropic3D(void);
+
+  /** Create a pointer to a copy of this.
+   *
+   * @returns Pointer to copy
+   */
+  ElasticMaterial* clone(void) const;
+
+  // PROTECTED METHODS //////////////////////////////////////////////////
+protected :
+
+  /** Copy constructor
+   *
+   * @param m Material to copy
+   */
+  ElasticIsotropic3D(const ElasticIsotropic3D& m);
+
+  /** Get names of values expected to be in database of parameters for
+   *  physical properties.
+   *
+   * @returns Names of values
+   */
+  const char** _dbValues(void) const;
+
+  /** Get number of values expected to be in database of parameters for
+   *  physical properties.
+   *
+   * @returns Number of values
+   */
+  int _numDBValues(void) const;
+
+  /** Get names of parameters for physical properties.
+   *
+   * @returns Names of parameters
+   */
+  const char** _parameterNames(void) const;
+
+  /** Get number of parameters for physical properties.
+   *
+   * @returns Number of parameters
+   */
+  int _numParameters(void) const;
+
+  /** Compute parameters from values in spatial database.
+   *
+   * Order of values in arrays matches order used in dbValues() and
+   * parameterNames().
+   *
+   * @param paramVals Array of parameters
+   * @param dbValues Array of database values
+   */
+  void _dbToParameters(double_array* paramVals,
+		       const double_array& dbValues) const;
+
+  /** Get number of entries in stress/strain tensors.
+   *
+   * 1-D = 1
+   * 2-D = 3
+   * 3-D = 6
+   *
+   * @returns Number of entries in stress/strain tensors.
+   */
+  int _tensorSize(void) const;
+
+  /** Get number of entries in derivative of elasticity matrix.
+   *
+   * 1-D = 1
+   * 2-D = 6
+   * 3-D = 21
+   *
+   * @returns Number of entries in derivative of elasticity matrix.
+   */
+  int _numElasticConsts(void) const;
+
+  /** Compute density from parameters.
+   *
+   * @param density Array for density
+   * @param parameters Parameters at location
+   */
+  void _calcDensity(double_array* const density,
+		    const double_array& parameters);
+
+  /** Compute stress tensor from parameters.
+   *
+   * @param stress Array for stress tensor
+   * @param parameters Parameters at locations.
+   * @param totalStrain Total strain at locations.
+   */
+  void _calcStress(double_array* const stress,
+		   const double_array& parameters,
+		   const double_array& totalStrain);
+
+  /** Compute derivatives of elasticity matrix from parameters.
+   *
+   * @param elasticConsts Array for elastic constants
+   * @param parameters Parameters at locations.
+   * @param totalStrain Total strain at locations.
+   */
+  void _calcElasticConsts(double_array* const elasticConsts,
+			  const double_array& parameters,
+			  const double_array& totalStrain);
+
+  // NOT IMPLEMENTED ////////////////////////////////////////////////////
+private :
+
+  /// Not implemented
+  const ElasticIsotropic3D& operator=(const ElasticIsotropic3D& m);
+
+}; // class ElasticIsotropic3D
+
+#include "ElasticIsotropic3D.icc" // inline methods
+
+#endif // pylith_materials_elasticisotropic3d_hh
+
+
+// End of file 
