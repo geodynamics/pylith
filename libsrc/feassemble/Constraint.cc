2e1f578462dffa9606dee7c8125e1868a1ebd8af
Brad Aagaard <baagaard@usgs.gov>
2007-05-25 02:56:48 +0000

    Added Constraint.

---
+++ libsrc/feassemble/Constraint.cc
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "Constraint.hh" // implementation of object methods
+
+// ----------------------------------------------------------------------
+// Default constructor.
+pylith::feassemble::Constraint::Constraint(void)
+{ // constructor
+} // constructor
+
+// ----------------------------------------------------------------------
+// Destructor.
+pylith::feassemble::Constraint::~Constraint(void)
+{ // destructor
+} // destructor
+
+
+// End of file 
---
+++ libsrc/feassemble/Constraint.hh
@@ -0,0 +1,92 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/** @file libsrc/feassemble/Constraint.hh
+ *
+ * @brief C++ abstract base class defining interface for constraints
+ * applied to finite-elements.
+ */
+
+#if !defined(pylith_feassemble_constraint_hh)
+#define pylith_feassemble_constraint_hh
+
+#include "pylith/utils/sievetypes.hh" // USES real_section_type
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace feassemble {
+    class Constraint;
+    class TestConstraint; // unit testing
+  } // feassemble
+} // pylith
+
+
+/// C++ abstract base class defining interface for constraints applied
+/// to finite-elements.
+class pylith::feassemble::Constraint
+{ // class Constraint
+  friend class TestConstraint; // unit testing
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Default constructor.
+  Constraint(void);
+
+  /// Destructor.
+  virtual
+  ~Constraint(void);
+
+  /** Set number of degrees of freedom that are constrained at points in field.
+   *
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void setConstraintSizes(const ALE::Obj<real_section_type>& field,
+			  const ALE::Obj<ALE::Mesh>& mesh) = 0;
+
+  /** Set which degrees of freedom are constrained at points in field.
+   *
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void setConstraints(const ALE::Obj<real_section_type>& field,
+		      const ALE::Obj<ALE::Mesh>& mesh) = 0;
+
+  /** Set values in field.
+   *
+   * @param t Current time
+   * @param field Solution field
+   * @param mesh PETSc mesh
+   */
+  virtual
+  void setField(const double t,
+		const ALE::Obj<real_section_type>& field,
+		const ALE::Obj<ALE::Mesh>& mesh) = 0;
+
+  // NOT IMPLEMENTED ////////////////////////////////////////////////////
+private :
+
+  /// Not implemented
+  Constraint(const Constraint& m);
+
+  /// Not implemented
+  const Constraint& operator=(const Constraint& m);
+
+}; // class Constraint
+
+#endif // pylith_feassemble_constraint_hh
+
+
+// End of file 
