feb18f7e2e3f9e7b0e4f42de5b0253eee60f1087
Brad Aagaard <brad>
2007-06-02 22:22:32 +0000

    Changed Dirichlet BC to allow case with no constrained DOF. Worked on setting up 2-D axial compression test with tri3 cells.

---
+++ tests/2d/tri3/TestAxialPlaneStrain.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# ----------------------------------------------------------------------
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# <LicenseText>
+#
+# ----------------------------------------------------------------------
+#
+
+## @file tests/TestAxialPlaneStrain.py
+##
+## @brief Test suite for testing pylith with axial extension in
+## y-direction for 2-D box.
+
+import unittest
+import numpy
+import tables
+
+def run_pylith():
+  """
+  Run pylith.
+  """
+  if not "done" in dir(run_pylith):
+    from pylith.PyLithApp import PyLithApp
+    app = PyLithApp("axialplanestrain")
+    app.run()
+    run_pylith.done = True
+  return
+
+
+class TestAxialPlaneStrain(unittest.TestCase):
+  """
+  Test suite for testing pylith with axial extension in y-direction
+  for 2-D box.
+  """
+
+  def setUp(self):
+    """
+    Setup for test.
+    """
+    run_pylith()
+    return
+
+
+  def test_disp(self):
+    """
+    Check displacement field.
+    """
+    return
+
+
+# End of file 
