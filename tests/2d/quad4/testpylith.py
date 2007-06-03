476d6cbd61b79e2d15ebe1d6b6a0d90511ef65f0
Brad Aagaard <brad>
2007-06-03 03:02:01 +0000

    Fixed bugs in FIATLagrange.validateDimension(). Worked on setting up 2-D axial compression test with quad4 cells.

---
+++ tests/2d/quad4/testpylith.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# ======================================================================
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# {LicenseText}
+#
+# ======================================================================
+#
+
+__requires__ = "PyLith"
+
+import unittest
+
+def suite():
+  """
+  Create test suite.
+  """
+  suite = unittest.TestSuite()
+
+  from TestAxialPlaneStrain import TestAxialPlaneStrain
+  suite.addTest(unittest.makeSuite(TestAxialPlaneStrain))
+
+  return suite
+
+
+def main():
+  """
+  Run test suite.
+  """
+  unittest.TextTestRunner(verbosity=2).run(suite())
+  return
+
+
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
+
+  
+# End of file 
