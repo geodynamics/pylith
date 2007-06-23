004092d96c456fae568e4f1620349f24c4a97525
Brad Aagaard <baagaard@usgs.gov>
2007-06-23 02:00:27 +0000

    Added some assert() calls for error checking.

--- libsrc/faults/FaultCohesive.cc
+++ libsrc/faults/FaultCohesive.cc
@@ -123,6 +123,7 @@ pylith::faults::FaultCohesive::_orient3D(double_array* orientation,
   double r2 =  j00*j11 - j10*j01;
   // Make unit vector
   double mag = sqrt(r0*r0 + r1*r1 + r2*r2);
+  assert(mag > 0.0);
   r0 /= mag;
   r1 /= mag;
   r2 /= mag;
@@ -133,6 +134,7 @@ pylith::faults::FaultCohesive::_orient3D(double_array* orientation,
   double p2 =  upDir[0]*r1 - upDir[1]*r0;
   // Make unit vector
   mag = sqrt(p0*p0 + p1*p1 + p2*p2);
+  assert(mag > 0.0);
   p0 /= mag;
   p1 /= mag;
   p2 /= mag;
@@ -141,6 +143,8 @@ pylith::faults::FaultCohesive::_orient3D(double_array* orientation,
   const double q0 =  r1*p2 - r2*p1;
   const double q1 = -r0*p2 + r2*p0;
   const double q2 =  r0*p1 - r1*p0;
+  mag = sqrt(q0*q0 + q1*q1 + q2*q2);
+  assert(mag > 0.0);
   
   const double wt = jacobianDet;
   (*orientation)[0] =  p0*wt;
