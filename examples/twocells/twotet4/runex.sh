acf0faea941a4fffd18c9eb85faf0220033c09b8
Charles Williams <willic3>
2007-06-28 18:05:28 +0000

    Removed examples that don't exist in twotet4.

--- examples/twocells/twotet4/runex.sh
+++ examples/twocells/twotet4/runex.sh
@@ -7,7 +7,3 @@ pylith dislocation.cfg 2>&1 | tee dislocation-np1.log
 mv dislocation_t0.vtk dislocation_t0.np1.vtk
 pylith dislocation.cfg --nodes=2 2>&1 | tee dislocation-np2.log
 mv dislocation_t0.vtk dislocation_t0.np2.vtk
-pylith sheardisp.cfg 2>&1 | tee sheardisp-np1.log
-mv sheardisp_t0.vtk sheardisp_t0.np1.vtk
-pylith sheardisp.cfg --nodes=2 2>&1 | tee sheardisp-np2.log
-mv sheardisp_t0.vtk sheardisp_t0.np2.vtk
