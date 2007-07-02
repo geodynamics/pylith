99e4544c304405d22066cb0fbe9f19e2ead2def4
Brad Aagaard <brad>
2007-07-02 17:35:29 +0000

    Removed explicit setting of PYTHON_EGG_PYXFLAGS; configure will set if using latest merlin.

--- examples/twocells/twotet4/runex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pylith axialdisp.cfg 2>&1 | tee axialdisp-np1.log
-mv axialdisp_t0.vtk axialdisp_t0.np1.vtk
-pylith axialdisp.cfg --nodes=2 2>&1 | tee axialdisp-np2.log
-mv axialdisp_t0.vtk axialdisp_t0.np2.vtk
-pylith dislocation.cfg 2>&1 | tee dislocation-np1.log
-mv dislocation_t0.vtk dislocation_t0.np1.vtk
-pylith dislocation.cfg --nodes=2 2>&1 | tee dislocation-np2.log
-mv dislocation_t0.vtk dislocation_t0.np2.vtk
