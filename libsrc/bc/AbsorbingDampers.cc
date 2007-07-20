55d58e70146b62af53ce8b0068ba48a5cd54f13b
Brad Aagaard <baagaard@usgs.gov>
2007-07-20 19:07:36 +0000

    Finished initial implementation of AbsorbingDampers. Need unit tests and bindings.

--- libsrc/bc/AbsorbingDampers.cc
+++ libsrc/bc/AbsorbingDampers.cc
@@ -16,6 +16,7 @@
 
 #include "pylith/feassemble/Quadrature.hh" // USES Quadrature
 #include "pylith/feassemble/CellGeometry.hh" // USES CellGeometry
+#include "pylith/topology/FieldsManager.hh" // USES FieldsManager
 
 #include "spatialdata/spatialdb/SpatialDB.hh" // USES SpatialDB
 #include "spatialdata/geocoords/CoordSys.hh" // USES CoordSys
@@ -56,9 +57,9 @@ pylith::bc::AbsorbingDampers::initialize(const ALE::Obj<ALE::Mesh>& mesh,
 			     "a vector with 3 components.");
 
   // Extract submesh associated with boundary
-  const ALE::Obj<ALE::Mesh> boundaryMesh =
+  _boundaryMesh = 
     ALE::Selection<ALE::Mesh>::submesh(mesh, mesh->getIntSection(_label));
-  if (boundaryMesh.isNull()) {
+  if (_boundaryMesh.isNull()) {
     std::ostringstream msg;
     msg << "Could not construct boundary mesh for absorbing boundary "
 	<< "condition '" << _label << "'.";
@@ -66,11 +67,11 @@ pylith::bc::AbsorbingDampers::initialize(const ALE::Obj<ALE::Mesh>& mesh,
   } // if
 
   // check compatibility of quadrature and boundary mesh
-  if (_quadrature->cellDim() != boundaryMesh->getDimension()) {
+  if (_quadrature->cellDim() != _boundaryMesh->getDimension()) {
     std::ostringstream msg;
     msg << "Quadrature is incompatible with cells for absorbing boundary "
 	<< "condition '" << _label << "'.\n"
-	<< "Dimension of boundary mesh: " << boundaryMesh->getDimension()
+	<< "Dimension of boundary mesh: " << _boundaryMesh->getDimension()
 	<< ", dimension of quadrature: " << _quadrature->cellDim()
 	<< ".";
     throw std::runtime_error(msg.str());
@@ -79,7 +80,7 @@ pylith::bc::AbsorbingDampers::initialize(const ALE::Obj<ALE::Mesh>& mesh,
 
   // Get 'surface' cells (1 dimension lower than top-level cells)
   const ALE::Obj<ALE::Mesh::label_sequence>& cells = 
-    boundaryMesh->heightStratum(1);
+    _boundaryMesh->heightStratum(1);
   assert(!cells.isNull());
   const Mesh::label_sequence::iterator cellsBegin = cells->begin();
   const Mesh::label_sequence::iterator cellsEnd = cells->end();
@@ -148,7 +149,7 @@ pylith::bc::AbsorbingDampers::initialize(const ALE::Obj<ALE::Mesh>& mesh,
   for(Mesh::label_sequence::iterator c_iter = cells->begin();
       c_iter != cells->end();
       ++c_iter) {
-    _quadrature->computeGeometry(mesh, coordinates, *c_iter);
+    _quadrature->computeGeometry(_boundaryMesh, coordinates, *c_iter);
     const double_array& quadPts = _quadrature->quadPts();
     const double_array& quadPtsRef = _quadrature->quadPtsRef();
 
@@ -193,22 +194,194 @@ pylith::bc::AbsorbingDampers::initialize(const ALE::Obj<ALE::Mesh>& mesh,
 // Integrate contributions to residual term (r) for operator.
 void
 pylith::bc::AbsorbingDampers::integrateResidual(
-				  const ALE::Obj<real_section_type>& residual,
-				  const ALE::Obj<Mesh>& mesh)
+				 const ALE::Obj<real_section_type>& residual,
+				 const double t,
+				 topology::FieldsManager* const fields,
+				 const ALE::Obj<Mesh>& mesh)
 { // integrateResidual
-  throw std::logic_error("Not implemented.");
+  assert(0 != _quadrature);
+  assert(!_boundaryMesh.isNull());
+  assert(!residual.isNull());
+  assert(0 != fields);
+  assert(!mesh.isNull());
+
+  PetscErrorCode err = 0;
+
+  // Get cell information
+  const ALE::Obj<ALE::Mesh::label_sequence>& cells = 
+    _boundaryMesh->heightStratum(1);
+  assert(!cells.isNull());
+  const Mesh::label_sequence::iterator cellsEnd = cells->end();
+
+  // Get sections
+  const ALE::Obj<real_section_type>& coordinates = 
+    mesh->getRealSection("coordinates");
+  assert(!coordinates.isNull());
+  const ALE::Obj<real_section_type>& dispTpdt = fields->getHistoryItem(0);
+  const ALE::Obj<real_section_type>& dispTmdt = fields->getHistoryItem(2);
+  assert(!dispTpdt.isNull());
+  assert(!dispTmdt.isNull());
+
+  // Get parameters used in integration.
+  const double dt = _dt;
+  assert(dt > 0);
+
+  // Get cell geometry information that doesn't depend on cell
+  const int numQuadPts = _quadrature->numQuadPts();
+  const double_array& quadWts = _quadrature->quadWts();
+  assert(quadWts.size() == numQuadPts);
+  const int numBasis = _quadrature->numBasis();
+  const int spaceDim = _quadrature->spaceDim();
+  const int cellDim = _quadrature->cellDim();
+
+  // Allocate vectors for cell values.
+  _initCellVector();
+  const int cellVecSize = numBasis*spaceDim;
+  double_array dispTpdtCell(cellVecSize);
+  double_array dispTmdtCell(cellVecSize);
+  double_array dampingConstsCell(numQuadPts*spaceDim);
+
+  for (Mesh::label_sequence::iterator c_iter=cells->begin();
+       c_iter != cellsEnd;
+       ++c_iter) {
+    // Compute geometry information for current cell
+    _quadrature->computeGeometry(_boundaryMesh, coordinates, *c_iter);
+
+    // Reset element vector to zero
+    _resetCellVector();
+
+    // Restrict input fields to cell
+    _boundaryMesh->restrict(dispTpdt, *c_iter, &dispTpdtCell[0], cellVecSize);
+    _boundaryMesh->restrict(dispTmdt, *c_iter, &dispTmdtCell[0], cellVecSize);
+    _boundaryMesh->restrict(_dampingConsts, *c_iter, 
+			    &dampingConstsCell[0], dampingConstsCell.size());
+
+    // Get cell geometry information that depends on cell
+    const double_array& basis = _quadrature->basis();
+    const double_array& jacobianDet = _quadrature->jacobianDet();
+
+    // Compute action for absorbing bc terms
+    for (int iQuad=0; iQuad < numQuadPts; ++iQuad) {
+      const double wt = 
+	quadWts[iQuad] * jacobianDet[iQuad] / (2.0 * dt);
+      for (int iBasis=0; iBasis < numBasis; ++iBasis) {
+        const double valI = wt*basis[iQuad*numBasis+iBasis];
+        for (int jBasis=0; jBasis < numBasis; ++jBasis) {
+          const double valIJ = valI * basis[iQuad*numBasis+jBasis];
+          for (int iDim=0; iDim < spaceDim; ++iDim)
+            _cellVector[iBasis*spaceDim+iDim] += 
+	      dampingConstsCell[iQuad*spaceDim+iDim] *
+	      valIJ * (- dispTpdtCell[jBasis*spaceDim+iDim] 
+		       + dispTmdtCell[jBasis*spaceDim+iDim]);
+        } // for
+      } // for
+    } // for
+    err = PetscLogFlops(numQuadPts*(3+numBasis*(1+numBasis*(5*spaceDim))));
+    if (err)
+      throw std::runtime_error("Logging PETSc flops failed.");
+
+    // Assemble cell contribution into field
+    mesh->updateAdd(residual, *c_iter, _cellVector);
+  } // for
 } // integrateResidual
 
 // ----------------------------------------------------------------------
 // Integrate contributions to Jacobian matrix (A) associated with
 void
 pylith::bc::AbsorbingDampers::integrateJacobian(
-					 PetscMat* mat,
+					 PetscMat* jacobian,
 					 const double t,
 					 topology::FieldsManager* const fields,
 					 const ALE::Obj<Mesh>& mesh)
 { // integrateJacobian
-  throw std::logic_error("Not implemented.");
+  assert(0 != _quadrature);
+  assert(!_boundaryMesh.isNull());
+  assert(0 != jacobian);
+  assert(0 != fields);
+  assert(!mesh.isNull());
+
+  PetscErrorCode err = 0;
+
+  // Get cell information
+  const ALE::Obj<ALE::Mesh::label_sequence>& cells = 
+    _boundaryMesh->heightStratum(1);
+  assert(!cells.isNull());
+  const Mesh::label_sequence::iterator cellsEnd = cells->end();
+
+  // Get sections
+  const ALE::Obj<real_section_type>& coordinates = 
+    mesh->getRealSection("coordinates");
+  assert(!coordinates.isNull());
+  const ALE::Obj<real_section_type>& dispT = fields->getHistoryItem(1);
+  assert(!dispT.isNull());
+
+  // Get parameters used in integration.
+  const double dt = _dt;
+  assert(dt > 0);
+
+  // Get cell geometry information that doesn't depend on cell
+  const int numQuadPts = _quadrature->numQuadPts();
+  const double_array& quadWts = _quadrature->quadWts();
+  assert(quadWts.size() == numQuadPts);
+  const int numBasis = _quadrature->numBasis();
+  const int spaceDim = _quadrature->spaceDim();
+  const int cellDim = _quadrature->cellDim();
+
+  // Allocate vectors for cell values.
+  _initCellVector();
+  const int cellVecSize = numBasis*spaceDim;
+  double_array dampingConstsCell(numQuadPts*spaceDim);
+
+  // Allocate vector for cell values (if necessary)
+  _initCellMatrix();
+
+  for (Mesh::label_sequence::iterator c_iter=cells->begin();
+       c_iter != cellsEnd;
+       ++c_iter) {
+    // Compute geometry information for current cell
+    _quadrature->computeGeometry(_boundaryMesh, coordinates, *c_iter);
+
+    // Reset element vector to zero
+    _resetCellMatrix();
+
+    // Get cell geometry information that depends on cell
+    const double_array& basis = _quadrature->basis();
+    const double_array& jacobianDet = _quadrature->jacobianDet();
+
+    // Compute Jacobian for absorbing bc terms
+    for (int iQuad=0; iQuad < numQuadPts; ++iQuad) {
+      const double wt = 
+	quadWts[iQuad] * jacobianDet[iQuad] / (2.0 * dt);
+      for (int iBasis=0, iQ=iQuad*numBasis; iBasis < numBasis; ++iBasis) {
+        const double valI = wt*basis[iQ+iBasis];
+        for (int jBasis=0; jBasis < numBasis; ++jBasis) {
+          const double valIJ = valI * basis[iQ+jBasis];
+          for (int iDim=0; iDim < spaceDim; ++iDim) {
+            const int iBlock = (iBasis*spaceDim + iDim) * (numBasis*spaceDim);
+            const int jBlock = (jBasis*spaceDim + iDim);
+            _cellMatrix[iBlock+jBlock] += 
+	      valIJ * dampingConstsCell[iQuad*spaceDim+iDim];
+          } // for
+        } // for
+      } // for
+    } // for
+    PetscErrorCode err = 
+      PetscLogFlops(numQuadPts*(3+numBasis*(1+numBasis*(1+2*spaceDim))));
+    if (err)
+      throw std::runtime_error("Logging PETSc flops failed.");
+    
+    // Assemble cell contribution into PETSc Matrix
+    const ALE::Obj<Mesh::order_type>& globalOrder = 
+      mesh->getFactory()->getGlobalOrder(mesh, "default", dispT);
+    assert(!globalOrder.isNull());
+
+    err = updateOperator(*jacobian, mesh, dispT, globalOrder,
+			 *c_iter, _cellMatrix, ADD_VALUES);
+    if (err)
+      throw std::runtime_error("Update to PETSc Mat failed.");
+  } // for
+
+  _needNewJacobian = false;
 } // integrateJacobian
 
 // ----------------------------------------------------------------------
