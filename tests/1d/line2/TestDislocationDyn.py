114284f7ad4934a5a1361bbc701d884f53f1c2f3
Brad Aagaard <baagaard@usgs.gov>
2007-11-13 19:56:48 +0000

    Setup running tests as test suite.

---
+++ tests/1d/line2/TestDislocationDyn.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# ----------------------------------------------------------------------
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# <LicenseText>
+#
+# ----------------------------------------------------------------------
+#
+
+## @file tests/1d/line2/TestDislocationDyn.py
+##
+## @brief Test suite for testing pylith with dynamic dislocation in
+## 1-D mesh.
+
+import unittest
+#import numpy
+#import tables
+
+
+from pylith.PyLithApp import PyLithApp
+class Dislocation(PyLithApp):
+
+  def __init__(self):
+    PyLithApp.__init__(self, "dislocation_dyn")
+    return
+
+
+def run_pylith():
+  """
+  Run pylith.
+  """
+  if not "done" in dir(run_pylith):
+    app = Dislocation()
+    app.run()
+    run_pylith.done = True
+  return
+
+
+class TestDislocationDyn(unittest.TestCase):
+  """
+  Test suite for testing pylith with dynamic dislocation in 1-D mesh.
+  """
+
+  def setUp(self):
+    """
+    Setup for test.
+    """
+    run_pylith()
+    return
+
+
+  def test_disp(self):
+    """
+    Check displacement field.
+    """
+    return
+
+
+# End of file 
