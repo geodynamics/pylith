b681cca7c5e211e55bc7d4e7b15611c4b1b7c496
Brad Aagaard <baagaard@usgs.gov>
2007-12-15 03:11:02 +0000

    Added missing headers.

---
+++ libsrc/meshio/DataWriterVTK.cc
@@ -0,0 +1,174 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+#include <portinfo>
+
+#include "DataWriterVTK.hh" // implementation of class methods
+
+#include <petscmesh_viewers.hh> // USES VTKViewer
+#include <assert.h> // USES assert()
+#include <sstream> // USES std::ostringstream
+#include <stdexcept> // USES std::runtime_error
+
+// ----------------------------------------------------------------------
+// Constructor
+pylith::meshio::DataWriterVTK::DataWriterVTK(void) :
+  _filename("output.vtk"),
+  _timeFormat("%f"),
+  _viewer(0)
+{ // constructor
+} // constructor
+
+// ----------------------------------------------------------------------
+// Destructor
+pylith::meshio::DataWriterVTK::~DataWriterVTK(void)
+{ // destructor
+  if (0 != _viewer)
+    PetscViewerDestroy(_viewer);
+  _viewer = 0;
+} // destructor  
+
+// ----------------------------------------------------------------------
+// Prepare file for data at a new time step.
+void
+pylith::meshio::DataWriterVTK::openTimeStep(
+			       const double t,
+			       const ALE::Obj<ALE::Mesh>& mesh,
+			       const spatialdata::geocoords::CoordSys* csMesh)
+{ // openTimeStep
+  try {
+    PetscErrorCode err;
+
+    std::ostringstream buffer;
+    const int indexExt = _filename.find(".vtk");
+    char sbuffer[256];
+    sprintf(sbuffer, _timeFormat.c_str(), t);
+    std::string timestamp(sbuffer);
+    
+    const int pos = timestamp.find(".");
+    if (pos != timestamp.length())
+      timestamp.erase(pos, 1);
+    buffer
+      << std::string(_filename, 0, indexExt) << "_t" << timestamp << ".vtk";
+
+    err = PetscViewerCreate(mesh->comm(), &_viewer);
+    err = PetscViewerSetType(_viewer, PETSC_VIEWER_ASCII);
+    err = PetscViewerSetFormat(_viewer, PETSC_VIEWER_ASCII_VTK);
+    err = PetscViewerFileSetName(_viewer, buffer.str().c_str());
+    if (err)
+      throw std::runtime_error("Could not open VTK file.");
+    
+    err = VTKViewer::writeHeader(_viewer);
+    err = VTKViewer::writeVertices(mesh, _viewer);
+    err = VTKViewer::writeElements(mesh, _viewer);
+    if (err)
+      throw std::runtime_error("Could not write topology.");
+  } catch (const std::exception& err) {
+    std::ostringstream msg;
+    msg << "Error while preparing for writing data to VTK file "
+	<< _filename << " at time " << t << ".\n" << err.what();
+    throw std::runtime_error(msg.str());
+  } catch (...) { 
+    std::ostringstream msg;
+    msg << "Unknown error while preparing for writing data to VTK file "
+	<< _filename << " at time " << t << ".\n";
+    throw std::runtime_error(msg.str());
+  } // try/catch
+} // openTimeStep
+
+// ----------------------------------------------------------------------
+/// Cleanup after writing data for a time step.
+void
+pylith::meshio::DataWriterVTK::closeTimeStep(void)
+{ // closeTimeStep
+  PetscViewerDestroy(_viewer);
+} // closeTimeStep
+
+// ----------------------------------------------------------------------
+// Write field over vertices to file.
+void
+pylith::meshio::DataWriterVTK::writeVertexField(
+				       const double t,
+				       const ALE::Obj<real_section_type>& field,
+				       const char* name,
+				       const ALE::Obj<ALE::Mesh>& mesh)
+{ // writeVertexField
+  try {
+    std::ostringstream buffer;
+    buffer.str("");
+    char timestamp[256];
+    sprintf(timestamp, _timeFormat.c_str(), t);
+    buffer << name << "_t" << timestamp;
+
+    // Now we are enforcing a 3D solution
+    //   Perhaps we need to push this argument higher
+    const int spaceDim = 3;
+    PetscErrorCode err = SectionView_Sieve_Ascii(mesh, field, 
+						 buffer.str().c_str(), 
+						 _viewer, spaceDim);
+    if (err)
+      throw std::runtime_error("Could not write vertex data.");
+  } catch (const std::exception& err) {
+    std::ostringstream msg;
+    msg << "Error while writing field '" << name << "' at time " 
+	<< t << " to VTK file '" << _filename << "'.\n" << err.what();
+    throw std::runtime_error(msg.str());
+  } catch (...) { 
+    std::ostringstream msg;
+    msg << "Error while writing field '" << name << "' at time " 
+	<< t << " to VTK file '" << _filename << "'.\n";
+    throw std::runtime_error(msg.str());
+  } // try/catch
+} // writeVertexField
+
+// ----------------------------------------------------------------------
+// Write field over cells to file.
+void
+pylith::meshio::DataWriterVTK::writeCellField(
+				       const double t,
+				       const ALE::Obj<real_section_type>& field,
+				       const char* name,
+				       const ALE::Obj<ALE::Mesh>& mesh)
+{ // writeCellField
+  try {
+    PetscErrorCode err = 0;
+
+    std::ostringstream buffer;
+    buffer.str("");
+    char timestamp[256];
+    sprintf(timestamp, _timeFormat.c_str(), t);
+    buffer << name << "_t" << timestamp;
+
+    err = PetscViewerPushFormat(_viewer, PETSC_VIEWER_ASCII_VTK_CELL);
+
+   // Get fiber dimension of first cell
+   const ALE::Obj<Mesh::label_sequence>& cells = mesh->heightStratum(0);
+   const int fiberDim = field->getFiberDimension(*cells->begin());
+   err = SectionView_Sieve_Ascii(mesh, field, buffer.str().c_str(), 
+				 _viewer, fiberDim);
+    if (err)
+      throw std::runtime_error("Could not write cell data.");   
+  } catch (const std::exception& err) {
+    std::ostringstream msg;
+    msg << "Error while writing field '" << name << "' at time " 
+	<< t << " to VTK file '" << _filename << "'.\n" << err.what();
+    throw std::runtime_error(msg.str());
+  } catch (...) { 
+    std::ostringstream msg;
+    msg << "Error while writing field '" << name << "' at time " 
+	<< t << " to VTK file '" << _filename << "'.\n";
+    throw std::runtime_error(msg.str());
+  } // try/catch
+} // writeCellField
+
+
+// End of file 
---
+++ libsrc/meshio/DataWriterVTK.hh
@@ -0,0 +1,106 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+/**
+ * @file pylith/meshio/DataWriterVTK.hh
+ *
+ * @brief Abstract base class for writing finite-element data to file.
+ */
+
+#if !defined(pylith_meshio_datawritervtk_hh)
+#define pylith_meshio_datawritervtk_hh
+
+#include "DataWriter.hh" // ISA DataWriter
+
+namespace pylith {
+  namespace meshio {
+    class DataWriterVTK;
+  } // meshio
+} // pylith
+
+class pylith::meshio::DataWriterVTK : public DataWriter
+{ // DataWriterVTK
+
+// PUBLIC METHODS ///////////////////////////////////////////////////////
+public :
+
+  /// Constructor
+  DataWriterVTK(void);
+
+  /// Destructor
+  ~DataWriterVTK(void);
+
+  /** Set filename for VTK file.
+   *
+   * @param filename Name of VTK file.
+   */
+  void filename(const char* filename);
+
+  /** Set time format for time stamp in name of VTK file.
+   *
+   * @param format C style time format for filename.
+   */
+  void timeFormat(const char* format);
+
+  /** Prepare file for data at a new time step.
+   *
+   * @param t Time stamp for new data
+   * @param mesh PETSc mesh object
+   * @param csMesh Coordinate system of mesh geometry
+   */
+  void openTimeStep(const double t,
+		    const ALE::Obj<ALE::Mesh>& mesh,
+		    const spatialdata::geocoords::CoordSys* csMesh);
+
+  /// Cleanup after writing data for a time step.
+  void closeTimeStep(void);
+
+  /** Write field over vertices to file.
+   *
+   * @param t Time associated with field.
+   * @param field PETSc field over vertices.
+   * @param mesh Finite-element mesh
+   * @param name Name of field.
+   */
+  void writeVertexField(const double t,
+			const ALE::Obj<real_section_type>& field,
+			const char* name,
+			const ALE::Obj<ALE::Mesh>& mesh);
+
+  /** Write field over cells to file.
+   *
+   * @param t Time associated with field.
+   * @param field PETSc field over cells.
+   * @param name Name of field.
+   * @param mesh PETSc mesh object.
+   */
+  void writeCellField(const double t,
+		      const ALE::Obj<real_section_type>& field,
+		      const char* name,
+		      const ALE::Obj<ALE::Mesh>& mesh);
+
+// PRIVATE MEMBERS //////////////////////////////////////////////////////
+private :
+
+  std::string _filename; ///< Name of VTK file.
+  std::string _timeFormat; ///< C style time format for time stamp.
+
+  PetscViewer _viewer; ///< Output file
+
+}; // DataWriterVTK
+
+#include "DataWriterVTK.icc" // inline methods
+
+#endif // pylith_meshio_datawritervtk_hh
+
+
+// End of file 
