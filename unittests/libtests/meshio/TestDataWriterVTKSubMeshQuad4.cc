cd002fc314f749b6a9aa111e8d7dc7447366aa05
Brad Aagaard <baagaard@usgs.gov>
2008-02-29 01:19:45 +0000

    Finished C++ unit tests for output of sub mesh (material).

---
+++ unittests/libtests/meshio/TestDataWriterVTKSubMeshQuad4.cc
@@ -0,0 +1,34 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestDataWriterVTKSubMeshQuad4.hh" // Implementation of class methods
+
+#include "data/DataWriterVTKDataSubMeshQuad4.hh" // USES DataWriterVTKDataMeshQuad4
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestDataWriterVTKSubMeshQuad4 );
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::meshio::TestDataWriterVTKSubMeshQuad4::setUp(void)
+{ // setUp
+  TestDataWriterVTKMesh::setUp();
+  _data = new DataWriterVTKDataSubMeshQuad4;
+  _dataMesh = new DataWriterVTKDataSubMeshQuad4;
+  _initialize();
+} // setUp
+
+
+// End of file 
