d408c39920457407026b032c63b0c535581ba3d0
Brad Aagaard <baagaard@usgs.gov>
2008-03-02 05:25:22 +0000

    Started work on unit tests for submesh().

---
+++ unittests/libtests/bc/TestBoundaryMesh.hh
@@ -0,0 +1,64 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/**
+ * @file unittests/libtests/bc/TestBoundaryMesh.hh
+ *
+ * @brief C++ TestBoundaryMesh object.
+ *
+ * C++ unit testing for BoundaryMesh.
+ */
+
+#if !defined(pylith_bc_testboundarymesh_hh)
+#define pylith_bc_testboundarymesh_hh
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace bc {
+    class TestBoundaryMesh;
+
+    class BoundaryMeshData;
+  } // bc
+} // pylith
+
+/// C++ unit testing for BoundaryMesh.
+class pylith::bc::TestBoundaryMesh : public CppUnit::TestFixture
+{ // class TestBoundaryMesh
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+  /// Tear down testing data.
+  void tearDown(void);
+
+  /// Test submesh() without fault.
+  void testSubmesh(void);
+
+  /// Test submesh() with fault().
+  void testSubmeshFault(void);
+
+  // PROTECTED MEMBERS //////////////////////////////////////////////////
+protected :
+
+  BoundaryMeshData* _data; ///< Data for testing
+
+}; // class TestBoundaryMesh
+
+#endif // pylith_bc_boundarymesh_hh
+
+
+// End of file 
