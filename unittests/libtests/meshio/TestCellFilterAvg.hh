dd1a5645b01169580035177d54eb3e8761d668bf
Brad Aagaard <baagaard@usgs.gov>
2008-03-06 00:10:26 +0000

    Added C++ unit tests for CellFilterAvg and VertexFilterVecNorm.

---
+++ unittests/libtests/meshio/TestCellFilterAvg.hh
@@ -0,0 +1,58 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+/**
+ * @file unittests/libtests/meshio/TestCellFilterAvg.hh
+ *
+ * @brief C++ TestCellFilterAvg object
+ *
+ * C++ unit testing for CellFilterAvg.
+ */
+
+#if !defined(pylith_meshio_testcellfilteravg_hh)
+#define pylith_meshio_testcellfilteravg_hh
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace meshio {
+    class TestCellFilterAvg;
+  } // meshio
+} // pylith
+
+/// C++ unit testing for CellFilterAvg
+class pylith::meshio::TestCellFilterAvg : public CppUnit::TestFixture
+{ // class TestCellFilterAvg
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestCellFilterAvg );
+
+  CPPUNIT_TEST( testConstructor );
+  CPPUNIT_TEST( testFilter );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Test constructor
+  void testConstructor(void);
+
+  /// Test filter()
+  void testFilter(void);
+
+}; // class TestCellFilterAvg
+
+#endif // pylith_meshio_testcellfilteravg_hh
+
+// End of file 
---
+++ unittests/libtests/meshio/TestVertexFilterVecNorm.cc
@@ -0,0 +1,105 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestVertexFilterVecNorm.hh" // Implementation of class methods
+
+#include "pylith/meshio/VertexFilterVecNorm.hh"
+
+#include "pylith/meshio/MeshIOAscii.hh" // USES MeshIOAscii
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestVertexFilterVecNorm );
+
+// ----------------------------------------------------------------------
+// Test constructor
+void
+pylith::meshio::TestVertexFilterVecNorm::testConstructor(void)
+{ // testConstructor
+  VertexFilterVecNorm filter;
+} // testConstructor
+
+// ----------------------------------------------------------------------
+// Test filter()
+void
+pylith::meshio::TestVertexFilterVecNorm::testFilter(void)
+{ // testFilter
+  const char* filename = "data/tri3.mesh";
+  const int fiberDim = 2;
+  const int nvertices = 4;
+  const char* fieldName = "field data";
+  const VectorFieldEnum fieldType = VECTOR_FIELD;
+  const double fieldValues[] = {
+    1.1, 1.2,
+    2.1, 2.2,
+    3.1, 3.2,
+    4.1, 4.2
+  };
+  const VectorFieldEnum fieldTypeE = SCALAR_FIELD;
+  const int fiberDimE = 1;
+  const double fieldValuesE[] = {
+    sqrt(pow(1.1, 2) + pow(1.2, 2)),
+    sqrt(pow(2.1, 2) + pow(2.2, 2)),
+    sqrt(pow(3.1, 2) + pow(3.2, 2)),
+    sqrt(pow(4.1, 2) + pow(4.2, 2))
+  };
+
+  MeshIOAscii iohandler;
+  ALE::Obj<Mesh> mesh;
+  iohandler.filename(filename);
+  iohandler.read(&mesh);
+  CPPUNIT_ASSERT(!mesh.isNull());
+  mesh->getFactory()->clear();
+
+  // Set vertex field
+  const ALE::Obj<Mesh::label_sequence>& vertices = mesh->depthStratum(0);
+  const Mesh::label_sequence::iterator verticesEnd = vertices->end();
+
+  ALE::Obj<real_section_type> field = 
+    new real_section_type(mesh->comm(), mesh->debug());
+  field->setFiberDimension(vertices, fiberDim);
+  mesh->allocate(field);
+
+  CPPUNIT_ASSERT_EQUAL(nvertices, int(vertices->size()));
+
+  int ipt = 0;
+  for (Mesh::label_sequence::iterator v_iter=vertices->begin();
+       v_iter != verticesEnd;
+       ++v_iter, ++ipt) {
+    const double* values = &fieldValues[ipt*fiberDim];
+    field->updatePoint(*v_iter, values);
+  } // for
+
+  VertexFilterVecNorm filter;
+  VectorFieldEnum fieldTypeF = OTHER_FIELD;
+  const ALE::Obj<real_section_type>& fieldF =
+    filter.filter(&fieldTypeF, field, mesh);
+
+  CPPUNIT_ASSERT_EQUAL(fieldTypeE, fieldTypeF);
+  ipt = 0;
+  for (Mesh::label_sequence::iterator v_iter=vertices->begin();
+       v_iter != verticesEnd;
+       ++v_iter, ++ipt) {
+    CPPUNIT_ASSERT_EQUAL(fiberDimE, fieldF->getFiberDimension(*v_iter));
+    const double* values = fieldF->restrictPoint(*v_iter);
+    CPPUNIT_ASSERT(0 != values);
+    const double tolerance = 1.0e-06;
+    for (int i=0; i < fiberDimE; ++i)
+      CPPUNIT_ASSERT_DOUBLES_EQUAL(1.0, 
+				   values[i]/fieldValuesE[ipt*fiberDimE+i],
+				   tolerance);
+  } // for
+} // testFilter
+
+
+// End of file 
