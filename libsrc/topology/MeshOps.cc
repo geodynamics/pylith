4b34ad47d57a1b937678a3b5fc9d5275dd7360a3
Brad Aagaard <brad>
2008-09-13 22:14:55 +0000

    Added check to make sure all materials are associated with at least 1 cell.

--- libsrc/topology/MeshOps.cc
+++ libsrc/topology/MeshOps.cc
@@ -14,11 +14,15 @@
 
 #include "MeshOps.hh" // implementation of class methods
 
+#include "pylith/utils/array.hh" // USES int_array
+
 #include <stdexcept> // USES std::runtime_error
 #include <sstream> // USES std::ostringstream
 #include <assert.h> // USES assert()
 
 #include <algorithm> // USES std::sort, std::find
+#include <map> // USES std::map
+
 
 // ----------------------------------------------------------------------
 void
@@ -28,6 +32,14 @@ pylith::topology::MeshOps::checkMaterialIds(const ALE::Obj<Mesh>& mesh,
 { // checkMaterialIds
   assert( (0 == numMaterials && 0 == materialIds) ||
 	  (0 < numMaterials && 0 != materialIds) );
+
+  // Create map with indices for each material
+  std::map<int, int> materialIndex;
+  for (int i=0; i < numMaterials; ++i)
+    materialIndex[materialIds[i]] = i;
+
+  int_array matCellCounts(numMaterials);
+  matCellCounts = 0;
 
   const ALE::Obj<Mesh::label_sequence>& cells = mesh->heightStratum(0);
   assert(!cells.isNull());
@@ -49,7 +61,24 @@ pylith::topology::MeshOps::checkMaterialIds(const ALE::Obj<Mesh>& mesh,
 	  << "' does not match the id of any available materials or interfaces.";
       throw std::runtime_error(msg.str());
     } // if
+
+    const int matIndex = materialIndex[cellId];
+    assert(0 <= matIndex && matIndex < numMaterials);
+    ++matCellCounts[matIndex];
   } // for
+
+  // Make sure each material has 
+  int_array matCellCountsAll(matCellCounts.size());
+  MPI_Allreduce(&matCellCounts[0], &matCellCountsAll[0],
+		matCellCounts.size(), MPI_INT, MPI_SUM, mesh->comm());
+  for (int i=0; i < numMaterials; ++i)
+    if (matCellCountsAll[i] <= 0) {
+      std::ostringstream msg;
+      msg << "No cells associated with material with id '" << materialIds[i]
+	  << "'.";
+      throw std::runtime_error(msg.str());
+    } // if
+  
 } // checkMaterialIds
 
 
