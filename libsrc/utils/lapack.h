9efb6c21225b9b98c25cf4e3fae4543ca9df5d30
Brad Aagaard <baagaard@usgs.gov>
2008-10-27 17:44:02 +0000

    Finally figured out PETSc's conventions for interfacing with LAPACK- need to explicitly include petscblaslapack header file.

--- libsrc/utils/lapack.h
+++ libsrc/utils/lapack.h
@@ -13,31 +13,15 @@
 /**
  * @file pylith/utils/lapack.h
  *
- * @brief Declarations for BLAS routines that we use. We rely on
+ * @brief Declarations for LAPACK routines that we use. We rely on
  * PETSc's interface to these routines.
  */
 
 #if !defined(pylith_utils_lapack_h)
 #define pylith_utils_lapack_h
 
-#define lapack_dgesvd dgesvd_
-
-extern "C" {
-  EXTERN void lapack_dgesvd(const char*,
-			    const char*,
-			    PetscBLASInt*,
-			    PetscBLASInt*,
-			    PetscScalar*,
-			    PetscBLASInt*,
-			    PetscReal*,
-			    PetscScalar*,
-			    PetscBLASInt*,
-			    PetscScalar*,
-			    PetscBLASInt*,
-			    PetscScalar*,
-			    PetscBLASInt*,
-			    PetscBLASInt*);
-}
+#include <petscblaslapack.h>
+#define lapack_dgesvd LAPACKgesvd_
 
 #endif // pylith_utils_lapack_h
 
