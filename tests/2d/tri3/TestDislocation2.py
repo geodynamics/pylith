645f0ce4769f8d8d5f9de13c299e70e650860b92
Brad Aagaard <baagaard@usgs.gov>
2008-11-20 18:42:11 +0000

    Adjusted domain and added two fault test.

---
+++ tests/2d/tri3/TestDislocation2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# ----------------------------------------------------------------------
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# <LicenseText>
+#
+# ----------------------------------------------------------------------
+#
+
+## @file tests/2d/tri3/TestDislocation2.py
+##
+## @brief Test suite for testing pylith with two parallel shear
+## dislocations for 2-D box.
+
+import unittest
+
+# Local application
+from pylith.PyLithApp import PyLithApp
+class DislocationApp(PyLithApp):
+  def __init__(self):
+    PyLithApp.__init__(self, name="dislocation2")
+    return
+
+
+# Helper function to run PyLith
+def run_pylith():
+  """
+  Run pylith.
+  """
+  if not "done" in dir(run_pylith):
+    app = DislocationApp()
+    app.run()
+    run_pylith.done = True
+  return
+
+
+class TestDislocation2(unittest.TestCase):
+  """
+  Test suite for testing pylith with shear dislocation for 2-D box.
+  """
+
+  def setUp(self):
+    """
+    Setup for test.
+    """
+    run_pylith()
+    return
+
+
+  def test_disp(self):
+    """
+    Check displacement field.
+    """
+    return
+
+
+# End of file 
