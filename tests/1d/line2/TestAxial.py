3499057b41616924cc4525dae350f09db1129579
Brad Aagaard <baagaard@usgs.gov>
2008-11-20 00:47:59 +0000

    Clean up of tests.

--- tests/1d/line2/TestAxial.py
+++ tests/1d/line2/TestAxial.py
@@ -15,24 +15,26 @@
 ## @brief Test suite for testing pylith with 1-D axial extension.
 
 import unittest
-#import numpy
-#import tables
+import numpy
+from pylith.utils.VTKDataReader import has_vtk
+from pylith.utils.VTKDataReader import VTKDataReader
 
 
+# Local version of PyLithApp
 from pylith.PyLithApp import PyLithApp
-class AxialExtension(PyLithApp):
-
+class AxialPlaneStrainApp(PyLithApp):
   def __init__(self):
-    PyLithApp.__init__(self, "axialextension")
+    PyLithApp.__init__(self, name="axialextension")
     return
 
 
+# Helper function to run PyLith
 def run_pylith():
   """
   Run pylith.
   """
   if not "done" in dir(run_pylith):
-    app = AxialExtension()
+    app = AxialPlaneStrainApp()
     app.run()
     run_pylith.done = True
   return
@@ -48,12 +50,127 @@ class TestAxial(unittest.TestCase):
     Setup for test.
     """
     run_pylith()
+    if has_vtk():
+      self.reader = VTKDataReader()
+    else:
+      self.reader = None
     return
 
 
-  def test_disp(self):
+  def test_elastic_info(self):
     """
-    Check displacement field.
+    Check elastic info.
+    """
+    if self.reader is None:
+      return
+
+    data = self.reader.read("axialextension-statevars-elastic_info.vtk")
+
+    # Check cells
+    ncellsE = 4
+    ncornersE = 2
+    (ncells, ncorners) = data['cells'].shape
+    self.assertEqual(ncellsE, ncells)
+    self.assertEqual(ncornersE, ncorners)
+
+    # Check vertices
+    nverticesE = 5
+    spaceDimE = 2
+    (nvertices, spaceDim) = data['vertices'].shape
+    self.assertEqual(nverticesE, nvertices)
+    self.assertEqual(spaceDimE, spaceDim)
+
+    # Check physical properties
+    tolerance = 1.0e-5
+    vsE = 3000.0
+    vpE = 5291.502622129181
+    densityE = 2500.0
+
+    # Lame's constant mu (shear modulus)
+    muE = densityE*vsE**2
+    diff = numpy.abs(1.0 - data['cell_fields']['mu']/muE)
+    okay = diff < tolerance
+    if numpy.sum(okay) != ncells:
+      print "Lame's constant mu: ",data['cell_fields']['mu']
+      self.assertEqual(ncells, numpy.sum(okay))    
+
+    # Lame's constant lambda
+    lambdaE = densityE*vpE**2 - 2*muE
+    diff = numpy.abs(1.0 - data['cell_fields']['lambda']/lambdaE)
+    okay = diff < tolerance
+    if numpy.sum(okay) != ncells:
+      print "Lame's constant lambda: ",data['cell_fields']['lambda']
+      self.assertEqual(ncells, numpy.sum(okay))    
+
+    # Density
+    diff = numpy.abs(1.0 - data['cell_fields']['density']/densityE)
+    okay = diff < tolerance
+    if numpy.sum(okay) != ncells:
+      print "Density: ",data['cell_fields']['density']
+      self.assertEqual(ncells, numpy.sum(okay))    
+    return
+
+
+  def test_soln(self):
+    """
+    Check solution (displacement) field.
+    """
+    if self.reader is None:
+      return
+
+    data = self.reader.read("axialextension_t0000000.vtk")
+
+    # Check cells
+    ncellsE = 4
+    ncornersE = 2
+    (ncells, ncorners) = data['cells'].shape
+    self.assertEqual(ncellsE, ncells)
+    self.assertEqual(ncornersE, ncorners)
+
+    # Check vertices
+    nverticesE = 5
+    spaceDimE = 3
+    vertices = data['vertices']
+    (nvertices, spaceDim) = vertices.shape
+    self.assertEqual(nverticesE, nvertices)
+    self.assertEqual(spaceDimE, spaceDim)
+
+    # Check displacement solution
+    tolerance = 1.0e-5
+    dispE = numpy.zeros( (nvertices, spaceDim), dtype=numpy.float64)
+    dispE[:,0] = -0.2 + 0.1 * vertices[:,0]
+
+    disp = data['vertex_fields']['displacements']
+
+    # Check x displacements
+    diff = numpy.abs(disp[:,0] - dispE[:,0])
+    okay = diff < tolerance
+    if numpy.sum(okay) != nvertices:
+      print "Displacement field: ",disp
+      self.assertEqual(nvertices, numpy.sum(okay))    
+    
+    # Check y displacements
+    mask = dispE[:,1] > 0.0
+    diff = mask * numpy.abs(1.0 - disp[:,1] / dispE[:,1]) + \
+        ~mask * numpy.abs(disp[:,1] - dispE[:,1])
+    okay = diff < tolerance
+    if numpy.sum(okay) != nvertices:
+      print "Displacement field: ",disp
+      self.assertEqual(nvertices, numpy.sum(okay))    
+
+    # Check z displacements
+    diff = numpy.abs(disp[:,2] - dispE[:,2])
+    okay = diff < tolerance
+    if numpy.sum(okay) != nvertices:
+      print "Displacement field: ",disp
+      self.assertEqual(nvertices, numpy.sum(okay))    
+    
+    return
+
+
+  def test_elastic_statevars(self):
+    """
+    Check elastic state variables.
     """
     return
 
