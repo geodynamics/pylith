bf692c684bebc0dafbe13da7c58d5c9b1a1489f8
Brad Aagaard <brad>
2009-05-10 00:01:16 +0000

    Fixed compatibility bugs.

--- pylith/perf/Memory.py
+++ pylith/perf/Memory.py
@@ -1,4 +1,4 @@
-class Memory():
+class Memory(object):
   sizeInt    = 4
   sizeDouble = 8
   import distutils.sysconfig
@@ -7,6 +7,8 @@ class Memory():
     sizeArrow = 40 # 32 bit
   elif pointerSize == 8:
     sizeArrow = 56 # 64 bit
+  elif pointerSize is None:
+    sizeArrow = 0 # Use 0 if can't get estimate of pointer size.
   else:
     raise RuntimeError('Could not determine the size of a pointer')
 
