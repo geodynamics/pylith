55d3d466c02da9b81b926979736933b8fd9ea1f1
Brad Aagaard <brad>
2009-08-18 23:00:18 +0000

    Moved orientation and area routines back to FaultCohesiveKin from FaultCohesive because they are vertex-based and won't be shared with FaultCohesiveDyn.

--- libsrc/faults/FaultCohesiveKin.hh
+++ libsrc/faults/FaultCohesiveKin.hh
@@ -206,6 +206,21 @@ public :
   // PRIVATE METHODS ////////////////////////////////////////////////////
 private :
 
+  /** Calculate orientation at fault vertices.
+   *
+   * @param upDir Direction perpendicular to along-strike direction that is 
+   *   not collinear with fault normal (usually "up" direction but could 
+   *   be up-dip direction; only applies to fault surfaces in a 3-D domain).
+   * @param normalDir General preferred direction for fault normal
+   *   (used to pick which of two possible normal directions for
+   *   interface; only applies to fault surfaces in a 3-D domain).
+   */
+  void _calcOrientation(const double upDir[3],
+			const double normalDir[3]);
+
+  /// Calculate fault area field.
+  void _calcArea(void);
+
   /** Compute change in tractions on fault surface using solution.
    *
    * @param tractions Field for tractions.
