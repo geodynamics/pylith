d8c2b6add77a45f7f7521090dcfd9a2c8b01266a
Brad Aagaard <brad>
2009-11-20 18:00:19 +0000

    Finished implicit large deformation gravity C++ unit tests.

---
+++ unittests/libtests/feassemble/TestElasticityImplicitLgDeformGrav1DQuadratic.cc
@@ -0,0 +1,56 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestElasticityImplicitLgDeformGrav1DQuadratic.hh" // Implementation of class methods
+
+#include "data/ElasticityImplicitLgDeformGravData1DQuadratic.hh"
+
+#include "pylith/topology/Mesh.hh" // USES Quadrature<Mesh>
+#include "pylith/feassemble/Quadrature.hh" // USES Quadrature
+#include "pylith/feassemble/GeometryLine1D.hh" // USES GeometryLine1D
+#include "pylith/materials/ElasticStrain1D.hh" // USES ElasticStrain1D
+#include "spatialdata/spatialdb/GravityField.hh" // USES GravityField
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::feassemble::TestElasticityImplicitLgDeformGrav1DQuadratic );
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::feassemble::TestElasticityImplicitLgDeformGrav1DQuadratic::setUp(void)
+{ // setUp
+  TestElasticityImplicitLgDeform::setUp();
+
+  _data = new ElasticityImplicitLgDeformGravData1DQuadratic();
+  _gravityField = new spatialdata::spatialdb::GravityField();
+  CPPUNIT_ASSERT(0 != _quadrature);
+  CPPUNIT_ASSERT(0 != _gravityField);
+  GeometryLine1D geometry;
+  _quadrature->refGeometry(&geometry);
+
+  const double g = 1.0e8;
+  const double gravityDir[] = { -1.0, 0.0, 0.0};
+  _gravityField->gravAcceleration(g);
+  _gravityField->gravityDir(gravityDir[0], gravityDir[1], gravityDir[2]);
+
+  _material = new materials::ElasticStrain1D;
+  CPPUNIT_ASSERT(0 != _material);
+  
+  CPPUNIT_ASSERT(0 != _data);
+  CPPUNIT_ASSERT_EQUAL(std::string("ElasticStrain1D"),
+		       std::string(_data->matType));
+} // setUp
+
+
+// End of file 
