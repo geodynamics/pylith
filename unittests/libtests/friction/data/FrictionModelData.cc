7ee114428057d0e4a5ffdea160290d1fe922ca3f
Brad Aagaard <baagaard@usgs.gov>
2010-01-22 05:54:49 +0000

    Started work on setting up unit tests for fault constitutive models.

---
+++ unittests/libtests/friction/data/FrictionModelData.cc
@@ -0,0 +1,47 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+#include "MaterialData.hh"
+
+// ----------------------------------------------------------------------
+// Constructor
+pylith::materials::MaterialData::MaterialData(void) :
+  dimension(0),
+  numLocs(0),
+  numProperties(0),
+  numStateVars(0),
+  numDBProperties(0),
+  numDBStateVars(0),
+  numPropsQuadPt(0),
+  numVarsQuadPt(0),
+  numPropertyValues(0),
+  numStateVarValues(0),
+  dbPropertyValues(0),
+  dbStateVarValues(0),
+  dbProperties(0),
+  dbStateVars(0),
+  properties(0),
+  stateVars(0),
+  lengthScale(0),
+  timeScale(0),
+  pressureScale(0),
+  densityScale(0)
+{ // constructor
+} // constructor
+
+// ----------------------------------------------------------------------
+// Destructor
+pylith::materials::MaterialData::~MaterialData(void)
+{ // destructor
+} // destructor
+
+// End of file
