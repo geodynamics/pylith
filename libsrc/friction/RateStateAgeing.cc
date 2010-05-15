544b846c8a17465bc3f0e50bdcb44f72fca8679e
Brad Aagaard <baagaard@usgs.gov>
2010-05-15 18:25:13 +0000

    Removed some debugging output.

--- libsrc/friction/RateStateAgeing.cc
+++ libsrc/friction/RateStateAgeing.cc
@@ -348,8 +348,6 @@ pylith::friction::RateStateAgeing::_updateStateVars(const double slip,
 
   stateVars[s_state] = thetaTpdtVertex;
 
-  std::cout << "STATEVAR before: " << thetaTVertex << ", after: " << thetaTpdtVertex << std::endl;
-  std::cout << "SLIP RATE: " << slipRate << std::endl;
   PetscLogFlops(6);
 } // _updateStateVars
 
