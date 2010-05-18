4dbe3ba7bd28dd20ca4318a52af60de568787628
Brad Aagaard <baagaard@usgs.gov>
2010-05-18 03:58:16 +0000

    Finished friction template.

--- templates/friction/ViscousFriction.py
+++ templates/friction/ViscousFriction.py
@@ -16,8 +16,13 @@
 ##
 ## Factory: friction_model.
 
-from FrictionModel import FrictionModel
-from friction import ViscousFriction as ModuleViscousFriction
+# ISA FrictionModel
+from pylith.friction.FrictionModel import FrictionModel
+
+# Import the SWIG module ViscousFriction object and rename it
+# ModuleViscousFriction so that it doesn't clash with the local Python
+# class of the same name.
+from frictioncontrib import ViscousFriction as ModuleViscousFriction
 
 # ViscousFriction class
 class ViscousFriction(FrictionModel, ModuleViscousFriction):
@@ -34,6 +39,16 @@ class ViscousFriction(FrictionModel, ModuleViscousFriction):
     Constructor.
     """
     FrictionModel.__init__(self, name)
+    # Set the fields that are available for output. These are the
+    # stored physical properties and state variables. The friction
+    # model information is output with the fault information, so we
+    # can also output slip, slip rate and the fault tractions.
+    #
+    # There are no cell fields because the fault constitutive model
+    # operations on quantities evaluated at the fault vertices.
+    #
+    # Do not change the name of this variable. The output manager will
+    # request this variable by name.
     self.availableFields = \
         {'vertex': \
            {'info': ["static_coefficient",
@@ -42,7 +57,7 @@ class ViscousFriction(FrictionModel, ModuleViscousFriction):
          'cell': \
            {'info': [],
             'data': []}}
-    self._loggingPrefix = "FrVisc "
+    self._loggingPrefix = "FrVisc " # Prefix that appears in PETSc logging
     return
 
 
@@ -50,7 +65,9 @@ class ViscousFriction(FrictionModel, ModuleViscousFriction):
 
   def _createModuleObj(self):
     """
-    Call constructor for module object for access to C++ object.
+    Call constructor for module object for access to C++ object. This
+    function is called automatically by the generic Python FrictionModel
+    object. It must have this name and self as the only argument.
     """
     ModuleViscousFriction.__init__(self)
     return
@@ -58,11 +75,14 @@ class ViscousFriction(FrictionModel, ModuleViscousFriction):
 
 # FACTORIES ////////////////////////////////////////////////////////////
 
+# This is the function that is called when you invoke
+# friction = pylith.pylith.contrib.ViscousFfriction
+# The name of this function MUST be 'friction_model'.
 def friction_model():
   """
   Factory associated with ViscousFriction.
   """
-  return ViscousFriction()
+  return ViscousFriction() # Return our object
 
 
 # End of file 
---
+++ templates/friction/tests/TestViscousFriction.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+# ======================================================================
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# {LicenseText}
+#
+# ======================================================================
+#
+
+# We cannot test the low-level functionality of the ViscousFriction
+# object because it is not exposed to Python. You should really setup
+# C++ unit tests using CppUnit as is done for PyLith in addition to
+# the simple Python unit tests here.
+
+import unittest
+
+
+class TestViscousFriction(unittest.TestCase):
+  """
+  Unit testing of ViscousFriction object.
+  """
+
+  def setUp(self):
+    """
+    Setup test subject.
+    """
+    from pylith.friction.contrib.ViscousFriction import ViscousFriction
+    self.model = ViscousFriction()
+    return
+  
+
+  def test_label(self):
+    """
+    Test constructor.
+    """
+    label = "viscous friction"
+    self.model.label(label)
+    self.assertEqual(label, self.model.label())
+    return
+
+
+  def test_timeStep(self):
+    """
+    Test constructor.
+    """
+    dt = 2.4
+    self.model.timeStep(dt)
+    self.assertEqual(dt, self.model.timeStep())
+    return
+
+
+  def testHasProperty(self):
+    self.failUnless(self.model.hasProperty("static_coefficient"))
+    self.failUnless(self.model.hasProperty("reference_slip_rate"))
+    self.failUnless(self.model.hasProperty("cohesion"))
+    return
+
+
+  def testHasStateVar(self):
+    self.failUnless(self.model.hasStateVar("slip_rate"))
+    return
+
+
+  def test_factory(self):
+    """
+    Test factory method.
+    """
+    from pylith.friction.contrib.ViscousFriction import friction_model
+    f = friction_model()
+    return
+
+
+# End of file 
