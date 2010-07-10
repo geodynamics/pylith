f96f3e0c97800f4473dac0e85e2c3534ad75b20f
Surendra Somala <surendra@caltech.edu>
2010-07-10 14:43:32 +0000

    Added unittests for time-weakening friction

---
+++ unittests/libtests/friction/data/TimeWeakeningData.hh
@@ -0,0 +1,95 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+//                           Brad T. Aagaard
+//                        U.S. Geological Survey
+//
+// {LicenseText}
+//
+// ======================================================================
+//
+
+#if !defined(pylith_friction_timeweakeningdata_hh)
+#define pylith_friction_timeweakeningdata_hh
+
+#include "FrictionModelData.hh"
+
+namespace pylith {
+  namespace friction {
+     class TimeWeakeningData;
+  } // pylith
+} // friction
+
+class pylith::friction::TimeWeakeningData : public FrictionModelData
+{
+
+public: 
+
+  /// Constructor
+  TimeWeakeningData(void);
+
+  /// Destructor
+  ~TimeWeakeningData(void);
+
+private:
+
+  static const int _numLocs;
+
+  static const int _numProperties;
+
+  static const int _numStateVars;
+
+  static const int _numDBProperties;
+
+  static const int _numDBStateVars;
+
+  static const int _numPropsVertex;
+
+  static const int _numVarsVertex;
+
+  static const double _lengthScale;
+
+  static const double _timeScale;
+
+  static const double _pressureScale;
+
+  static const double _densityScale;
+
+  static const double _dt;
+
+  static const int _numPropertyValues[];
+
+  static const int _numStateVarValues[];
+
+  static const char* _dbPropertyValues[];
+
+  static const char* _dbStateVarValues[];
+
+  static const double _dbProperties[];
+
+  static const double _dbStateVars[];
+
+  static const double _properties[];
+
+  static const double _stateVars[];
+
+  static const double _propertiesNondim[];
+
+  static const double _stateVarsNondim[];
+
+  static const double _friction[];
+
+  static const double _slip[];
+
+  static const double _slipRate[];
+
+  static const double _normalTraction[];
+
+  static const double _stateVarsUpdated[];
+
+};
+
+#endif // pylith_friction_timeweakeningdata_hh
+
+// End of file
