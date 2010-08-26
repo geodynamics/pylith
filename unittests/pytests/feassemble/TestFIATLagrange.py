102821d5c0a638a980415ba8b9547fef4ecd0064
Brad Aagaard <baagaard@usgs.gov>
2010-08-26 23:31:22 +0000

    Added unit test to check FIATLagrange for 2-D cells with degree == 2 (quadratic quadrilaterals). Fixed ordering of basis stuff in FIATLagrange for 2-D. Faults will not work without additional Sieve code to handle additional vertices on faces.

--- unittests/pytests/feassemble/TestFIATLagrange.py
+++ unittests/pytests/feassemble/TestFIATLagrange.py
@@ -72,6 +72,7 @@ class Line2(object):
   def N1p(self, p):
     return 0.5
 
+
 # ----------------------------------------------------------------------
 class Line3(object):
 
@@ -124,6 +125,7 @@ class Line3(object):
   def N2p(self, p):
     return -2.0*p
 
+
 # ----------------------------------------------------------------------
 class Quad4(object):
 
@@ -137,8 +139,8 @@ class Quad4(object):
                             [-1.0, +1.0]])
     quadPts = numpy.array([ [-1.0/3**0.5, -1.0/3**0.5],
                             [+1.0/3**0.5, -1.0/3**0.5],
-                            [+1.0/3**0.5, +1.0/3**0.5],
-                            [-1.0/3**0.5, +1.0/3**0.5] ])
+                            [-1.0/3**0.5, +1.0/3**0.5],
+                            [+1.0/3**0.5, +1.0/3**0.5] ])
     quadWts = numpy.array( [1.0, 1.0, 1.0, 1.0])
 
     # Compute basis fns and derivatives at quadrature points
@@ -202,6 +204,152 @@ class Quad4(object):
 
   def N3q(self, p):
     return (1-p[0])/4.0
+
+
+# ----------------------------------------------------------------------
+class Quad9(object):
+
+  def __init__(self):
+    """
+    Setup quad9 cell.
+    """
+    vertices = numpy.array([[-1.0, -1.0], # corners
+                            [+1.0, -1.0],
+                            [+1.0, +1.0],
+                            [-1.0, +1.0],
+                            [ 0.0, -1.0], # edges
+                            [+1.0,  0.0],
+                            [ 0.0, +1.0],
+                            [-1.0,  0.0],
+                            [ 0.0,  0.0], # face
+                            ])
+    quadPts = numpy.array([ [-(3.0/5)**0.5, -(3.0/5)**0.5],
+                            [          0.0, -(3.0/5)**0.5],
+                            [+(3.0/5)**0.5, -(3.0/5)**0.5],
+                            [-(3.0/5)**0.5, 0.0],
+                            [          0.0, 0.0],
+                            [+(3.0/5)**0.5, 0.0],
+                            [-(3.0/5)**0.5, +(3.0/5)**0.5],
+                            [          0.0, +(3.0/5)**0.5],
+                            [+(3.0/5)**0.5, +(3.0/5)**0.5] ])
+    quadWts = numpy.array( [25.0/81, 40.0/81, 25.0/81,
+                            40.0/81, 64.0/81, 40.0/81,
+                            25.0/81, 40.0/81, 25.0/81])
+
+    # Compute basis fns and derivatives at quadrature points
+    basis = numpy.zeros( (9, 9), dtype=numpy.float64)
+    basisDeriv = numpy.zeros( (9, 9, 2), dtype=numpy.float64)
+    iQuad = 0
+    for q in quadPts:
+      basis[iQuad] = numpy.array([self.N0(q), self.N1(q), self.N2(q),
+                                  self.N3(q), self.N4(q), self.N5(q),
+                                  self.N6(q), self.N7(q), self.N8(q)],
+                                 dtype=numpy.float64).reshape( (9,) )
+      print basis[iQuad]
+      deriv = numpy.array([[self.N0p(q), self.N0q(q)],
+                           [self.N1p(q), self.N1q(q)],
+                           [self.N2p(q), self.N2q(q)],
+                           [self.N3p(q), self.N3q(q)],
+                           [self.N4p(q), self.N4q(q)],
+                           [self.N5p(q), self.N5q(q)],
+                           [self.N6p(q), self.N6q(q)],
+                           [self.N7p(q), self.N7q(q)],
+                           [self.N8p(q), self.N8q(q)]])
+      basisDeriv[iQuad] = deriv.reshape((9, 2))
+      iQuad += 1
+
+    self.cellDim = 2
+    self.numCorners = len(vertices)
+    self.numQuadPts = len(quadPts)
+    self.vertices = vertices
+    self.quadPts = quadPts
+    self.quadWts = quadWts
+    self.basis = basis
+    self.basisDeriv = basisDeriv
+    return
+
+
+  def N0(self, p):
+    return (-0.5*p[0])*(1.0-p[0])*(-0.5*p[1])*(1.0-p[1])
+
+  def N0p(self, p):
+    return (p[0]-0.5)*(-0.5*p[1])*(1.0-p[1])
+
+  def N0q(self, p):
+    return (-0.5*p[0])*(1.0-p[0])*(p[1]-0.5)
+
+  def N1(self, p):
+    return (+0.5*p[0])*(1.0+p[0])*(-0.5*p[1])*(1.0-p[1])
+
+  def N1p(self, p):
+    return (p[0]+0.5)*(-0.5*p[1])*(1.0-p[1])
+
+  def N1q(self, p):
+    return (+0.5*p[0])*(1.0+p[0])*(p[1]-0.5)
+
+  def N2(self, p):
+    return (+0.5*p[0])*(1.0+p[0])*(+0.5*p[1])*(1.0+p[1])
+
+  def N2p(self, p):
+    return (p[0]+0.5)*(+0.5*p[1])*(1.0+p[1])
+
+  def N2q(self, p):
+    return (+0.5*p[0])*(1.0+p[0])*(p[1]+0.5)
+
+  def N3(self, p):
+    return (-0.5*p[0])*(1.0-p[0])*(+0.5*p[1])*(1.0+p[1])
+
+  def N3p(self, p):
+    return (p[0]-0.5)*(+0.5*p[1])*(1.0+p[1])
+
+  def N3q(self, p):
+    return (-0.5*p[0])*(1.0-p[0])*(p[1]+0.5)
+
+  def N4(self, p):
+    return (1.0-p[0]**2)*(-0.5*p[1])*(1.0-p[1])
+
+  def N4p(self, p):
+    return (-2.0*p[0])*(-0.5*p[1])*(1.0-p[1])
+
+  def N4q(self, p):
+    return (1.0-p[0]**2)*(p[1]-0.5)
+
+  def N5(self, p):
+    return (+0.5*p[0])*(1.0+p[0])*(1.0-p[1]**2)
+
+  def N5p(self, p):
+    return (p[0]+0.5)*(1.0-p[1]**2)
+
+  def N5q(self, p):
+    return (+0.5*p[0])*(1.0+p[0])*(-2.0*p[1])
+
+  def N6(self, p):
+    return (1.0-p[0]**2)*(+0.5*p[1])*(1.0+p[1])
+
+  def N6p(self, p):
+    return (-2.0*p[0])*(+0.5*p[1])*(1.0+p[1])
+
+  def N6q(self, p):
+    return (1.0-p[0]**2)*(p[1]+0.5)
+
+  def N7(self, p):
+    return (-0.5*p[0])*(1.0-p[0])*(1.0-p[1]**2)
+
+  def N7p(self, p):
+    return (p[0]-0.5)*(1.0-p[1]**2)
+
+  def N7q(self, p):
+    return (-0.5*p[0])*(1.0-p[0])*(-2.0*p[1])
+
+  def N8(self, p):
+    return (1.0-p[0]**2)*(1.0-p[1]**2)
+
+  def N8p(self, p):
+    return (-2.0*p[0])*(1.0-p[1]**2)
+
+  def N8q(self, p):
+    return (1.0-p[0]**2)*(-2.0*p[1])
+
 
 # ----------------------------------------------------------------------
 class Hex8(object):
@@ -366,11 +514,11 @@ class TestFIATLagrange(unittest.TestCase):
     """
     Test initialize() with line2 cell.
     """
-    from pylith.feassemble.FIATSimplex import FIATSimplex
     cell = FIATLagrange()
-    cell.cellDim = 1
-    cell.degree = 1
-    cell.order  = 1
+    cell.inventory.dimension = 1
+    cell.inventory.degree = 1
+    cell.inventory.order  = 1
+    cell._configure()
     cell.initialize(spaceDim=1)
 
     cellE = Line2()
@@ -384,11 +532,11 @@ class TestFIATLagrange(unittest.TestCase):
     """
     Test initialize() with line3 cell.
     """
-    from pylith.feassemble.FIATSimplex import FIATSimplex
     cell = FIATLagrange()
-    cell.cellDim = 1
-    cell.degree = 2
-    cell.order  = 2
+    cell.inventory.dimension = 1
+    cell.inventory.degree = 2
+    cell.inventory.order  = 2
+    cell._configure()
     cell.initialize(spaceDim=2)
 
     cellE = Line3()
@@ -402,14 +550,32 @@ class TestFIATLagrange(unittest.TestCase):
     """
     Test initialize() with quad4 cell.
     """
-    from pylith.feassemble.FIATSimplex import FIATSimplex
     cell = FIATLagrange()
-    cell.cellDim = 2
-    cell.degree = 1
-    cell.order  = 2
+    cell.inventory.dimension = 2
+    cell.inventory.degree = 1
+    cell.inventory.order  = 2
+    cell._configure()
     cell.initialize(spaceDim=2)
 
     cellE = Quad4()
+    self._checkVals(cellE, cell)
+    from pylith.feassemble.CellGeometry import GeometryQuad2D
+    self.failUnless(isinstance(cell.geometry, GeometryQuad2D))
+    return
+
+
+  def test_initialize_quad9(self):
+    """
+    Test initialize() with quad9 cell.
+    """
+    cell = FIATLagrange()
+    cell.inventory.dimension = 2
+    cell.inventory.degree = 2
+    cell.inventory.order  = 5
+    cell._configure()
+    cell.initialize(spaceDim=2)
+
+    cellE = Quad9()
     self._checkVals(cellE, cell)
     from pylith.feassemble.CellGeometry import GeometryQuad2D
     self.failUnless(isinstance(cell.geometry, GeometryQuad2D))
@@ -420,11 +586,11 @@ class TestFIATLagrange(unittest.TestCase):
     """
     Test initialize() with hex8 cell.
     """
-    from pylith.feassemble.FIATSimplex import FIATSimplex
     cell = FIATLagrange()
-    cell.cellDim = 3
-    cell.degree = 1
-    cell.order  = 2
+    cell.inventory.dimension = 3
+    cell.inventory.degree = 1
+    cell.inventory.order  = 2
+    cell._configure()
     cell.initialize(spaceDim=3)
 
     cellE = Hex8()
@@ -445,7 +611,7 @@ class TestFIATLagrange(unittest.TestCase):
 
   def _checkVals(self, cellE, cell):
     """
-    Check known values against those generated by FIATSimplex.
+    Check known values against those generated by FIATLagrange.
     """
     
     # Check basic attributes
