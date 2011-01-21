9520b311b44b1289ca5306e0b3c2b2e7b6c02f1c
Brad Aagaard <baagaard@usgs.gov>
2011-01-21 01:33:59 +0000

    Added unit tests for HDF5 external datasets and material mesh data.

---
+++ unittests/libtests/meshio/TestDataWriterHDF5ExtMatMeshCases.cc
@@ -0,0 +1,98 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestDataWriterHDF5ExtMatMeshCases.hh" // Implementation of class methods
+
+#include "data/DataWriterHDF5DataMatMeshLine2.hh"
+#include "data/DataWriterHDF5DataMatMeshTri3.hh"
+#include "data/DataWriterHDF5DataMatMeshQuad4.hh"
+#include "data/DataWriterHDF5DataMatMeshTet4.hh"
+#include "data/DataWriterHDF5DataMatMeshHex8.hh"
+
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestDataWriterHDF5ExtMatMeshLine2 );
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestDataWriterHDF5ExtMatMeshTri3 );
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestDataWriterHDF5ExtMatMeshQuad4 );
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestDataWriterHDF5ExtMatMeshTet4 );
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::meshio::TestDataWriterHDF5ExtMatMeshHex8 );
+
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::meshio::TestDataWriterHDF5ExtMatMeshLine2::setUp(void)
+{ // setUp
+  TestDataWriterHDF5ExtMesh::setUp();
+  _data = new DataWriterHDF5DataMatMeshLine2;
+  _flipFault = false;
+  _initialize();
+} // setUp
+
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::meshio::TestDataWriterHDF5ExtMatMeshTri3::setUp(void)
+{ // setUp
+  TestDataWriterHDF5ExtMesh::setUp();
+  _data = new DataWriterHDF5DataMatMeshTri3;
+  _flipFault = true;
+  _initialize();
+} // setUp
+
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::meshio::TestDataWriterHDF5ExtMatMeshQuad4::setUp(void)
+{ // setUp
+  TestDataWriterHDF5ExtMesh::setUp();
+  _data = new DataWriterHDF5DataMatMeshQuad4;
+  _flipFault = false;
+  _initialize();
+} // setUp
+
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::meshio::TestDataWriterHDF5ExtMatMeshTet4::setUp(void)
+{ // setUp
+  TestDataWriterHDF5ExtMesh::setUp();
+  _data = new DataWriterHDF5DataMatMeshTet4;
+  _flipFault = false;
+  _initialize();
+} // setUp
+
+
+// ----------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::meshio::TestDataWriterHDF5ExtMatMeshHex8::setUp(void)
+{ // setUp
+  TestDataWriterHDF5ExtMesh::setUp();
+  _data = new DataWriterHDF5DataMatMeshHex8;
+  _flipFault = true;
+  _initialize();
+} // setUp
+
+
+// End of file 
