79729cfc9574678afe04ab764ac4018072802b11
Matt Knepley <knepley>
2011-02-22 20:44:35 +0000

    Fixed Sieve types

--- libsrc/utils/sievetypes.hh
+++ libsrc/utils/sievetypes.hh
@@ -30,10 +30,10 @@
 namespace pylith {
 
   /// Sieve mesh.
-  typedef ALE::IMesh<> Mesh;
+  typedef ALE::IMesh<PetscInt,PetscScalar> Mesh;
 
   /// Sieve submesh.
-  typedef ALE::IMesh<ALE::LabelSifter<int, Mesh::point_type> > SubMesh;
+  typedef ALE::IMesh<PetscInt,PetscScalar,ALE::LabelSifter<int, Mesh::point_type> > SubMesh;
 
 } // pylith
 
