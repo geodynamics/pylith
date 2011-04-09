ad5c1901c37619a3fb2932421af3e36c9cbeab44
Brad Aagaard <brad>
2011-04-09 17:55:11 +0000

    Update version number.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from merlin import setup, find_packages
 setup(
     
     name = 'PyLith', 
-    version = '1.5.1',
+    version = '1.6.0',
 
     zip_safe = False,
     packages = find_packages(),
