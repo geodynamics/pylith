bf7f82508da0fa0b901061caa4a4741dfaefc87c
Brad Aagaard <baagaard@usgs.gov>
2011-09-21 01:00:48 +0000

    Started debugging revised fault implementation (fixed some bugs).

--- unittests/libtests/faults/data/CohesiveKinDataLine2.cc
+++ unittests/libtests/faults/data/CohesiveKinDataLine2.cc
@@ -133,7 +133,7 @@ const double pylith::faults::CohesiveKinDataLine2::_residualIncr[] = {
    7.5,
    0.0,
   -7.5,
-  -0.2+1.89546413727,
+   -0.2+1.89546413727,
 };
 
 const double pylith::faults::CohesiveKinDataLine2::_residual[] = {
@@ -141,7 +141,7 @@ const double pylith::faults::CohesiveKinDataLine2::_residual[] = {
    7.5, // 3
    0.0,
    -7.5, // 5
-  -0.2+1.89546413727,
+   -0.2+1.89546413727,
 };
 
 const double pylith::faults::CohesiveKinDataLine2::_jacobian[] = {
