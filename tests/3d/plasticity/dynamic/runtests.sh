7906a8c2b018abca5a16a0416bddba5d4934e36c
Brad Aagaard <brad>
2012-01-29 00:26:43 +0000

    Added tet4 cases for dynamic plasticity test.

--- tests/3d/plasticity/dynamic/runtests.sh
+++ tests/3d/plasticity/dynamic/runtests.sh
@@ -6,8 +6,8 @@ plot_data.py hex8 swave &
 pylith hex8.cfg pwave.cfg hex8_pwave.cfg
 plot_data.py hex8 pwave &
 
-#pylith tet4.cfg swave.cfg tet4_swave.cfg
-#plot_data.py tet4 swave &
+pylith tet4.cfg swave.cfg tet4_swave.cfg
+plot_data.py tet4 swave &
 
-#pylith tet4.cfg pwave.cfg tet4_pwave.cfg
-#plot_data.py tet4 pwave &
+pylith tet4.cfg pwave.cfg tet4_pwave.cfg
+plot_data.py tet4 pwave &
