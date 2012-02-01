c9da3bcb816c04695a00ae424697834b34c81c57
Brad Aagaard <baagaard@usgs.gov>
2012-02-01 18:57:02 +0000

    Fixed bug in updating state variables for slip weakening with time weakening. Bad previous merge from trunk.

--- libsrc/pylith/friction/SlipWeakeningTime.cc
+++ libsrc/pylith/friction/SlipWeakeningTime.cc
@@ -330,8 +330,8 @@ pylith::friction::SlipWeakeningTime::_updateStateVars(const PylithScalar t,
 
   // Don't reset state variables when sliding stops
   // (SCEC dynamic rupture branch only).
-  const double slipPrev = slip;
-  stateVars[s_slipPrev] = stateVars[s_slipCum];
+  const PylithScalar slipPrev = stateVars[s_slipPrev];
+  stateVars[s_slipPrev] = slip;
   stateVars[s_slipCum] += fabs(slip - slipPrev);
 } // _updateStateVars
 
