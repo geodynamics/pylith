a569e9831e70c704e7a34687e62ebb5ffde1c668
Brad Aagaard <brad>
2012-02-24 21:47:00 +0000

    Merge from trunk.

---
+++ doc/developer/updatecopyright.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for suffix in .cc .hh .am .py .i; do
+  for f in `find . -name "*$suffix"` configure.ac aclocal.m4 COPYING; do
+    sed -e "s/Copyright (c) 2010-2011 University of California, Davis/Copyright (c) 2010-2012 University of California, Davis/g" $f > tmp && mv -f tmp $f
+  done
+done
