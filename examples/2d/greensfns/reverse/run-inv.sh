47e117cce83e8616f8b59b6ca3bf444ffba3d23a
Charles Williams <C.Williams@gns.cri.nz>
2012-06-01 03:48:15 +0000

    Added inversion files for reverse case.

---
+++ examples/2d/greensfns/reverse/run-inv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./simple_invert.py --impulse_file=output/greensfns-fault.h5 --response_file=output/greensfns-points.h5 --data_file=output/eqsim-points.h5 --penalty_file=penalty_params.txt --output_file=reverse_inversion.txt
