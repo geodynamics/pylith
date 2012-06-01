0a1ee8f1b9850acdcc8e7e6bb6e6009ffbee92e4
Charles Williams <willic3>
2012-06-01 05:19:14 +0000

    Added simple python scripts to plot inversion results, and added stuff to README's.

---
+++ examples/2d/greensfns/reverse/make-plot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./plot_results.py --solution_file=output/eqsim-fault.h5 --predicted_file=reverse_inversion.txt
---
+++ examples/2d/greensfns/strikeslip/make-plot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./plot_results.py --solution_file=output/eqsim-fault.h5 --predicted_file=strikeslip_inversion.txt
