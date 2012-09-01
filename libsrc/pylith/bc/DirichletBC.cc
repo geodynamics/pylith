cde8561b0b2d8b0efd47fe834eee607dcaae1ae2
Matt Knepley <knepley>
2012-09-01 01:11:25 +0000

    Added in some DirichletBC support

--- libsrc/pylith/bc/DirichletBC.cc
+++ libsrc/pylith/bc/DirichletBC.cc
@@ -85,25 +85,46 @@ pylith::bc::DirichletBC::setConstraintSizes(const topology::Field<topology::Mesh
   const ALE::Obj<RealSection>& section = field.section();
   assert(!section.isNull());
 
+  PetscSection sectionP = field.petscSection();
+  PetscErrorCode err;
+  assert(sectionP);
+
   // Set constraints in field
   const int numPoints = _points.size();
   _offsetLocal.resize(numPoints);
   for (int iPoint=0; iPoint < numPoints; ++iPoint) {
-    const int fiberDim = section->getFiberDimension(_points[iPoint]);
-    const int curNumConstraints = 
-      section->getConstraintDimension(_points[iPoint]);
+    const PetscInt point        = _points[iPoint];
+    const int fiberDim          = section->getFiberDimension(point);
+    const int curNumConstraints = section->getConstraintDimension(point);
     if (curNumConstraints + numFixedDOF > fiberDim) {
       std::ostringstream msg;
       msg
 	<< "Found overly constrained point while setting up constraints for\n"
 	<< "DirichletBC boundary condition '" << _label << "'.\n"
-	<< "Number of DOF at point " << _points[iPoint] << " is " << fiberDim
+	<< "Number of DOF at point " << point << " is " << fiberDim
 	<< "\nand number of attempted constraints is "
 	<< curNumConstraints+numFixedDOF << ".";
       throw std::runtime_error(msg.str());
     } // if
     _offsetLocal[iPoint] = curNumConstraints;
-    section->addConstraintDimension(_points[iPoint], numFixedDOF);
+    section->addConstraintDimension(point, numFixedDOF);
+    PetscInt dof, cdof;
+
+    err = PetscSectionGetDof(sectionP, point, &dof);CHECK_PETSC_ERROR(err);
+    err = PetscSectionGetConstraintDof(sectionP, point, &cdof);CHECK_PETSC_ERROR(err);
+    if (cdof + numFixedDOF > dof) {
+      std::ostringstream msg;
+      msg
+	<< "Found overly constrained point while setting up constraints for\n"
+	<< "DirichletBC boundary condition '" << _label << "'.\n"
+	<< "Number of DOF at point " << point << " is " << dof
+	<< "\nand number of attempted constraints is " << cdof+numFixedDOF << ".";
+      throw std::runtime_error(msg.str());
+    } // if
+    _offsetLocal[iPoint] = cdof;
+    err = PetscSectionAddConstraintDof(sectionP, point, numFixedDOF);CHECK_PETSC_ERROR(err);
+    // We should be specifying what field the BC is for
+    //err = PetscSectionAddConstraintFieldDof(sectionP, point, field, numFixedDOF);CHECK_PETSC_ERROR(err);
   } // for
 
   // We only worry about the conventional DOF in a split field associated with
@@ -129,6 +150,10 @@ pylith::bc::DirichletBC::setConstraints(const topology::Field<topology::Mesh>& f
   const ALE::Obj<RealSection>& section = field.section();
   assert(!section.isNull());
 
+  PetscSection sectionP = field.petscSection();
+  PetscErrorCode err;
+  assert(sectionP);
+
   const int numPoints = _points.size();
   for (int iPoint=0; iPoint < numPoints; ++iPoint) {
     const SieveMesh::point_type point = _points[iPoint];
@@ -136,28 +161,35 @@ pylith::bc::DirichletBC::setConstraints(const topology::Field<topology::Mesh>& f
     // Get list of currently constrained DOF
     const int* curFixedDOF = section->getConstraintDof(point);
     const int numTotalConstrained = section->getConstraintDimension(point);
+    PetscInt cdof, *cInd;
+
+    err = PetscSectionGetConstraintDof(sectionP, point, &cdof);CHECK_PETSC_ERROR(err);
+    err = PetscSectionGetConstraintIndices(sectionP, point, &cInd);CHECK_PETSC_ERROR(err);
 
     // Create array holding all constrained DOF
     int_array allFixedDOF(curFixedDOF, numTotalConstrained);
+    int_array allCInd(cInd, cdof);
 
     // Verify other BC has not already constrained DOF
     const int numPrevious = _offsetLocal[iPoint];
     for (int iDOF=0; iDOF < numPrevious; ++iDOF)
       for (int jDOF=0; jDOF < numFixedDOF; ++jDOF)
-	if (allFixedDOF[iDOF] == _bcDOF[jDOF]) {
-	  std::ostringstream msg;
-	  msg << "Found multiple constraints on degrees of freedom at\n"
-	      << "point while setting up constraints for DirichletBC\n"
-	      << "boundary condition '" << _label << "'.\n"
+        if ((allFixedDOF[iDOF] == _bcDOF[jDOF]) || (allCInd[iDOF] == _bcDOF[jDOF])) {
+          std::ostringstream msg;
+          msg << "Found multiple constraints on degrees of freedom at\n"
+              << "point while setting up constraints for DirichletBC\n"
+              << "boundary condition '" << _label << "'.\n"
 	      << "Degree of freedom " << _bcDOF[jDOF] 
-	      << " is already constrained by another Dirichlet BC.";
-	  throw std::runtime_error(msg.str());
-	} // if
+              << " is already constrained by another Dirichlet BC.";
+          throw std::runtime_error(msg.str());
+        } // if
 
     // Add in the ones for this DirichletBC BC
     for (int iDOF=0; iDOF < numFixedDOF; ++iDOF) {
       assert(_offsetLocal[iPoint]+iDOF < numTotalConstrained);
+      assert(_offsetLocal[iPoint]+iDOF < cdof);
       allFixedDOF[_offsetLocal[iPoint]+iDOF] = _bcDOF[iDOF];
+      allCInd[_offsetLocal[iPoint]+iDOF] = _bcDOF[iDOF];
     } // for
 
     // Fill in rest of values not yet set (will be set by
@@ -168,13 +200,19 @@ pylith::bc::DirichletBC::setConstraints(const topology::Field<topology::Mesh>& f
       assert(iDOF < numTotalConstrained);
       allFixedDOF[iDOF] = 999;
     } // for
+    for (int iDOF=_offsetLocal[iPoint]+numFixedDOF; iDOF < cdof; ++iDOF) {
+      allCInd[iDOF] = 999;
+    } // for
 
     // Sort list of constrained DOF
     //   I need these sorted for my update algorithms to work properly
     std::sort(&allFixedDOF[0], &allFixedDOF[numTotalConstrained]);
+    std::sort(&allCInd[0], &allCInd[cdof]);
 
     // Update list of constrained DOF
     section->setConstraintDof(point, &allFixedDOF[0]);
+    err = PetscSectionSetConstraintIndices(sectionP, point, &allCInd[0]);CHECK_PETSC_ERROR(err);
+    //err = PetscSectionSetConstraintFieldIndices(sectionP, point, field, &fieldCInd[0]);CHECK_PETSC_ERROR(err);
   } // for
 
   // We only worry about the conventional DOF in a split field associated with
@@ -220,19 +258,34 @@ pylith::bc::DirichletBC::setField(const PylithScalar t,
   const int fiberDimension = 
     (numPoints > 0) ? fieldSection->getFiberDimension(_points[0]) : 0;
   scalar_array fieldVertex(fiberDimension);
+
+  PetscSection fieldSectionP = field.petscSection();
+  Vec          localVec      = field.localVector();
+  PetscErrorCode err;
+  assert(fieldSectionP);
   
   for (int iPoint=0; iPoint < numPoints; ++iPoint) {
     const SieveMesh::point_type p_bc = _points[iPoint];
 
-    fieldSection->restrictPoint(p_bc, &fieldVertex[0], fieldVertex.size());
-
     assert(parametersFiberDim == parametersSection->getFiberDimension(p_bc));
+    fieldSection->restrictPoint(p_bc, &fieldVertex[0], fieldVertex.size());
+    PetscInt dof, off;
+
+    err = PetscSectionGetDof(fieldSectionP, p_bc, &dof);CHECK_PETSC_ERROR(err);
+    err = PetscSectionGetOffset(fieldSectionP, p_bc, &off);CHECK_PETSC_ERROR(err);
+
     const PylithScalar* parametersVertex = parametersSection->restrictPoint(p_bc);
 
     for (int iDOF=0; iDOF < numFixedDOF; ++iDOF)
       fieldVertex[_bcDOF[iDOF]] = parametersVertex[valueIndex+iDOF];
 
     fieldSection->updatePointAll(p_bc, &fieldVertex[0]);
+    PetscScalar *array;
+
+    err = VecGetArray(localVec, &array);CHECK_PETSC_ERROR(err);
+    for (int iDOF=0; iDOF < numFixedDOF; ++iDOF)
+      array[_bcDOF[iDOF]] = parametersVertex[valueIndex+iDOF];
+    err = VecRestoreArray(localVec, &array);CHECK_PETSC_ERROR(err);
   } // for
 } // setField
 
