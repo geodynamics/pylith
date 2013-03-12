8383c5f4725d812a0360688c32cc4d5a7e5cb520
Brad Aagaard <baagaard@usgs.gov>
2013-03-12 18:36:55 +0000

    Improved visitors. Added Stratum helper class.

---
+++ libsrc/pylith/topology/Stratum.hh
@@ -0,0 +1,147 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2012 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ======================================================================
+//
+
+/**
+ * @file libsrc/topology/Stratum.hh
+ *
+ * @brief C++ helper classes for accessing statum related information in
+ * a finite-element mesh.
+ */
+
+#if !defined(pylith_topology_stratum_hh)
+#define pylith_topology_stratum_hh
+
+// Include directives ---------------------------------------------------
+#include "topologyfwd.hh" // forward declarations
+
+#include "pylith/utils/petscfwd.h" // HASA PetscDM, PetscIS
+
+#include <petscdmmesh.hh>
+
+// Stratum --------------------------------------------------------
+/// Height or depth stratum.
+template<typename mesh_type>
+class pylith::topology::Stratum
+{ // Stratum
+  friend class TestStratum; // unit testing
+
+// PUBLIC ENUMS /////////////////////////////////////////////////////////
+public :
+
+  /// Type of stratum (height or depth).
+  enum StratumEnum {
+    HEIGHT=0,
+    DEPTH=1,
+  }; // StratumEnum
+
+// PUBLIC METHODS ///////////////////////////////////////////////////////
+public :
+
+  /** Default constructor.
+   *
+   * @param mesh Finite-element mesh.
+   * @param stype Type of stratum [HEIGHT, DEPTH].
+   * @param level Height of depth of stratum.
+   */
+  Stratum(const mesh_type& mesh,
+	  const StratumEnum stype,
+	  const int level);
+
+  /// Default destructor.
+  ~Stratum(void);
+
+  /** Get starting point.
+   *
+   * @return Index of starting point.
+   */
+  PetscInt begin(void) const;
+
+  /** Get ending point.
+   *
+   * @return Index of ending point.
+   */
+  PetscInt end(void) const;
+
+  /** Get number of points in stratum.
+   *
+   * @return Number of points.
+   */
+  PetscInt size(void) const;
+
+// PRIVATE MEMBERS //////////////////////////////////////////////////////
+private :
+
+  PetscInt _begin; ///< Starting point.
+  PetscInt _end; ///< End point.
+
+}; // HeightStratum
+
+
+// StratumIS ------------------------------------------------------------
+/// Index set associated with stratum (usually over label of points).
+template<typename mesh_type>
+class pylith::topology::StratumIS
+{ // StratumIS
+  friend class TestStratumIS; // unit testing
+
+// PUBLIC METHODS ///////////////////////////////////////////////////////
+public :
+
+  /** Default constructor.
+   *
+   * @param mesh Finite-element mesh.
+   * @param label Label for stratum.
+   * @param id Value of label defining stratum.
+   */
+  StratumIS(const mesh_type& mesh,
+	    const char* label,
+	    const int id);
+
+  /// Default destructor.
+  ~StratumIS(void);
+
+  /// Deallocate data.
+  void deallocate(void);
+
+  /** Get array of points.
+   *
+   * @return Array of points.
+   */
+  const PetscInt* points(void) const;
+
+  /** Get number of points in index set.
+   *
+   * @return Number of points.
+   */
+  PetscInt size(void) const;
+
+// PRIVATE MEMBERS //////////////////////////////////////////////////////
+private :
+
+  PetscIS _indexSet; ///< PETSc index set.
+  PetscInt _size; ///< Size of index set.
+  const PetscInt* _points; ///< Array of points in index set.
+
+}; // StratumIS
+
+#include "Stratum.icc"
+
+#endif // pylith_topology_stratum_hh
+
+
+// End of file
---
+++ libsrc/pylith/topology/SubMeshVisitor.hh
@@ -0,0 +1,206 @@
+// -*- C++ -*-
+//
+// ======================================================================
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2012 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ======================================================================
+//
+
+/**
+ * @file libsrc/topology/SubMeshVisitor.hh
+ *
+ * @brief C++ helper class for accessing field values at points in a
+ * submesh within a finite-element mesh.
+ *
+ * This visitor is used to access values associated with a submesh
+ * when the field is defined over the entire mesh. This is why a
+ * submesh and index set are passed to the constructor.
+ *
+ * Use the PointVisitorMesh object when the field and mesh/submesh are
+ * defined over the same set of points (i.e., field over a submesh or field
+ * of a mesh).
+ */
+
+#if !defined(pylith_topology_submeshvisitor_hh)
+#define pylith_topology_submeshvisitor_hh
+
+// Include directives ---------------------------------------------------
+#include "topologyfwd.hh" // forward declarations
+
+#include "pylith/utils/petscfwd.h" // HASA PetscVec, PetscSection
+
+#include <petscdmmesh.hh>
+
+// SubMeshVisitor -------------------------------------------------------
+/** @brief Helper class for accessing field values at points in a
+ *  finite-element mesh.
+ */
+template<typename field_type>
+class pylith::topology::SubMeshVisitor
+{ // SubMeshVisitor
+  friend class TestSubMeshVisitor; // unit testing
+
+// PUBLIC METHODS ///////////////////////////////////////////////////////
+public :
+
+  /** Default constructor.
+   *
+   * @param field Field associated with visitor.
+   * @param submeshIS Submesh index set associated with visitor.
+   */
+  SubMeshVisitor(const field_type& field,
+		 const SubMeshIS& submeshIS);
+
+  /// Default destructor
+  ~SubMeshVisitor(void);
+
+  /* Initialize cached data.
+   *
+   * @param submeshIS Submesh index set associated with visitor.
+   */
+  void initialize(const SubMeshIS& submeshIS);
+
+  /// Clear cached data.
+  void clear(void);
+  
+  /** Get the array of values associated with the local PETSc Vec.
+   * 
+   * @returns Array of values.
+   */
+  PetscScalar* localArray(void) const;
+
+  /** Get the PETSc section.
+   * 
+   * @returns PETSc section.
+   */
+  PetscSection petscSection(void) const;
+
+  /** Get the local PETSc Vec.
+   * 
+   * @returns PETSc Vec.
+   */
+  PetscVec localVec(void) const;
+
+  /** Get fiber dimension of coordinates for point.
+   *
+   * @param point Point in mesh.
+   * @returns Fiber dimension.
+   */
+  PetscInt sectionDof(const PetscInt point) const;
+
+  /** Get offset into coordinates array for point.
+   *
+   * @param point Point in mesh.
+   * @returns Offset.
+   */
+  PetscInt sectionOffset(const PetscInt point) const;
+
+  /** Get array of values associated with closure.
+   *
+   * @param valuesCell Array of values for cell.
+   * @param valuesSize Size of values array.
+   * @param cell Finite-element cell.
+   */
+  void getClosure(PetscScalar** valuesCell,
+		  PetscInt* valuesSize,
+		  const PetscInt cell) const;
+
+  /** Restore array of values associated with closure.
+   *
+   * @param valuesCell Array of values for cell.
+   * @param valuesSize Size of values array.
+   * @param cell Finite-element cell.
+   */
+  void restoreClosure(PetscScalar** valuesCell,
+		      PetscInt* valuesSize,
+		      const PetscInt cell) const;
+
+  /** Set values associated with closure.
+   *
+   * @param valuesCell Array of values for cell.
+   * @param valuesSize Size of values array.
+   * @param cell Finite-element cell.
+   * @param mode Mode for inserting values.
+   */
+  void setClosure(const PetscScalar* valuesCell,
+		  const PetscInt valuesSize,
+		  const PetscInt cell,
+		  const InsertMode mode) const;
+
+// PRIVATE MEMBERS //////////////////////////////////////////////////////
+private :
+
+  const field_type& _field;
+
+  PetscDM _dm; ///< Cached PETSc dm for mesh.
+  PetscVec _localVec; ///< Cached local PETSc Vec.
+  PetscSection _section; ///< Cached PETSc subsection.
+  PetscScalar* _localArray; ///< Cached local array
+
+// NOT IMPLEMENTED //////////////////////////////////////////////////////
+private :
+
+  SubMeshVisitor(const SubMeshVisitor&); ///< Not implemented
+  const SubMeshVisitor& operator=(const SubMeshVisitor&); ///< Not implemented
+
+}; // SubMeshVisitor
+
+
+// SubMeshIS ------------------------------------------------------------
+/// Index set associated with submesh.
+class pylith::topology::SubMeshIS
+{ // SubMeshIS
+  friend class TestSubMeshIS; // unit testing
+
+// PUBLIC METHODS ///////////////////////////////////////////////////////
+public :
+
+  /** Default constructor.
+   * 
+   * @param submesh Submesh associated with index set.
+   */
+  SubMeshIS(const SubMesh& submesh);
+
+  /// Default destructor.
+  ~SubMeshIS(void);
+
+  /// Deallocate.
+  void deallocate(void);
+
+  /** Get the submesh.
+   *
+   * @returns Submesh.
+   */
+  const SubMesh& submesh(void) const;
+
+  /** Get PETSc index set.
+   *
+   * @returns PETSc index set.
+   */
+  PetscIS indexSet(void) const;
+
+// PRIVATE MEMBERS //////////////////////////////////////////////////////
+private :
+
+  const SubMesh& _submesh;
+  PetscIS _indexSet; ///< PETSc index set.
+
+}; // SubMeshIS
+
+
+#include "SubMeshVisitor.icc"
+
+#endif // pylith_topology_submeshvisitor_hh
+
+
+// End of file
