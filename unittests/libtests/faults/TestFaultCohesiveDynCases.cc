db3a05fa959596de5c006b18b7915469a1b3b7c1
Brad Aagaard <brad>
2013-05-07 17:56:39 +0000

    Consolidated test cases.

---
+++ unittests/libtests/faults/TestFaultCohesiveDynCases.cc
@@ -0,0 +1,144 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2013 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestFaultCohesiveDynCases.hh" // Implementation of class methods
+
+#include "pylith/topology/SubMesh.hh" // USES SubMesh
+#include "pylith/feassemble/Quadrature.hh" // USES Quadrature<SubMesh>
+#include "pylith/feassemble/GeometryLine2D.hh" // USES GeometryLine2D
+#include "pylith/feassemble/GeometryTri3D.hh" // USES GeometryTri3D
+#include "pylith/feassemble/GeometryQuad3D.hh" // USES GeometryQuad3D
+
+// ----------------------------------------------------------------------
+#include "data/CohesiveDynDataTri3.hh" // USES CohesiveDynDataTri3
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::faults::TestFaultCohesiveDynTri3 );
+
+// Setup testing data.
+void
+pylith::faults::TestFaultCohesiveDynTri3::setUp(void)
+{ // setUp
+  PYLITH_METHOD_BEGIN;
+
+  TestFaultCohesiveDyn::setUp();
+  _data = new CohesiveDynDataTri3();
+
+  CPPUNIT_ASSERT(_quadrature);
+  feassemble::GeometryLine2D geometry;
+  _quadrature->refGeometry(&geometry);
+  
+  _flipFault = true;
+
+  PYLITH_METHOD_END;
+} // setUp
+
+
+// ----------------------------------------------------------------------
+#include "data/CohesiveDynDataTri3d.hh" // USES CohesiveDynDataTri3d
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::faults::TestFaultCohesiveDynTri3d );
+
+// Setup testing data.
+void
+pylith::faults::TestFaultCohesiveDynTri3d::setUp(void)
+{ // setUp
+  PYLITH_METHOD_BEGIN;
+
+  TestFaultCohesiveDyn::setUp();
+  _data = new CohesiveDynDataTri3d();
+
+  CPPUNIT_ASSERT(_quadrature);
+  feassemble::GeometryLine2D geometry;
+  _quadrature->refGeometry(&geometry);
+  
+  _flipFault = true;
+
+  PYLITH_METHOD_END;
+} // setUp
+
+
+// ----------------------------------------------------------------------
+#include "data/CohesiveDynDataQuad4.hh" // USES CohesiveDynDataQuad4
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::faults::TestFaultCohesiveDynQuad4 );
+
+// Setup testing data.
+void
+pylith::faults::TestFaultCohesiveDynQuad4::setUp(void)
+{ // setUp
+  PYLITH_METHOD_BEGIN;
+
+  TestFaultCohesiveDyn::setUp();
+  _data = new CohesiveDynDataQuad4();
+
+  CPPUNIT_ASSERT(_quadrature);
+  feassemble::GeometryLine2D geometry;
+  _quadrature->refGeometry(&geometry);
+  
+  _flipFault = true;
+
+  PYLITH_METHOD_END;
+} // setUp
+
+
+// ----------------------------------------------------------------------
+#include "data/CohesiveDynDataTet4.hh" // USES CohesiveDynDataTet4
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::faults::TestFaultCohesiveDynTet4 );
+
+// Setup testing data.
+void
+pylith::faults::TestFaultCohesiveDynTet4::setUp(void)
+{ // setUp
+  PYLITH_METHOD_BEGIN;
+
+  TestFaultCohesiveDyn::setUp();
+  _data = new CohesiveDynDataTet4();
+
+  CPPUNIT_ASSERT(_quadrature);
+  feassemble::GeometryTri3D geometry;
+  _quadrature->refGeometry(&geometry);
+  
+  _flipFault = true;
+
+  PYLITH_METHOD_END;
+} // setUp
+
+
+// ----------------------------------------------------------------------
+#include "data/CohesiveDynDataHex8.hh" // USES CohesiveDynLDataHex8
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::faults::TestFaultCohesiveDynHex8 );
+
+// Setup testing data.
+void
+pylith::faults::TestFaultCohesiveDynHex8::setUp(void)
+{ // setUp
+  PYLITH_METHOD_BEGIN;
+
+  TestFaultCohesiveDyn::setUp();
+  _data = new CohesiveDynDataHex8();
+
+  CPPUNIT_ASSERT(_quadrature);
+  feassemble::GeometryQuad3D geometry;
+  _quadrature->refGeometry(&geometry);
+  
+  _flipFault = true;
+
+  PYLITH_METHOD_END;
+} // setUp
+
+
+// End of file 
---
+++ unittests/libtests/faults/TestFaultCohesiveKinCases.hh
@@ -0,0 +1,287 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2013 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+/**
+ * @file unittests/libtests/faults/TestFaultCohesiveKinCases.hh
+ *
+ * @brief C++ unit testing for FaultCohesiveKin.
+ */
+
+#if !defined(pylith_faults_testfaultcohesivekincases_hh)
+#define pylith_faults_testfaultcohesivekincases_hh
+
+#include "TestFaultCohesiveKin.hh" // ISA TestFaultCohesiveKin
+
+/// Namespace for pylith package
+namespace pylith {
+  namespace faults {
+    class TestFaultCohesiveKinLine2;
+    class TestFaultCohesiveKinTri3;
+    class TestFaultCohesiveKinTri3d;
+    class TestFaultCohesiveKinQuad4;
+    class TestFaultCohesiveKinQuad4e;
+    class TestFaultCohesiveKinTet4;
+    class TestFaultCohesiveKinTet4e;
+    class TestFaultCohesiveKinTet4f;
+    class TestFaultCohesiveKinHex8;
+  } // bc
+} // pylith
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 1-D line cells.
+class pylith::faults::TestFaultCohesiveKinLine2 : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinLine2
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinLine2 );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testAdjustSolnLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinLine2
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 2-D triangular cells.
+class pylith::faults::TestFaultCohesiveKinTri3 : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinTri3
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinTri3 );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testAdjustSolnLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinTri3
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 2-D triangular cells.
+class pylith::faults::TestFaultCohesiveKinTri3d : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinTri3d
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinTri3d );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinTri3d
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 2-D quadrilateral cells.
+class pylith::faults::TestFaultCohesiveKinQuad4 : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinQuad4
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinQuad4 );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testAdjustSolnLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinQuad4
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 2-D quadrilateral cells.
+class pylith::faults::TestFaultCohesiveKinQuad4e : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinQuad4e
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinQuad4e );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinQuad4e
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 3-D tetrahedral cells.
+class pylith::faults::TestFaultCohesiveKinTet4 : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinTet4
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinTet4 );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testAdjustSolnLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinTet4
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 3-D tetrahedral cells.
+class pylith::faults::TestFaultCohesiveKinTet4e : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinTet4e
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinTet4e );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinTet4e
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 3-D tetrahedral cells.
+class pylith::faults::TestFaultCohesiveKinTet4f : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinTet4f
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinTet4f );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinTet4f
+
+
+// ----------------------------------------------------------------------
+/// C++ unit testing for FaultCohesiveKin for mesh with 3-D hex cells.
+class pylith::faults::TestFaultCohesiveKinHex8 : public TestFaultCohesiveKin
+{ // class TestFaultCohesiveKinHex8
+
+  // CPPUNIT TEST SUITE /////////////////////////////////////////////////
+  CPPUNIT_TEST_SUITE( TestFaultCohesiveKinHex8 );
+
+  CPPUNIT_TEST( testInitialize );
+  CPPUNIT_TEST( testIntegrateResidual );
+  CPPUNIT_TEST( testIntegrateJacobian );
+  CPPUNIT_TEST( testIntegrateJacobianLumped );
+  CPPUNIT_TEST( testAdjustSolnLumped );
+  CPPUNIT_TEST( testCalcTractionsChange );
+  CPPUNIT_TEST( testSplitField );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  // PUBLIC METHODS /////////////////////////////////////////////////////
+public :
+
+  /// Setup testing data.
+  void setUp(void);
+
+}; // class TestFaultCohesiveKinHex8
+
+
+#endif // pylith_faults_testfaultcohesivecases_hh
+
+
+// End of file 
