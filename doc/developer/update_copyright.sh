e44750845bc8ee5a313c7e034a065643f0ea2410
Brad Aagaard <baagaard@usgs.gov>
2013-06-11 23:09:27 +0000

    Updated copyright in journal files.

--- doc/developer/update_copyright.sh
+++ doc/developer/update_copyright.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for suffix in .cc .icc .hh .h .am .py .i; do
+for suffix in .cc .icc .hh .h .am .py .i .jou; do
   for f in `find . -name "*$suffix"` configure.ac aclocal.m4 COPYING; do
     sed -e "s/Copyright (c) 2010-2011 University of California, Davis/Copyright (c) 2010-2013 University of California, Davis/g" $f > tmp && mv -f tmp $f
     sed -e "s/Copyright (c) 2010-2012 University of California, Davis/Copyright (c) 2010-2013 University of California, Davis/g" $f > tmp && mv -f tmp $f
