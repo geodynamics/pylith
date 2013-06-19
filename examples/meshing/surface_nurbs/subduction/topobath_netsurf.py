02493918169f58a042c28ee82397350e591687b8
Charles Williams <C.Williams@gns.cri.nz>
2013-06-19 05:28:39 +0000

    Added topography and started on geometry and mesh generation. Right now, things are taking too long on my laptop.

--- examples/meshing/surface_nurbs/subduction/topobath_netsurf.py
+++ examples/meshing/surface_nurbs/subduction/topobath_netsurf.py
@@ -22,36 +22,36 @@ import numpy
 # pdb.set_trace()
 
 # Define parameters.
-splayFile = "splay_points.txt"
-numProfs = 5
-pointsPerProf = 4
-journalFile = "splay_skinsurf.jou"
-acisFile = "splay_surf.sat"
+demFile = "topobath_points.txt"
+numProfs = 4
+pointsPerProf = 9
+journalFile = "topobath_netsurf.jou"
+acisFile = "topobath_surf.sat"
 
 # Journal file formatting, etc.
 separator = "# ----------------------------------------------------------\n"
 journalBeg = \
            "reset\n" + \
            "# This is a simple Cubit journal file to create an ACIS\n" + \
-           "# NURBS surface from a set of profiles.\n" + \
+           "# NURBS surface from a set of intersecting lines.\n" + \
            "#\n" + separator
 lineBeg = "create curve spline"
 splineFmt = " location %15.11e %15.11e %15.11e"
-skinCmd = "create surface skin curve all\n"
+netFmt = "create surface net u curve %d to %d v curve %d to %d\n"
 delCmd = "delete curve all\n"
 expCmd = "export Acis '" + acisFile + "'\n"
 
 
 # Read coordinates and reshape them.
-splayCoords = numpy.loadtxt(splayFile, dtype=numpy.float64).reshape(
+demCoords = numpy.loadtxt(demFile, dtype=numpy.float64).reshape(
     numProfs, pointsPerProf, 3)
 
 j = open(journalFile, 'w')
 j.write(journalBeg)
 
-# Loop over profiles.
+# Loop over profiles (u-lines).
 for profile in range(numProfs):
-    points = splayCoords[profile,:,:]
+    points = demCoords[profile,:,:]
     j.write(lineBeg)
     for pointNum in range(pointsPerProf):
         point = points[pointNum,:]
@@ -59,8 +59,22 @@ for profile in range(numProfs):
 
     j.write("\n")
 
-# Create skin surface.
-j.write(skinCmd)
+# Loop over contours (v-lines).
+for contour in range(pointsPerProf):
+    points = demCoords[:,contour,:]
+    j.write(lineBeg)
+    for pointNum in range(numProfs):
+        point = points[pointNum,:]
+        j.write(splineFmt % (point[0], point[1], point[2]))
+
+    j.write("\n")
+
+# Create net surface.
+uline1 = 1
+uline2 = numProfs
+vline1 = numProfs + 1
+vline2 = vline1 + pointsPerProf - 1
+j.write(netFmt % (uline1, uline2, vline1, vline2))
 
 # Delete spline curves and export Acis file.
 j.write(delCmd)
