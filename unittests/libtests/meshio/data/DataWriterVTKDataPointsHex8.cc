f4fc4fbe8988e18f1e97a95b95ecae5a8ad9b7e9
Brad Aagaard <baagaard@usgs.gov>
2013-11-05 15:56:54 -0800

    Update meshio test data for interpolated meshes.
    Also includes some flipping of fault and permutations in topology.

--- unittests/libtests/meshio/data/DataWriterVTKDataPointsHex8.cc
+++ unittests/libtests/meshio/data/DataWriterVTKDataPointsHex8.cc
@@ -38,7 +38,7 @@ const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_time = 1.0;
 const char* pylith::meshio::DataWriterVTKDataPointsHex8::_timeFormat = 
   "%3.1f";
 
-const int pylith::meshio::DataWriterVTKDataPointsHex8::_numVertices = 20;
+const int pylith::meshio::DataWriterVTKDataPointsHex8::_numVertices = 16;
 
 const pylith::meshio::DataWriterData::FieldStruct
 pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFields[4] = {
@@ -47,7 +47,7 @@ pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFields[4] = {
   { "stress", topology::FieldBase::TENSOR, 6 },
   { "other", topology::FieldBase::OTHER, 2 },
 };
-const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldScalar[20*1] = {
+const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldScalar[16*1] = {
   2.1, // 0
   3.2, // 1
   4.3, // 2 
@@ -64,12 +64,8 @@ const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldScal
   15.1, // 13
   16.1, // 14
   17.1, // 15
-  18.1, // 16
-  19.1, // 17
-  20.1, // 18
-  21.2, // 19
 };
-const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldVector[20*3] = {
+const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldVector[16*3] = {
   1.1, 2.2, 3.3, // 0
   4.4, 5.5, 6.6, // 1
   7.7, 8.8, 9.9, // 2
@@ -86,12 +82,8 @@ const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldVect
   16.8, 17.9, 18.1, // 13
   19.2, 20.3, 21.4, // 14
   22.5, 23.6, 24.7, // 15
-  25.8, 26.9, 27.1, // 16
-  28.8, 29.9, 30.1, // 17
-  31.8, 32.9, 33.1, // 18
-  34.8, 35.9, 36.1, // 19
 };
-const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldTensor[20*6] = {
+const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldTensor[16*6] = {
   1.1, 1.2, 1.3, 1.4, 1.5, 1.6,
   2.1, 2.2, 2.3, 2.4, 2.5, 2.6,
   3.1, 3.2, 3.3, 3.4, 3.5, 3.6,
@@ -108,12 +100,8 @@ const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldTens
   14.1, 14.2, 14.3, 14.4, 14.5, 14.6,
   15.1, 15.2, 15.3, 15.4, 15.5, 15.6,
   16.1, 16.2, 16.3, 16.4, 16.5, 16.6,
-  17.1, 17.2, 17.3, 17.4, 17.5, 17.6,
-  18.1, 18.2, 18.3, 18.4, 18.5, 18.6,
-  19.1, 19.2, 19.3, 19.4, 19.5, 19.6,
-  20.1, 20.2, 20.3, 20.4, 20.5, 20.6,
 };
-const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldOther[20*2] = {
+const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldOther[16*2] = {
   1.2, 2.3, // 0
   3.4, 4.5, // 1
   5.6, 6.7, // 2
@@ -130,10 +118,6 @@ const PylithScalar pylith::meshio::DataWriterVTKDataPointsHex8::_vertexFieldOthe
   4.8, 1.5, // 13
   2.6, 3.7, // 14
   4.8, 5.9, // 15
-  6.1, 7.2, // 16
-  7.1, 8.2, // 17
-  8.1, 9.2, // 18
-  9.1, 10.1, // 19
 };
 
 const int pylith::meshio::DataWriterVTKDataPointsHex8::_numPoints = 4;
