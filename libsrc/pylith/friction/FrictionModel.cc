954bfb2930acc92ce208aae0a6f34cc810569795
Brad Aagaard <baagaard@usgs.gov>
2014-07-15 09:33:24 -0700

    Fixed bug in overzealous size checking.
    Overzealous size checking of stateVarsVertex. Small code formatting
    cleanup.

--- libsrc/pylith/friction/FrictionModel.cc
+++ libsrc/pylith/friction/FrictionModel.cc
@@ -182,17 +182,16 @@ pylith::friction::FrictionModel::initialize(const topology::Mesh& faultMesh,
 
   // Query database for initial state variables
   if (_dbInitialState) {
-    assert(_varsFiberDim > 0);
 
-    // Create arrays for querying
-    const int numDBStateVars = _metadata.numDBStateVars();
+    // Create arrays for querying    
+    const int numDBStateVars = _metadata.numDBStateVars();assert(numDBStateVars > 0);
+    assert(_varsFiberDim > 0);
     scalar_array stateVarsDBQuery(numDBStateVars);
     scalar_array stateVarsVertex(_varsFiberDim);
     
     // Setup database for querying for initial state variables
     _dbInitialState->open();
-    _dbInitialState->queryVals(_metadata.dbStateVars(),
-			       _metadata.numDBStateVars());
+    _dbInitialState->queryVals(_metadata.dbStateVars(), _metadata.numDBStateVars());
     
     PetscDMLabel clamped = NULL;
     PetscErrorCode err = DMPlexGetLabel(faultDMMesh, "clamped", &clamped);PYLITH_CHECK_ERROR(err);
@@ -209,8 +208,7 @@ pylith::friction::FrictionModel::initialize(const topology::Mesh& faultMesh,
       } // for
       _normalizer->dimensionalize(&coordsVertexGlobal[0], coordsVertexGlobal.size(), lengthScale);
       
-      int err = _dbInitialState->query(&stateVarsDBQuery[0], numDBStateVars,
-				       &coordsVertexGlobal[0], spaceDim, cs);
+      int err = _dbInitialState->query(&stateVarsDBQuery[0], numDBStateVars, &coordsVertexGlobal[0], spaceDim, cs);
       if (err) {
         std::ostringstream msg;
         msg << "Could not find initial state variables at " << "(";
@@ -231,8 +229,6 @@ pylith::friction::FrictionModel::initialize(const topology::Mesh& faultMesh,
 	PetscScalar* stateVarArray = stateVarVisitor.localArray();
 	const PetscInt off = stateVarVisitor.sectionOffset(v);
 	const PetscInt dof = stateVarVisitor.sectionDof(v);
-	std::cout << "v: " << v << ", dof: " << dof << ", stateVarsVetex: " << stateVarsVertex.size() << std::endl;
-	assert(stateVarsVertex.size() == dof);
         for(PetscInt d = 0; d < dof; ++d, ++iOff) {
           stateVarArray[off+d] += stateVarsVertex[iOff];
         } // for
