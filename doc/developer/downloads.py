8afe5a54f60219b3f90f107f987aa7903791803d
Brad Aagaard <baagaard@usgs.gov>
2015-02-20 09:37:28 -0800

    Add other PyLith related packages. Improve format of output.

--- doc/developer/downloads.py
+++ doc/developer/downloads.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 
-url = "https://api.github.com/repos/geodynamics/pylith/releases"
+packages = ["pylith","pylith_installer","spatialdata"]
+baseurl = "https://api.github.com/repos/geodynamics/%s/releases"
 
 import urllib2
-raw = urllib2.urlopen(url).read()
-
 import json
-data = json.loads(raw)
-count = 0
-for release in data:
-    print release['name']
-    for asset in release['assets']:
-        print "    ",asset['name'],asset['download_count']
-        if not "petsc" in asset['name'] and not "manual" in asset['name'] and not "installer" in asset['name']:
-            count += asset['download_count']
-print "Total downloads:",count
+
+for package in packages:
+    raw = urllib2.urlopen(baseurl % package).read()
+
+    data = json.loads(raw)
+    count = 0
+    print("\n%s" % package)
+    for release in data:
+        print("    %s" % release['name'])
+        for asset in release['assets']:
+            print("        %s: %d" % (asset['name'],asset['download_count']))
+            if package == "pylith":
+                if not "petsc" in asset['name'] and not "manual" in asset['name'] and not "installer" in asset['name']:
+                    count += asset['download_count']
+            else:
+                count += asset['download_count']
+    print("  Total downloads: %d" % count)
