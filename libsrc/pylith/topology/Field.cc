31068cd51eb5382b470efbf522c6848f8a514776
Matthew G. Knepley <knepley@gmail.com>
2015-05-19 09:28:01 -0500

    Field: Getting closer to working - The default section should not be set. I am having to erase it currently - Somewhere a work array is still checked out

--- libsrc/pylith/topology/Field.cc
+++ libsrc/pylith/topology/Field.cc
@@ -1287,17 +1287,18 @@ pylith::topology::Field::subfieldsSetup(void)
 
   const PylithInt numSubfields = _subfields.size();
   err = DMGetDS(_dm, &prob);PYLITH_CHECK_ERROR(err);
-  err = DMGetDefaultSection(_dm, &section);PYLITH_CHECK_ERROR(err);assert(section);
-  err = PetscSectionSetNumFields(section, numSubfields);PYLITH_CHECK_ERROR(err);
-  err = DMSetNumFields(_dm, numSubfields);PYLITH_CHECK_ERROR(err);
+  //err = DMGetDefaultSection(_dm, &section);PYLITH_CHECK_ERROR(err);assert(section);
+  // MATT: I should not need to do this, somewhere the default section is getting set
+  err = DMSetDefaultSection(_dm, NULL);PYLITH_CHECK_ERROR(err);
+  err = DMSetNumFields(_dm, _subfields.size());PYLITH_CHECK_ERROR(err);
 
   for(subfields_type::const_iterator s_iter = _subfields.begin(); s_iter != _subfields.end(); ++s_iter) {
     PetscObject sobj;
     const char* sname = s_iter->first.c_str();
     const SubfieldInfo& sinfo = s_iter->second;
-    err = PetscSectionSetFieldName(section, sinfo.index, sname);PYLITH_CHECK_ERROR(err);
-    err = PetscSectionSetFieldComponents(section, sinfo.index, sinfo.numComponents);PYLITH_CHECK_ERROR(err);
-    err = DMGetField(_dm, sinfo.index, &sobj);PYLITH_CHECK_ERROR(err);assert(section);
+    //err = PetscSectionSetFieldName(section, sinfo.index, sname);PYLITH_CHECK_ERROR(err);
+    //err = PetscSectionSetFieldComponents(section, sinfo.index, sinfo.numComponents);PYLITH_CHECK_ERROR(err);
+    err = DMGetField(_dm, sinfo.index, &sobj);PYLITH_CHECK_ERROR(err);assert(sobj);
     err = PetscObjectSetName(sobj, sname);PYLITH_CHECK_ERROR(err);
 
     PetscFE fe = FieldOps::createFE(sinfo.fe, _dm, _mesh.isSimplex(), sinfo.numComponents);
