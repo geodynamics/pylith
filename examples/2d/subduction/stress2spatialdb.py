3cb00de778ca0320999e7cd849b601a569ae2fbb
Brad Aagaard <baagaard@usgs.gov>
2015-08-05 17:01:34 -0700

    Setup of initial stresses from gravity.

---
+++ examples/2d/subduction/stress2spatialdb.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+This script creates an initial stress spatial database from output stress
+results.
+"""
+
+materials = ["concrust","oceancrust","conmantle","oceanmantle"]
+
+# The code requires the numpy and h5py packages.
+import numpy
+import h5py
+
+# Create coordinate system for spatial database
+from spatialdata.spatialdb.SimpleIOAscii import SimpleIOAscii
+from spatialdata.geocoords.CSCart import CSCart
+cs = CSCart()
+cs._configure()
+cs.setSpaceDim(2)
+
+for material in materials:
+
+  filenameH5 = "output/step05-%s.h5" % material
+  filenameDB = "grav_stress-%s.spatialdb" % material
+
+  # Open HDF5 file and get coordinates, cells, and stress.
+  h5 = h5py.File(filenameH5, "r")
+  vertices = h5['geometry/vertices'][:]
+  cells = numpy.array(h5['topology/cells'][:], dtype=numpy.int)
+  stress = h5['cell_fields/stress'][0,:,:]
+  h5.close()
+
+  # Get cell centers for output.
+  cellCoords = vertices[cells,:]
+  cellCenters = numpy.mean(cellCoords, axis=1)
+
+  # Create writer for spatial database file
+  writer = SimpleIOAscii()
+  writer.inventory.filename = filenameDB
+  writer._configure()
+  writer.write({'points': cellCenters,
+              'coordsys': cs,
+              'data_dim': 2,
+              'values': [{'name': "stress-xx",
+                          'units': "Pa",
+                          'data': stress[:,0]},
+                         {'name': "stress-yy",
+                          'units': "Pa",
+                          'data': stress[:,1]},
+                         {'name': "stress-xy",
+                          'units': "Pa",
+                          'data': stress[:,2]},
+                       ]})
+
