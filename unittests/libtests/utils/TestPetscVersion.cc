1119d5a780f87507c555db7d6b2faf9f1818af22
Brad Aagaard <baagaard@usgs.gov>
2016-07-01 14:15:31 -0700

    Add C++ unit tests for version info.

---
+++ unittests/libtests/utils/TestPetscVersion.cc
@@ -0,0 +1,114 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2016 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestPetscVersion.hh" // Implementation of class methods
+
+#include "pylith/utils/PetscVersion.hh" // USES PetscVersion
+
+#include "petsc.h" // USES PETSC_VERSION_*
+
+#include <string> // USES std::string()
+#include <string.h> // USES strlen()
+#include <stdio.h> // USES snprintf()
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::utils::TestPetscVersion );
+
+// ----------------------------------------------------------------------
+// Test isRelease()
+void
+pylith::utils::TestPetscVersion::testIsRelease(void)
+{ // testIsRelease
+#if PETSC_VERSION_RELEASE
+  CPPUNIT_ASSERT(PetscVersion::isRelease());
+#else
+  CPPUNIT_ASSERT(!PetscVersion::isRelease());
+#endif
+} // testIsRelease
+
+// ----------------------------------------------------------------------
+// Test version()
+void
+pylith::utils::TestPetscVersion::testVersion(void)
+{ // testVersion
+  const int maxsize = 64;
+  char value[maxsize];
+  snprintf(value, maxsize-1, "%d.%d.%d", PETSC_VERSION_MAJOR, PETSC_VERSION_MINOR, PETSC_VERSION_SUBMINOR);
+  CPPUNIT_ASSERT_EQUAL(std::string(value), std::string(PetscVersion::version()));
+} // testVersion
+
+// ----------------------------------------------------------------------
+// Test gitRevision()
+void
+pylith::utils::TestPetscVersion::testGitRevision(void)
+{ // testGitRevision
+#if PETSC_VERSION_RELEASE
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PetscVersion::gitRevision()));
+#else
+  // Git revision should be of the form vX.X.X-gXXXXX.
+  const char* rev = PetscVersion::gitRevision();
+  CPPUNIT_ASSERT_EQUAL('v', rev[0]);
+#endif
+} // testGitRevision
+
+// ----------------------------------------------------------------------
+// Test gitDate()
+void
+pylith::utils::TestPetscVersion::testGitDate(void)
+{ // testGitDate
+#if PETSC_VERSION_RELEASE
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PetscVersion::gitDate()));
+#else
+  const char* datetime = PetscVersion::gitDate();
+  CPPUNIT_ASSERT(strlen(datetime) > 0);
+#endif
+} // testGitDate
+
+// ----------------------------------------------------------------------
+// Test gitBranch()
+void
+pylith::utils::TestPetscVersion::testGitBranch(void)
+{ // testGitBranch
+#if PETSC_VERSION_RELEASE
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PetscVersion::gitBranch()));
+#else
+  const char* branch = PetscVersion::gitBranch();
+  CPPUNIT_ASSERT(strlen(branch) > 0);
+#endif
+} // testGitBranch
+
+
+// ----------------------------------------------------------------------
+// Test petscDir()
+void
+pylith::utils::TestPetscVersion::testPetscDir(void)
+{ // testPetscDir
+  CPPUNIT_ASSERT(strlen(PetscVersion::petscDir()) > 0);
+} // testPetscDir
+
+
+// ----------------------------------------------------------------------
+// Test petscArch()
+void
+pylith::utils::TestPetscVersion::testPetscArch(void)
+{ // testPetscArch
+  CPPUNIT_ASSERT(strlen(PetscVersion::petscArch()) > 0);
+} // testPetscArch
+
+
+// End of file 
---
+++ unittests/libtests/utils/TestPylithVersion.cc
@@ -0,0 +1,108 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2016 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestPylithVersion.hh" // Implementation of class methods
+
+#include "pylith/utils/PylithVersion.hh" // USES PylithVersion
+
+#include <string> // USES std::string()
+#include <string.h> // USES strlen()
+
+// ----------------------------------------------------------------------
+CPPUNIT_TEST_SUITE_REGISTRATION( pylith::utils::TestPylithVersion );
+
+// ----------------------------------------------------------------------
+// Test isRelease()
+void
+pylith::utils::TestPylithVersion::testIsRelease(void)
+{ // testIsRelease
+#if PYLITH_RELEASE_VERSION
+  CPPUNIT_ASSERT(PylithVersion::isRelease());
+#else
+  CPPUNIT_ASSERT(!PylithVersion::isRelease());
+#endif
+} // testIsRelease
+
+// ----------------------------------------------------------------------
+// Test version()
+void
+pylith::utils::TestPylithVersion::testVersion(void)
+{ // testVersion
+  CPPUNIT_ASSERT_EQUAL(std::string(PYLITH_VERSION), std::string(PylithVersion::version()));
+} // testVersion
+
+// ----------------------------------------------------------------------
+// Test gitRevision()
+void
+pylith::utils::TestPylithVersion::testGitRevision(void)
+{ // testGitRevision
+#if PYLITH_RELEASE_VERSION
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PylithVersion::gitRevision()));
+#else
+  // Git revision should be of the form vX.X.X-gXXXXX.
+  const char* rev = PylithVersion::gitRevision();
+  CPPUNIT_ASSERT_EQUAL('v', rev[0]);
+#endif
+} // testGitRevision
+
+// ----------------------------------------------------------------------
+// Test gitHash()
+void
+pylith::utils::TestPylithVersion::testGitHash(void)
+{ // testGitHash
+#if PYLITH_RELEASE_VERSION
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PylithVersion::gitHash()));
+#else
+  // Git hash should contain lower case and numbers.
+  const char* hash = PylithVersion::gitHash();
+  const int len = strlen(hash);
+  for (int i=0; i < len; ++i) {
+    const int value = int(hash[i]);
+    CPPUNIT_ASSERT((value >= int('0') && value <= int('9')) || (value >= int('a') && value <= int('z')));
+  } // for
+#endif
+} // testGitHash
+
+// ----------------------------------------------------------------------
+// Test gitDate()
+void
+pylith::utils::TestPylithVersion::testGitDate(void)
+{ // testGitDate
+#if PYLITH_RELEASE_VERSION
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PylithVersion::gitDate()));
+#else
+  const char* datetime = PylithVersion::gitDate();
+  CPPUNIT_ASSERT(strlen(datetime) > 0);
+#endif
+} // testGitDate
+
+// ----------------------------------------------------------------------
+// Test gitBranch()
+void
+pylith::utils::TestPylithVersion::testGitBranch(void)
+{ // testGitBranch
+#if PYLITH_RELEASE_VERSION
+  CPPUNIT_ASSERT_EQUAL(std::string("unknown"), std::string(PylithVersion::gitBranch()));
+#else
+  const char* branch = PylithVersion::gitBranch();
+  CPPUNIT_ASSERT(strlen(branch) > 0);
+#endif
+} // testGitBranch
+
+
+// End of file 
