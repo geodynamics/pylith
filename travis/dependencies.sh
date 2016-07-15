aa1079f7018d9dc2bb7a04b63bd08d3a64f8b722
Brad Aagaard <baagaard@usgs.gov>
2016-07-15 13:33:46 -0700

    Enable force install because PETSc installed as part of travis build not via installer.

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -11,7 +11,7 @@ git clone --depth 1 --recursive https://github.com/geodynamics/pylith_installer.
 
 pushd pylith_installer && \
   autoreconf --install --verbose && \
-  ./configure --prefix=${DEPS_DIR} --disable-cmake --disable-cppunit --disable-proj4 --enable-numpy --enable-hdf5 --enable-netcdf --enable-netcdfpy --enable-nemesis --enable-fiat --with-fortran=no --with-make-threads=${MAKE_THREADS} CFLAGS="-O2 -w" CXXFLAGS="-O2 -w" && \
+  ./configure --prefix=${DEPS_DIR} --disable-cmake --disable-cppunit --disable-proj4 --enable-numpy --enable-hdf5 --enable-netcdf --enable-netcdfpy --enable-nemesis --enable-fiat --with-fortran=no --enable-force-install --with-make-threads=${MAKE_THREADS} CFLAGS="-O2 -w" CXXFLAGS="-O2 -w" && \
   . ./setup.sh && \
   make deps && \
   popd
