geodynamics/pylith

commit 6d00c0c1493949b87f46249e5bfb75011bd6ab22

Fix memory leak in VTK data writer (change to PETSc behavior).
The VTK PetscViewer in PETSc v3.7.2 destroyed the DM when the viewer was
destroyed (and the file written). PETSc v3.7.4 does not destroy the DM.
As a result, we do not need to increment the reference count on the _dm
held in DataWriterVTK.

libsrc/pylith/meshio/DataWriterVTK.cc
@@ -218,12 +218,12 @@ pylith::meshio::DataWriterVTK::openTimeStep(const PylithScalar t,
 
   err = PetscViewerCreate(mesh.comm(), &_viewer);PYLITH_CHECK_ERROR(err);
   err = PetscViewerSetType(_viewer, PETSCVIEWERVTK);PYLITH_CHECK_ERROR(err);
-  err = PetscViewerSetFormat(_viewer, PETSC_VIEWER_ASCII_VTK);PYLITH_CHECK_ERROR(err);
+  err = PetscViewerPushFormat(_viewer, PETSC_VIEWER_ASCII_VTK);PYLITH_CHECK_ERROR(err);
   err = PetscViewerFileSetName(_viewer, filename.c_str());PYLITH_CHECK_ERROR(err);
   
   // Increment reference count on mesh DM, because the viewer destroys the DM.
   assert(_dm);
-  err = PetscObjectReference((PetscObject) _dm);PYLITH_CHECK_ERROR(err);
+  //err = PetscObjectReference((PetscObject) _dm);PYLITH_CHECK_ERROR(err);
   
   _isOpenTimeStep = true;
 
@@ -242,12 +242,6 @@ pylith::meshio::DataWriterVTK::closeTimeStep(void)
   // Destroy the viewer (which also writes the file).
   err = PetscViewerDestroy(&_viewer);PYLITH_CHECK_ERROR(err);
 
-  // Account for possibility that no fields were written, so viewer doesn't have handle to DM.
-  if (_isOpenTimeStep && !_wroteVertexHeader && !_wroteCellHeader) {
-    // No fields written, so must manually dereference the mesh DM.
-    err = PetscObjectDereference((PetscObject) _dm);PYLITH_CHECK_ERROR(err);
-  } // if
-  
   // Remove label
   if (_isOpenTimeStep) {
     assert(_dm);
@@ -322,7 +316,7 @@ pylith::meshio::DataWriterVTK::writeCellField(const PylithScalar t,
 
   assert(_dm && _dm == field.mesh().dmMesh());
   assert(_isOpen && _isOpenTimeStep);
-
+#if 0
   // Cache cell field since PETSc writer collects all fields and
   // then writes file. Caching the field locally allows the output
   // manager to reuse fields as buffers.
@@ -349,6 +343,7 @@ pylith::meshio::DataWriterVTK::writeCellField(const PylithScalar t,
   PetscViewerVTKFieldType ft = fieldCached.vectorFieldType() != topology::FieldBase::VECTOR ? PETSC_VTK_CELL_FIELD : PETSC_VTK_CELL_VECTOR_FIELD;
   PetscErrorCode err = PetscViewerVTKAddField(_viewer, (PetscObject) _dm, DMPlexVTKWriteAll, ft, (PetscObject) fieldVec); PYLITH_CHECK_ERROR(err);
   err = PetscObjectReference((PetscObject) fieldVec);PYLITH_CHECK_ERROR(err); // Viewer destroys Vec
+#endif
   
   _wroteCellHeader = true;
 

commit 21f52754c9719db3655807899f9e5d9a0b6ff133

Fix error associated with commenting out code for debugging.
Error was inadvertently introduced in
https://github.com/geodynamics/pylith/commit/6d00c0c1493949b87f46249e5bfb75011bd6ab22.

libsrc/pylith/meshio/DataWriterVTK.cc
@@ -316,7 +316,6 @@ pylith::meshio::DataWriterVTK::writeCellField(const PylithScalar t,
 
   assert(_dm && _dm == field.mesh().dmMesh());
   assert(_isOpen && _isOpenTimeStep);
-#if 0
   // Cache cell field since PETSc writer collects all fields and
   // then writes file. Caching the field locally allows the output
   // manager to reuse fields as buffers.
@@ -343,7 +342,6 @@ pylith::meshio::DataWriterVTK::writeCellField(const PylithScalar t,
   PetscViewerVTKFieldType ft = fieldCached.vectorFieldType() != topology::FieldBase::VECTOR ? PETSC_VTK_CELL_FIELD : PETSC_VTK_CELL_VECTOR_FIELD;
   PetscErrorCode err = PetscViewerVTKAddField(_viewer, (PetscObject) _dm, DMPlexVTKWriteAll, ft, (PetscObject) fieldVec); PYLITH_CHECK_ERROR(err);
   err = PetscObjectReference((PetscObject) fieldVec);PYLITH_CHECK_ERROR(err); // Viewer destroys Vec
-#endif
   
   _wroteCellHeader = true;
 