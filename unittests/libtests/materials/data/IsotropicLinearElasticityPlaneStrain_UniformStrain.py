f5b94c7c5c3404a7eff443a3246afd3e169c3333
Brad Aagaard <baagaard@usgs.gov>
2017-04-05 10:34:31 -0700

    Improve test setup for uniform strain MMS.

--- unittests/libtests/materials/data/IsotropicLinearElasticityPlaneStrain_UniformStrain.py
+++ unittests/libtests/materials/data/IsotropicLinearElasticityPlaneStrain_UniformStrain.py
@@ -9,14 +9,14 @@
 # This code was developed as part of the Computational Infrastructure
 # for Geodynamics (http://geodynamics.org).
 #
-# Copyright (c) 2010-2015 University of California, Davis
+# Copyright (c) 2010-2017 University of California, Davis
 #
 # See COPYING for license information.
 #
 # ----------------------------------------------------------------------
 #
 
-## @file unittests/libtests/materials/data/IsotropicLinearElasticityPlaneStrain_UniStrain.py
+## @file unittests/libtests/materials/data/IsotropicLinearElasticityPlaneStrain_UniformStrain.py
 
 ## @brief Python application for generating spatial database files for
 ## testing IsotropicLinearElasticityPlaneStrain via Method of
@@ -46,7 +46,7 @@ cs.setSpaceDim(2)
 # ----------------------------------------------------------------------
 def generateAuxFields():
     writer = SimpleIOAscii()
-    writer.inventory.filename = "IsotropicLinearElasticityPlaneStrain_UniStrain_aux.spatialdb"
+    writer.inventory.filename = "IsotropicLinearElasticityPlaneStrain_UniformStrain_aux.spatialdb"
     writer._configure()
     writer.write({'points': points,
                   'coordsys': cs,
@@ -60,46 +60,27 @@ def generateAuxFields():
 
 # ----------------------------------------------------------------------
 def generateSolution():
-    exx0 = 0.1
-    eyy0 = 0.25
-    exy0 = 0.3
+    exx = 0.1
+    eyy = 0.25
+    exy = 0.3
     
-    exxR = 0.4
-    eyyR = 0.7
-    exyR = -0.2
-    
-    t = 0.1
-    exx = exx0 + exxR*t
-    eyy = eyy0 + eyyR*t
-    exy = exy0 + exyR*t
     disp = numpy.zeros((npts, 2))
     disp[:,0] = exx*points[:,0] + exy*points[:,1]
     disp[:,1] = exy*points[:,0] + eyy*points[:,1]
 
-    vel = numpy.zeros(disp.shape)
-    vel[:,0] = exxR*points[:,0] + exyR*points[:,1]
-    vel[:,1] = exyR*points[:,0] + eyyR*points[:,1]
-    vel *= 0.0 # :TEMPORARY: :KLUDGE:
-
-    disp_dot = vel
-
-    vel_dot = numpy.zeros(vel.shape)
+    disp_dot = 0*disp
 
     # Create writer for spatial database file
     writer = SimpleIOAscii()
-    writer.inventory.filename = "IsotropicLinearElasticityPlaneStrain_UniStrain_soln.spatialdb"
+    writer.inventory.filename = "IsotropicLinearElasticityPlaneStrain_UniformStrain_soln.spatialdb"
     writer._configure()
     writer.write({'points': points,
                   'coordsys': cs,
                   'data_dim': 2,
                   'values': [{'name': "displacement_x", 'units': "m", 'data': disp[:,0]},
                              {'name': "displacement_y", 'units': "m", 'data': disp[:,1]},
-                             {'name': "velocity_x", 'units': "m/s", 'data': vel[:,0]},
-                             {'name': "velocity_y", 'units': "m/s", 'data': vel[:,1]},
                              {'name': "displacement_dot_x", 'units': "m/s", 'data': disp_dot[:,0]},
                              {'name': "displacement_dot_y", 'units': "m/s", 'data': disp_dot[:,1]},
-                             {'name': "velocity_dot_x", 'units': "m/s**2", 'data': vel_dot[:,0]},
-                             {'name': "velocity_dot_y", 'units': "m/s**2", 'data': vel_dot[:,1]},
                            ]})
     
     return
@@ -107,8 +88,6 @@ def generateSolution():
 
 # ----------------------------------------------------------------------
 def generatePerturbation():
-    dt = 0.05
-
     x = numpy.arange(-4.0e+3, 4.01e+3, 0.5e+3, dtype=numpy.float64)
     y = numpy.arange(-4.0e+3, 4.01e+3, 0.5e+3, dtype=numpy.float64)
     numX = x.shape[0]
@@ -119,30 +98,20 @@ def generatePerturbation():
         points[iY*numX:(iY+1)*numX,0] = x
         points[iY*numX:(iY+1)*numX,1] = y[iY]
 
-    disp = 0.1*(numpy.random.rand(npts,2)-0.5)
-
-    vel = 1.0/dt * disp
-    vel *= 0.0 # :TEMPORARY: :KLUDGE:
-  
-    disp_dot = vel
-
-    vel_dot = numpy.zeros(vel.shape)
+    disp = 1.0e-2*(numpy.random.rand(npts,2)-0.5)
+    disp_dot = 0*disp
 
     # Create writer for spatial database file
     writer = SimpleIOAscii()
-    writer.inventory.filename = "IsotropicLinearElasticityPlaneStrain_UniStrain_pert.spatialdb"
+    writer.inventory.filename = "IsotropicLinearElasticityPlaneStrain_UniformStrain_pert.spatialdb"
     writer._configure()
     writer.write({'points': points,
                   'coordsys': cs,
                   'data_dim': 2,
                   'values': [{'name': "displacement_x", 'units': "m", 'data': disp[:,0]},
                              {'name': "displacement_y", 'units': "m", 'data': disp[:,1]},
-                             {'name': "velocity_x", 'units': "m/s", 'data': vel[:,0]},
-                             {'name': "velocity_y", 'units': "m/s", 'data': vel[:,1]},
                              {'name': "displacement_dot_x", 'units': "m/s", 'data': disp_dot[:,0]},
                              {'name': "displacement_dot_y", 'units': "m/s", 'data': disp_dot[:,1]},
-                             {'name': "velocity_dot_x", 'units': "m/s**2", 'data': vel_dot[:,0]},
-                             {'name': "velocity_dot_y", 'units': "m/s**2", 'data': vel_dot[:,1]},
                            ]})
     
     return
