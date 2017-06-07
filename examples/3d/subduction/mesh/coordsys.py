7554620bace25c4722c2686518875005a446a4e0
Brad Aagaard <baagaard@usgs.gov>
2017-06-06 20:45:43 -0700

    Added missing file.

---
+++ examples/3d/subduction/mesh/coordsys.py
@@ -0,0 +1,71 @@
+# Coordinate systems
+
+from spatialdata.geocoords.CSGeo import CSGeo
+from spatialdata.geocoords.CSGeoProj import CSGeoProj
+
+# Geographic lat/lon coordinates in WGS84 datum
+def cs_geo():
+    """Geographic lat/lon coordinates in WGS84 datum.
+    """
+    cs = CSGeo()
+    cs.inventory.datumHoriz = "WGS84"
+    cs.inventory.datumVert = "mean sea level"
+    cs.inventory.spaceDim = 2
+    cs._configure()
+    cs.initialize()
+    return cs
+
+
+def cs_geo3D():
+    """Geographic lat/lon/elev coordinates in WGS84 datum with mean sea
+    level as vertical datum.
+
+    """
+    cs = CSGeo()
+    cs.inventory.datumHoriz = "WGS84"
+    cs.inventory.datumVert = "mean sea level"
+    cs.inventory.spaceDim = 3
+    cs._configure()
+    cs.initialize()
+    return cs
+
+
+def cs_mesh():
+    """Cartesian coordinates for mesh with Portland as the origin and mean
+    sea level as vertical datum.
+
+    """
+    cs = CSGeoProj()
+    cs.inventory.datumHoriz = "WGS84"
+    cs.inventory.datumVert = "mean sea level"
+    cs.inventory.spaceDim = 3
+    cs.inventory.projector.inventory.projection = "tmerc"
+    cs.inventory.projector.inventory.projOptions = "+lat_0=45.5231 +lon_0=-122.6765 +k=0.9996"
+    cs.inventory.projector._configure()
+    cs._configure()
+    cs.initialize()
+    return cs
+
+
+def geoToMesh(xyz):
+    """Convert coordinates from geographic coordinates to mesh coordinates.
+    """
+    from spatialdata.geocoords.Converter import convert
+    convert(xyz, cs_mesh(), cs_geo3D())
+    return
+    
+
+def bbox_mesh():
+    """
+    """
+    xdim = 2000.0e+3
+    ydim = 2000.0e+3
+    xoffset = 0.0e+3
+    yoffset = 0.0e+3
+
+    bbox = (xoffset-0.5*xdim, yoffset-0.5*ydim,
+            xoffset+0.5*xdim, yoffset+0.5*ydim)
+    return bbox
+
+
+# End of file
