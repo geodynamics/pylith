b75367d44c9a1eefea05b92194e9bc0217f6e76c
Brad Aagaard <baagaard@usgs.gov>
2017-06-13 12:33:31 -0700

    Initial setup for examples/3d/subduction/step01.

---
+++ examples/3d/subduction/plot_dispvec.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env pvpython
+# -*- Python -*- (syntax highlighting)
+# ----------------------------------------------------------------------
+#
+# Brad T. Aagaard, U.S. Geological Survey
+# Charles A. Williams, GNS Science
+# Matthew G. Knepley, University of Chicago
+#
+# This code was developed as part of the Computational Infrastructure
+# for Geodynamics (http://geodynamics.org).
+#
+# Copyright (c) 2010-2017 University of California, Davis
+#
+# See COPYING for license information.
+#
+# ----------------------------------------------------------------------
+
+# Plot the domain, colored by the magnitude of the displacement
+# vector, with white arrows showing the displacement vectors.
+#
+# This Python script runs using pvpython or within the ParaView Python
+# shell.
+
+# User-specified parameters
+
+# Root name for simulation.
+SIM_NAME = "step01"
+
+# Format of simulation output (choices=["vtk", "hdf5"], case insentitive)
+DATA_FORMAT = "vtk"
+
+# Scale used to exaggerate deformation.
+DISPLACEMENT_SCALE = 10.0e+3
+
+
+# ----------------------------------------------------------------------
+# We create sources, filters, etc using the servermanager so that we can set the
+# name of the proxy shown in the pipeline using Register(registrationName=NAME).
+
+from paraview.simple import *
+# Disable automatic camera reset on "Show"
+paraview.simple._DisableFirstRenderCameraReset()
+
+# Read data
+if DATA_FORMAT.lower() == "vtk":
+    dataDomain = servermanager.sources.LegacyVTKReader(FileNames=['output/%s-domain_t0000000.vtk' % SIM_NAME])
+elif DATA_FORMAT.lower() == "hdf5":
+    pass
+else:
+     raise ValueError("Unknown file format '%s' when choosing reader in Python script." % DATA_FORMAT)
+servermanager.Register(dataDomain, registrationName="%s-domain" % SIM_NAME)
+view = GetActiveViewOrCreate('RenderView')
+
+# Show undeformed domain, colored by magnitude of displacement vector.
+domainDisplay = Show(dataDomain, view)
+ColorBy(domainDisplay, ("POINTS", "displacement", "Magnitude"))
+domainDisplay.RescaleTransferFunctionToDataRange(True)
+domainDisplay.SetScalarBarVisibility(view, True)
+domainDisplay.SetRepresentationType("Surface With Edges")
+
+# Rescale color and/or opacity maps used to exactly fit the current data range
+displacementLUT = GetColorTransferFunction('displacement')
+domainDisplay.RescaleTransferFunctionToDataRange(False, False)
+# Update scalar bar component title.
+UpdateScalarBarsComponentTitle(displacementLUT, domainDisplay)
+
+
+# Add arrows to show displacement vectors.
+glyph = servermanager.filters.Glyph(Input=dataDomain, GlyphType="Arrow")
+servermanager.Register(glyph, registrationName="%s-domain-glyph" % SIM_NAME)
+glyph.Vectors = ["POINTS", "displacement"]
+glyph.ScaleFactor = DISPLACEMENT_SCALE
+glyph.ScaleMode = "vector"
+glyph.GlyphMode = "All Points"
+
+glyphDisplay = Show(glyph, view)
+glyphDisplay.Representation = "Surface"
+
+view.ResetCamera()
+
+Render()
+
+# Uncomment if running from shell outside ParaView.
+#Interact()
+
+
+# End of file
---
+++ examples/3d/subduction/plot_dispwarp.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env pvpython
+# -*- Python -*- (syntax highlighting)
+# ----------------------------------------------------------------------
+#
+# Brad T. Aagaard, U.S. Geological Survey
+# Charles A. Williams, GNS Science
+# Matthew G. Knepley, University of Chicago
+#
+# This code was developed as part of the Computational Infrastructure
+# for Geodynamics (http://geodynamics.org).
+#
+# Copyright (c) 2010-2017 University of California, Davis
+#
+# See COPYING for license information.
+#
+# ----------------------------------------------------------------------
+
+# Plot the undeformed domain as a gray wireframe and then the deformed
+# domain, colored by the value of the x-displacemenet.
+#
+# This Python script runs using pvpython or within the ParaView Python
+# shell.
+
+# User-specified parameters
+
+# Root name for simulation.
+SIM_NAME = "step01"
+
+# Format of simulation output (choices=["vtk", "hdf5"], case insentitive)
+DATA_FORMAT = "vtk"
+
+# Scale used to exaggerate deformation.
+DISPLACEMENT_SCALE = 10.0e+3
+
+# ----------------------------------------------------------------------
+# We create sources, filters, etc using the servermanager so that we can set the
+# name of the proxy shown in the pipeline using Register(registrationName=NAME).
+
+from paraview.simple import *
+# Disable automatic camera reset on "Show"
+paraview.simple._DisableFirstRenderCameraReset()
+
+# Read data
+if DATA_FORMAT == "vtk":
+    dataDomain = servermanager.sources.LegacyVTKReader(FileNames=['output/%s-domain_t0000000.vtk' % SIM_NAME])
+elif DATA_FORMAT == "HDF5":
+    pass
+else:
+     raise ValueError("Unknown file format '%s' when choosing reader in Python script." % DATA_FORMAT)
+servermanager.Register(dataDomain, registrationName="%s-domain" % SIM_NAME)
+view = GetActiveViewOrCreate('RenderView')
+
+# Gray wireframe for undeformed domain.
+domainDisplay = Show(dataDomain, view)
+domainDisplay.Representation = 'Wireframe'
+domainDisplay.AmbientColor = [0.5, 0.5, 0.5]
+
+# Warp domain to show deformation
+warp = servermanager.filters.WarpByVector(Input=dataDomain)
+servermanager.Register(warp, registrationName="%s-domain-warp" % SIM_NAME)
+warp.Vectors = ['POINTS', 'displacement']
+warp.ScaleFactor = DISPLACEMENT_SCALE
+
+warpDisplay = Show(warp, view)
+ColorBy(warpDisplay, ('POINTS', 'displacement', 'X'))
+warpDisplay.RescaleTransferFunctionToDataRange(True)
+warpDisplay.SetScalarBarVisibility(view, True)
+warpDisplay.SetRepresentationType('Surface With Edges')
+
+# Rescale color and/or opacity maps used to exactly fit the current data range
+displacementLUT = GetColorTransferFunction('displacement')
+warpDisplay.RescaleTransferFunctionToDataRange(False, False)
+# Update a scalar bar component title.
+UpdateScalarBarsComponentTitle(displacementLUT, warpDisplay)
+
+view.ResetCamera()
+
+Render()
+
+# Uncomment if running from shell outside ParaView.
+#Interact()
+
+
+# End of file
