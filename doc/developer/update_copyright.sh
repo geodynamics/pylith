7002bc09b146b4a2ca5a309d2a7ad6eb54852f36
Brad Aagaard <baagaard@usgs.gov>
2017-06-23 16:35:10 -0700

    Update copyright script.

--- doc/developer/update_copyright.sh
+++ doc/developer/update_copyright.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for suffix in .cc .icc .hh .c .h .am .py .i .jou .odb .ac .in .dat .sh; do
+for suffix in .cc .icc .hh .c .h .am .py .i .jou .odb .ac .in .dat; do
   for f in `find . -name "*$suffix"` aclocal.m4 COPYING ; do
+    sed -e "s/Copyright (c) 2010-2015 University of California, Davis/Copyright (c) 2010-2017 University of California, Davis/g" $f > tmp && mv -f tmp $f
+    sed -e "s/Copyright (c) 2010-2016 University of California, Davis/Copyright (c) 2010-2017 University of California, Davis/g" $f > tmp && mv -f tmp $f
     sed -e "s/Copyright (c) 2010-2017 University of California, Davis/Copyright (c) 2010-2017 University of California, Davis/g" $f > tmp && mv -f tmp $f
-    sed -e "s/Copyright (c) 2010-2017 University of California, Davis/Copyright (c) 2010-2017 University of California, Davis/g" $f > tmp && mv -f tmp $f
-    sed -e "s/Copyright (c) 2010-2017 University of California, Davis/Copyright (c) 2010-2017 University of California, Davis/g" $f > tmp && mv -f tmp $f
+    sed -e "s/Matthew G. Knepley, University of Chicago/Matthew G. Knepley, Rice University/g" $f > tmp && mv -f tmp $f
   done
 done
