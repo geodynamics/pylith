38ff4e329b44abe5677ec34c3378545158c7b89e
Brad Aagaard <baagaard@usgs.gov>
2017-11-15 10:12:13 -0800

    Prevent possible memory leak.

--- libsrc/pylith/bc/DirichletNew.cc
+++ libsrc/pylith/bc/DirichletNew.cc
@@ -106,7 +106,7 @@ pylith::bc::DirichletNew::initialize(const pylith::topology::Field& solution)
 
     _setFEKernelsConstraint(solution);
 
-    _boundaryMesh = new pylith::topology::Mesh(solution.mesh(), _label.c_str()); assert(_boundaryMesh);
+    delete _boundaryMesh; _boundaryMesh = new pylith::topology::Mesh(solution.mesh(), _label.c_str()); assert(_boundaryMesh);
     PetscDM dmBoundary = _boundaryMesh->dmMesh(); assert(dmBoundary);
     pylith::topology::CoordsVisitor::optimizeClosure(dmBoundary);
 
