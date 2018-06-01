c93336e43b26477dfb704688167ea85a4c279d0b
Brad Aagaard <baagaard@usgs.gov>
2018-06-01 15:49:16 -0700

    Add 3-D test cases for C++ FieldQuery unit tests.

--- unittests/libtests/topology/TestFieldQuery_Cases.cc
+++ unittests/libtests/topology/TestFieldQuery_Cases.cc
@@ -30,35 +30,19 @@ namespace pylith {
     namespace topology {
         class TestFieldQuery_DispTemp;
 
+        class TestFieldQuery_DispTemp2D;
+        class TestFieldQuery_Tri;
         class TestFieldQuery_Quad;
+
+        class TestFieldQuery_DispTemp3D;
+        class TestFieldQuery_Tet;
+        class TestFieldQuery_Hex;
     } // topologyfwd
 } // pylith
 
 
 // ---------------------------------------------------------------------
 class pylith::topology::TestFieldQuery_DispTemp : public pylith::topology::TestFieldQuery {
-
-    // Spatial database user functions for auxiliary subfields.
-    static const char* disp_units(void) {
-        return "m";
-    }
-    static const char* temp_units(void) {
-        return "K";
-    }
-
-    static double disp_x(const double x,
-                         const double y) {
-        return 1.0 + 2.4*x + 3.2*y;
-    } // disp_x
-    static double disp_y(const double x,
-                         const double y) {
-        return 0.4 - 0.1*x + 0.6*y;
-    } // disp_y
-    static double temp(const double x,
-                       const double y) {
-        return 20.0 + 0.1*x*x + 0.3*x*y -0.2*y*y;
-    } // temp
-
 
 protected:
 
@@ -103,25 +87,102 @@ protected:
         };
         _data->auxDiscretizations = const_cast<pylith::topology::Field::Discretization*>(auxDiscretizations);
 
-        CPPUNIT_ASSERT(_data->auxDB);
-        _data->auxDB->addValue("displacement_x", disp_x, disp_units());
-        _data->auxDB->addValue("displacement_y", disp_y, disp_units());
-        _data->auxDB->addValue("temperature", temp, temp_units());
     }   // setUp
 
 }; // TestFieldQuery_DispTemp
 
 // ---------------------------------------------------------------------
-class pylith::topology::TestFieldQuery_Quad : public pylith::topology::TestFieldQuery_DispTemp {
+class pylith::topology::TestFieldQuery_DispTemp2D : public pylith::topology::TestFieldQuery_DispTemp {
+
+    // Spatial database user functions for auxiliary subfields.
+    static const char* disp_units(void) {
+        return "m";
+    }
+    static const char* temp_units(void) {
+        return "K";
+    }
+
+    static double disp_x(const double x,
+                         const double y) {
+        return 1.0 + 2.4*x + 3.2*y;
+    } // disp_x
+    static double disp_y(const double x,
+                         const double y) {
+        return 0.4 - 0.1*x + 0.6*y;
+    } // disp_y
+    static double temp(const double x,
+                       const double y) {
+        return 20.0 + 0.1*x*x + 0.3*x*y -0.2*y*y;
+    } // temp
+
+
+protected:
+
+    void setUp(void) {
+        TestFieldQuery_DispTemp::setUp();
+
+        CPPUNIT_ASSERT(_data->cs);
+        _data->cs->setSpaceDim(2);
+        _data->cs->initialize();
+
+        CPPUNIT_ASSERT(_data->auxDB);
+        _data->auxDB->label("Auxiliary db 2D");
+        _data->auxDB->coordsys(*_data->cs);
+        _data->auxDB->addValue("displacement_x", disp_x, disp_units());
+        _data->auxDB->addValue("displacement_y", disp_y, disp_units());
+        _data->auxDB->addValue("temperature", temp, temp_units());
+
+    }   // setUp
+
+}; // TestFieldQuery_DispTemp2D
+
+// ---------------------------------------------------------------------
+class pylith::topology::TestFieldQuery_Tri : public pylith::topology::TestFieldQuery_DispTemp2D {
 
     // Spatial database user functions for auxiliary subfields.
 
 protected:
-    CPPUNIT_TEST_SUB_SUITE(TestFieldQuery_Quad, TestFieldQuery);
+    CPPUNIT_TEST_SUB_SUITE(TestFieldQuery_Tri, TestFieldQuery_DispTemp2D);
     CPPUNIT_TEST_SUITE_END();
 
     void setUp(void) {
-        TestFieldQuery_DispTemp::setUp();
+        TestFieldQuery_DispTemp2D::setUp();
+
+        // Mesh information.
+        _data->cellDim = 2;
+        _data->numVertices = 4;
+        _data->numCells = 2;
+        _data->numCorners = 3;
+        static const int _cells[2*3] = {
+            0, 1, 2,
+            3, 2, 1,
+        };
+        _data->cells = const_cast<int*>(_cells);
+        static const PylithScalar _coordinates[4*2] = {
+            -1.0,  0.0,
+            +1.0, -2.0,
+            +0.8, +1.2,
+            +3.0,  0.0,
+        };
+        _data->coordinates = const_cast<PylithScalar*>(_coordinates);
+
+    }   // setUp
+
+};  // TestFieldQuery_Tri
+CPPUNIT_TEST_SUITE_REGISTRATION(pylith::topology::TestFieldQuery_Tri);
+
+
+// ---------------------------------------------------------------------
+class pylith::topology::TestFieldQuery_Quad : public pylith::topology::TestFieldQuery_DispTemp2D {
+
+    // Spatial database user functions for auxiliary subfields.
+
+protected:
+    CPPUNIT_TEST_SUB_SUITE(TestFieldQuery_Quad, TestFieldQuery_DispTemp2D);
+    CPPUNIT_TEST_SUITE_END();
+
+    void setUp(void) {
+        TestFieldQuery_DispTemp2D::setUp();
 
         // Mesh information.
         _data->cellDim = 2;
@@ -143,16 +204,139 @@ protected:
         };
         _data->coordinates = const_cast<PylithScalar*>(_coordinates);
 
-        CPPUNIT_ASSERT(_data->cs);
-        _data->cs->setSpaceDim(2);
-        _data->cs->initialize();
-
-        _data->auxDB->coordsys(*_data->cs);
     }   // setUp
-
 
 };  // TestFieldQuery_Quad
 CPPUNIT_TEST_SUITE_REGISTRATION(pylith::topology::TestFieldQuery_Quad);
+
+
+// ---------------------------------------------------------------------
+class pylith::topology::TestFieldQuery_DispTemp3D : public pylith::topology::TestFieldQuery_DispTemp {
+
+    // Spatial database user functions for auxiliary subfields.
+    static const char* disp_units(void) {
+        return "m";
+    }
+    static const char* temp_units(void) {
+        return "K";
+    }
+
+    static double disp_x(const double x,
+                         const double y,
+                         const double z) {
+        return 1.0 + 2.4*x + 3.2*y - 0.7*z;
+    } // disp_x
+    static double disp_y(const double x,
+                         const double y,
+                         const double z) {
+        return 0.4 - 0.1*x + 0.6*y + 0.3*z;
+    } // disp_y
+    static double temp(const double x,
+                       const double y,
+                       const double z) {
+        return 20.0 + 0.1*x*x + 0.3*x*y -0.2*y*y - 3.0*y*z;
+    } // temp
+
+
+protected:
+
+    void setUp(void) {
+        TestFieldQuery_DispTemp::setUp();
+
+        CPPUNIT_ASSERT(_data->cs);
+        _data->cs->setSpaceDim(3);
+        _data->cs->initialize();
+
+        CPPUNIT_ASSERT(_data->auxDB);
+        _data->auxDB->label("Auxiliary db 3D");
+        _data->auxDB->coordsys(*_data->cs);
+        _data->auxDB->addValue("displacement_x", disp_x, disp_units());
+        _data->auxDB->addValue("displacement_y", disp_y, disp_units());
+        _data->auxDB->addValue("temperature", temp, temp_units());
+
+    }   // setUp
+
+}; // TestFieldQuery_DispTemp3D
+
+// ---------------------------------------------------------------------
+class pylith::topology::TestFieldQuery_Tet : public pylith::topology::TestFieldQuery_DispTemp3D {
+
+    // Spatial database user functions for auxiliary subfields.
+
+protected:
+    CPPUNIT_TEST_SUB_SUITE(TestFieldQuery_Tet, TestFieldQuery);
+    CPPUNIT_TEST_SUITE_END();
+
+    void setUp(void) {
+        TestFieldQuery_DispTemp3D::setUp();
+
+        // Mesh information.
+        _data->cellDim = 3;
+        _data->numVertices = 5;
+        _data->numCells = 2;
+        _data->numCorners = 4;
+        static const int _cells[2*4] = {
+            0, 1, 3, 4,
+            4, 1, 3, 2,
+        };
+        _data->cells = const_cast<int*>(_cells);
+        static const PylithScalar _coordinates[5*3] = {
+            -2.0,  0.0,  0.0,
+            +0.0, -1.0,  0.0,
+            +2.0,  0.0,  0.0,
+            +0.0, +2.0,  0.0,
+            +0.0, +1.0, +2.0,
+        };
+        _data->coordinates = const_cast<PylithScalar*>(_coordinates);
+
+    }   // setUp
+
+};  // TestFieldQuery_Tet
+CPPUNIT_TEST_SUITE_REGISTRATION(pylith::topology::TestFieldQuery_Tet);
+
+
+// ---------------------------------------------------------------------
+class pylith::topology::TestFieldQuery_Hex : public pylith::topology::TestFieldQuery_DispTemp3D {
+
+    // Spatial database user functions for auxiliary subfields.
+
+protected:
+    CPPUNIT_TEST_SUB_SUITE(TestFieldQuery_Hex, TestFieldQuery);
+    CPPUNIT_TEST_SUITE_END();
+
+    void setUp(void) {
+        TestFieldQuery_DispTemp3D::setUp();
+
+        // Mesh information.
+        _data->cellDim = 3;
+        _data->numVertices = 12;
+        _data->numCells = 2;
+        _data->numCorners = 8;
+        static const int _cells[2*8] = {
+            0,  3,  9,  6,  1,  4, 10,  7,
+            10, 11,  8,  7,  4,  5,  2,  1,
+        };
+        _data->cells = const_cast<int*>(_cells);
+        static const PylithScalar _coordinates[12*3] = {
+            +0.0, +0.0, +0.0,
+            +0.0, +0.0, +2.0,
+            +0.0, +0.0, +4.5,
+            +1.8, +0.1, +0.1,
+            +1.9, +0.0, +2.0,
+            +1.8, -0.1, +3.9,
+            +0.0, +3.0, +0.2,
+            +0.1, +2.8, +1.9,
+            +0.0, +3.2, +5.0,
+            +2.0, +3.0, +0.0,
+            +2.1, +2.9, +2.0,
+            +2.0, +3.0, +4.0,
+        };
+        _data->coordinates = const_cast<PylithScalar*>(_coordinates);
+
+    }   // setUp
+
+};  // TestFieldQuery_Hex
+CPPUNIT_TEST_SUITE_REGISTRATION(pylith::topology::TestFieldQuery_Hex);
 
 
 // End of file
