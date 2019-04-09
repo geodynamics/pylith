976c84a1d60bba19cb7c506609065a0547305b8a
Charles Williams <C.Williams at gns.cri.nz>
2019-04-10 10:53:35 +1200

    Added directory for checking SNES Jacobians.

---
+++ playpen/check-jacobians/run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Run all 3D models.
+pylith hex.cfg drucker_prager_3d_solve.cfg 2>&1 | tee drucker_prager_3d_solve.log
+pylith hex.cfg drucker_prager_3d_jacobian.cfg 2>&1 | tee drucker_prager_3d_jacobian.log
+pylith hex.cfg drucker_prager_3d_nonassoc_solve.cfg 2>&1 | tee drucker_prager_3d_nonassoc_solve.log
+pylith hex.cfg drucker_prager_3d_nonassoc_jacobian.cfg 2>&1 | tee drucker_prager_3d_nonassoc_jacobian.log
+pylith hex.cfg power_law_3d_solve.cfg 2>&1 | tee power_law_3d_solve.log
+pylith hex.cfg power_law_3d_jacobian.cfg 2>&1 | tee power_law_3d_jacobian.log
+
+# Extract Jacobian info and output to separate files.
+grep -F -A 66 'Explicit preconditioning Jacobian' drucker_prager_3d_jacobian.log > pylith_dp_3d_jacobian.txt
+grep -F -A 66 'Explicit preconditioning Jacobian' drucker_prager_3d_nonassoc_jacobian.log > pylith_dp_3d_nonassoc_jacobian.txt
+grep -F -A 66 'Explicit preconditioning Jacobian' power_law_3d_jacobian.log > pylith_pl_3d_jacobian.txt
+grep -F -A 66 'Finite difference Jacobian' drucker_prager_3d_jacobian.log > finite_diff_dp_3d_jacobian.txt
+grep -F -A 66 'Finite difference Jacobian' drucker_prager_3d_nonassoc_jacobian.log > finite_diff_dp_3d_nonassoc_jacobian.txt
+grep -F -A 66 'Finite difference Jacobian' power_law_3d_jacobian.log > finite_diff_pl_3d_jacobian.txt
+grep -F -A 66 'minus finite difference Jacobian' drucker_prager_3d_jacobian.log > diff_dp_3d_jacobian.txt
+grep -F -A 66 'minus finite difference Jacobian' drucker_prager_3d_nonassoc_jacobian.log > diff_dp_3d_nonassoc_jacobian.txt
+grep -F -A 66 'minus finite difference Jacobian' power_law_3d_jacobian.log > diff_pl_3d_jacobian.txt
