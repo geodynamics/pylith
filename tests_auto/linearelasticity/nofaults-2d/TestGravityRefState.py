52a0b7227debc72070ae8c432d77e2fba8ffafb8
Brad Aagaard <baagaard@usgs.gov>
2019-06-03 14:52:50 -0700

    Fix field type (cell fields) for Cauchy stress and strain for test with gravity and reference state.

--- tests_auto/linearelasticity/nofaults-2d/TestGravityRefState.py
+++ tests_auto/linearelasticity/nofaults-2d/TestGravityRefState.py
@@ -66,10 +66,11 @@ class TestCase(unittest.TestCase):
         return
 
     def test_material_solution(self):
-        vertexFields = ["displacement", "cauchy_strain", "cauchy_stress"]
+        vertexFields = ["displacement"]
+        cellFields = ["cauchy_strain", "cauchy_stress"]
         for material in self.MATERIALS.keys():
             filename = "output/{}-{}.h5".format(self.NAME, material)
-            check_data(filename, self, self.MATERIALS[material], vertexFields=vertexFields)
+            check_data(filename, self, self.MATERIALS[material], vertexFields=vertexFields, cellFields=cellFields)
         return
 
     def test_bcdirichlet_info(self):
