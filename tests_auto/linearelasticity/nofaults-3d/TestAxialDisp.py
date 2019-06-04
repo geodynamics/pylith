d2a3fd69f979a92a0f8f498b169cd6a0c33939d8
Brad Aagaard <baagaard@usgs.gov>
2019-06-03 23:02:08 -0700

    Started work on implementing 3D versions of 2D automated tests.

---
+++ tests_auto/linearelasticity/nofaults-3d/TestAxialDisp.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+#
+# ----------------------------------------------------------------------
+#
+# Brad T. Aagaard, U.S. Geological Survey
+# Charles A. Williams, GNS Science
+# Matthew G. Knepley, University of Chicago
+#
+# This code was developed as part of the Computational Infrastructure
+# for Geodynamics (http://geodynamics.org).
+#
+# Copyright (c) 2010-2017 University of California, Davis
+#
+# See COPYING for license information.
+#
+# ----------------------------------------------------------------------
+#
+# @file tests_auto/linearelasticity/nofaults/TestAxialDisp.py
+#
+# @brief Test suite for testing pylith with 2-D axial extension.
+
+import unittest
+
+from pylith.tests.FullTestApp import run_pylith, check_data
+
+import meshes
+from axialdisp_soln import AnalyticalSoln
+from axialdisp_gendb import GenerateDB
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class TestCase(unittest.TestCase):
+    """
+    Test suite for testing PyLith with 3-D axial compression/extension.
+    """
+    NAME = None  # Set in child class.
+    DIRICHLET_BOUNDARIES = ["bc_xneg", "bc_xpos", "bc_yneg", "bc_ypos", "bc_zneg"]
+
+    def setUp(self):
+        """
+        Setup for test.
+        """
+        self.exactsoln = AnalyticalSoln()
+        self.verbosity = 0
+        return
+
+    def run_pylith(self, testName, args):
+        if self.verbosity > 0:
+            print("Running Pylith with args '{}' ...".format(" ".join(args)))
+        run_pylith(testName, args, GenerateDB)
+        return
+
+    def test_domain_solution(self):
+        filename = "output/{}-domain.h5".format(self.NAME)
+        vertexFields = ["displacement"]
+        check_data(filename, self, self.DOMAIN, vertexFields=vertexFields)
+        return
+
+    def test_material_info(self):
+        cellFields = ["density", "bulk_modulus", "shear_modulus"]
+        for material in self.MATERIALS.keys():
+            filename = "output/{}-{}_info.h5".format(self.NAME, material)
+            check_data(filename, self, self.MATERIALS[material], cellFields=cellFields)
+        return
+
+    def test_material_solution(self):
+        vertexFields = ["displacement", "cauchy_strain", "cauchy_stress"]
+        for material in self.MATERIALS.keys():
+            filename = "output/{}-{}.h5".format(self.NAME, material)
+            check_data(filename, self, self.MATERIALS[material], vertexFields=vertexFields)
+        return
+
+    def test_bcdirichlet_info(self):
+        vertexFields = ["initial_amplitude"]
+        for bc in self.DIRICHLET_BOUNDARIES:
+            filename = "output/{}-{}_info.h5".format(self.NAME, bc)
+            check_data(filename, self, self.BOUNDARIES[bc], vertexFields=vertexFields)
+        return
+
+    def test_bcdirichlet_solution(self):
+        vertexFields = ["displacement"]
+        for bc in self.DIRICHLET_BOUNDARIES:
+            filename = "output/{}-{}.h5".format(self.NAME, bc)
+            check_data(filename, self, self.BOUNDARIES[bc], vertexFields=vertexFields)
+        return
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class TestHex(TestCase, meshes.Hex):
+    NAME = "axialdisp_hex"
+
+    def setUp(self):
+        TestCase.setUp(self)
+        TestCase.run_pylith(self, self.NAME, ["axialdisp.cfg", "axialdisp_hex.cfg"])
+        return
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class TestTet(TestCase, meshes.Tet):
+    NAME = "axialdisp_tet"
+
+    def setUp(self):
+        TestCase.setUp(self)
+        TestCase.run_pylith(self, self.NAME, ["axialdisp.cfg", "axialdisp_tet.cfg"])
+        return
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def test_cases():
+    return [
+        TestHex,
+        TestTet,
+    ]
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    suite = unittest.TestSuite()
+    for test in test_cases():
+        suite.addTest(unittest.makeSuite(test))
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+# End of file
