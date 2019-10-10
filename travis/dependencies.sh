574e4b9bf324c76d944322c315aa75560a0ad868
Brad Aagaard <baagaard@usgs.gov>
2019-10-10 15:41:37 -0700

    Use PyLith installer to get proper SWIG/PCRE for Travis-CI.

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -11,7 +11,7 @@ git clone --depth 1 --recursive https://github.com/geodynamics/pylith_installer.
 
 pushd pylith_installer && \
   autoreconf --install --verbose && \
-  ./configure --prefix=${DEPS_DIR} --disable-cmake --disable-cppunit --disable-proj4 --enable-numpy --enable-hdf5 --enable-netcdf --enable-netcdfpy --enable-nemesis --enable-fiat --enable-swig --with-fortran=no --enable-force-install --with-make-threads=${MAKE_THREADS} CFLAGS="-O2 -w" CXXFLAGS="-O2 -w" && \
+  ./configure --prefix=${DEPS_DIR} --disable-cmake --disable-cppunit --disable-proj4 --enable-numpy --enable-hdf5 --enable-netcdf --enable-netcdfpy --enable-nemesis --enable-fiat --enable-pcre --enable-swig --with-fortran=no --enable-force-install --with-make-threads=${MAKE_THREADS} CFLAGS="-O2 -w" CXXFLAGS="-O2 -w" && \
   . ./setup.sh && \
   make external_deps && \
   popd
