db8876d9f557154051de45faff2d35dd3968e43b
Matthew G. Knepley <knepley@gmail.com>
2020-04-22 17:08:20 -0400

    Tests: Correct constructor for Discretization

--- tests/mmstests/faults/TestFaultKin.cc
+++ tests/mmstests/faults/TestFaultKin.cc
@@ -98,7 +98,7 @@ pylith::mmstests::TestFaultKin::_initialize(void) {
 
     for (int i = 0; i < _data->matNumAuxSubfields; ++i) {
         const pylith::topology::FieldBase::Discretization& info = _data->matAuxDiscretizations[i];
-        _material->setAuxiliarySubfieldDiscretization(_data->matAuxSubfields[i], info.basisOrder, info.quadOrder,
+        _material->setAuxiliarySubfieldDiscretization(_data->matAuxSubfields[i], info.tensorBasis, info.basisOrder, info.quadOrder,
                                                       _data->spaceDim, info.isBasisContinuous, info.feSpace);
     } // for
 
@@ -113,7 +113,7 @@ pylith::mmstests::TestFaultKin::_initialize(void) {
     _data->kinsrc->auxFieldDB(_data->faultAuxDB);
     for (int i = 0; i < _data->faultNumAuxSubfields; ++i) {
         const pylith::topology::FieldBase::Discretization& info = _data->faultAuxDiscretizations[i];
-        _fault->setAuxiliarySubfieldDiscretization(_data->faultAuxSubfields[i], info.basisOrder, info.quadOrder,
+        _fault->setAuxiliarySubfieldDiscretization(_data->faultAuxSubfields[i], info.tensorBasis, info.basisOrder, info.quadOrder,
                                                    _data->spaceDim-1, info.isBasisContinuous, info.feSpace);
     } // for
 
