63e549307eee7e21d5961e60e148452847db4ff7
Charles Williams <C.Williams at gns.cri.nz>
2020-10-29 11:16:52 +1300

    Added Python script to compute 2D Jacobians, and cleaned up existing Python Jacobian scripts.

--- tests/manual/2d/powerlaw/jacobians/powerlaw2d.py
+++ tests/manual/2d/powerlaw/jacobians/powerlaw2d.py
@@ -38,15 +38,14 @@ one = sympy.sympify(1)
 two = sympy.sympify(2)
 three = sympy.sympify(3)
 four = sympy.sympify(4)
-ndim = 3
+ndim = 2
 
 # Define basis and displacement vector.
-from sympy.abc import x, y, z
-X = [x, y, z]
-u1 = sympy.Function('u1')(x,y,z)
-u2 = sympy.Function('u2')(x,y,z)
-u3 = sympy.Function('u3')(x,y,z)
-U = [u1, u2, u3]
+from sympy.abc import x, y
+X = [x, y]
+u1 = sympy.Function('u1')(x,y)
+u2 = sympy.Function('u2')(x,y)
+U = [u1, u2]
 
 # Deformation gradient, transpose, and strain tensor.
 defGrad = sympy.derive_by_array(U, X)
@@ -61,25 +60,18 @@ devStrain = strain - volStrainArr/three
 # ----------------------------------------------------------------------
 # Power-law viscoelastic.
 print('Power-law viscoelastic material:')
-fileName = 'elasticity-powerlaw_iso3d.txt'
+fileName = 'elasticity-powerlaw_iso2d.txt'
 aT, n, alpha, deltaT = sympy.symbols('aT n alpha deltaT')
 bulkModulus, shearModulus = sympy.symbols('bulkModulus shearModulus')
-s11, s12, s13, s22, s23, s33 = sympy.symbols('s11 s12 s13 s22 s23 s33')
-s11T, s12T, s13T, s22T, s23T, s33T = sympy.symbols('s11T s12T s13T s22T s23T s33T')
-s11I, s12I, s13I, s22I, s23I, s33I = sympy.symbols('s11I s12I s13I s22I s23I s33I')
-s11FTau, s12FTau, s13FTau, s22FTau, s23FTau, s33FTau = sympy.symbols(
-  's11FTau s12FTau s13FTau s22FTau s23FTau s33FTau')
+s11, s12, s22 = sympy.symbols('s11 s12 s22')
+s11T, s12T, s22T = sympy.symbols('s11T s12T s22T')
+s11I, s12I, s22I = sympy.symbols('s11I s12I s22I')
+s11FTau, s12FTau, s22FTau = sympy.symbols('s11FTau s12FTau s22FTau')
 j2FTplusDt, j2FT, j2FTau, gammaFTau = sympy.symbols('j2FTplusDt j2FT j2FTau gammaFTau')
 meanStress = bulkModulus * volStrainArr
-devStress = sympy.Matrix([[s11, s12, s13],
-                          [s12, s22, s23],
-                          [s13, s23, s33]])
-devStressT = sympy.Matrix([[s11T, s12T, s13T],
-                           [s12T, s22T, s23T],
-                           [s13T, s23T, s33T]])
-devStressI = sympy.Matrix([[s11I, s12I, s13I],
-                           [s12I, s22I, s23I],
-                           [s13I, s23I, s33I]])
+devStress = sympy.Matrix([[s11, s12], [s12, s22]])
+devStressT = sympy.Matrix([[s11T, s12T], [s12T, s22T]])
+devStressI = sympy.Matrix([[s11I, s12I], [s12I, s22I]])
 devStressTau = alpha*devStress + (one - alpha)*devStressT
 j2TplusDt = sympy.sqrt(jc.innerProd(devStress, devStress)/two)
 j2T = sympy.sqrt(jc.innerProd(devStressT, devStressT)/two)
@@ -88,8 +80,8 @@ aE = one/(two*shearModulus)
 gammaTau = aT*(j2Tau)**(n-one)
 G = aE + deltaT*alpha*gammaTau
 H = deltaT*(one - alpha)*gammaTau
-GMat = G*sympy.ones(3,3)
-HMat = H*sympy.ones(3,3)
+GMat = G*sympy.ones(2,2)
+HMat = H*sympy.ones(2,2)
 
 # Derivatives.
 dGDdevStress = sympy.derive_by_array(G, devStress).tomatrix()
@@ -107,14 +99,11 @@ denomSimp = denom.subs([(gammaTau, gammaFTau),
                         (j2T, j2FT),
                         (devStressTau[0], s11FTau),
                         (devStressTau[1], s12FTau),
-                        (devStressTau[2], s13FTau),
-                        (devStressTau[4], s22FTau),
-                        (devStressTau[5], s23FTau),
-                        (devStressTau[8], s33FTau)])
+                        (devStressTau[3], s22FTau)])
 
 jacobianDev = jc.divideTensors(dDevStrainDstrain, denomSimp)
 jacobianVol = sympy.derive_by_array(meanStress, defGrad)
 jacobian = jacobianDev + jacobianVol
 jc.writeJacobianInfo(fileName, jacobian)
 if (checkJacobianSymmetry):
-    jc.checkSymmetry(jacobian, 'powerlaw3d_jacobian_symmetry.txt')
+    jc.checkSymmetry(jacobian, 'powerlaw2d_jacobian_symmetry.txt')
