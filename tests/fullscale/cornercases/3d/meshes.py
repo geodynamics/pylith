85e7ff893196d6d1cd9f55b260750e8548299744
Charles Williams <C.Williams at gns.cri.nz>
2021-04-06 12:36:43 +1200

    Updated meshes and .cfg file for inner boundary BC.

--- tests/fullscale/cornercases/3d/meshes.py
+++ tests/fullscale/cornercases/3d/meshes.py
@@ -18,6 +18,56 @@
 # @brief Mesh information for test cases.
 
 
+class CylinderTet(object):
+    """
+    Mesh information for cylindrical tet mesh.
+    """
+    DOMAIN = {
+        "ncells": 84,
+        "ncorners": 4,
+        "nvertices": 38,
+    }
+    MATERIALS = {
+        "elastic": {
+            "ncells": 84,
+            "ncorners": 4,
+            "nvertices": 38,
+        },
+    }
+    BOUNDARIES = {
+        "bc_xneg": {
+            "ncells": 7,
+            "ncorners": 3,
+            "nvertices": 8,
+        },
+        "bc_yneg": {
+            "ncells": 7,
+            "ncorners": 3,
+            "nvertices": 8,
+        },
+        "bc_zneg": {
+            "ncells": 17,
+            "ncorners": 3,
+            "nvertices": 15,
+        },
+        "bc_zpos": {
+            "ncells": 15,
+            "ncorners": 3,
+            "nvertices": 14,
+        },
+        "bc_outer": {
+            "ncells": 8,
+            "ncorners": 3,
+            "nvertices": 10,
+        },
+        "bc_inner": {
+            "ncells": 10,
+            "ncorners": 3,
+            "nvertices": 11,
+        },
+    }
+
+
 class Tet(object):
     """
     Mesh information for tet mesh.
@@ -97,6 +147,56 @@ class Hex(object):
         },
         "bc_domain": {
             "ncells": 6,
+            "ncorners": 4,
+            "nvertices": 8,
+        },
+    }
+
+
+class CylinderHex(object):
+    """
+    Mesh information for cylindrical hex mesh.
+    """
+    DOMAIN = {
+        "ncells": 6,
+        "ncorners": 8,
+        "nvertices": 24,
+    }
+    MATERIALS = {
+        "elastic": {
+            "ncells": 6,
+            "ncorners": 8,
+            "nvertices": 24,
+        },
+    }
+    BOUNDARIES = {
+        "bc_xneg": {
+            "ncells": 2,
+            "ncorners": 4,
+            "nvertices": 6,
+        },
+        "bc_yneg": {
+            "ncells": 2,
+            "ncorners": 4,
+            "nvertices": 6,
+        },
+        "bc_zneg": {
+            "ncells": 6,
+            "ncorners": 4,
+            "nvertices": 12,
+        },
+        "bc_zpos": {
+            "ncells": 6,
+            "ncorners": 4,
+            "nvertices": 12,
+        },
+        "bc_outer": {
+            "ncells": 3,
+            "ncorners": 4,
+            "nvertices": 8,
+        },
+        "bc_inner": {
+            "ncells": 3,
             "ncorners": 4,
             "nvertices": 8,
         },
