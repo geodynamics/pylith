fa9628b73946dda9856a61dd8f0d68694f85b77b
Robert Walker <rwalker6@buffalo.edu>
2021-04-21 22:11:16 -0500

    Added mms test

---
+++ tests/mmstests/poroelasticity/TestPoroelasticity.cc
@@ -0,0 +1,183 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2015 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+#include <portinfo>
+
+#include "TestPoroelasticity.hh" // Implementation of class methods
+
+#include "pylith/problems/TimeDependent.hh" // USES TimeDependent
+#include "pylith/materials/Poroelasticity.hh" // USES Poroelasticity
+
+#include "pylith/materials/Query.hh" // USES Query
+
+#include "pylith/topology/Mesh.hh" // USES Mesh
+#include "pylith/topology/MeshOps.hh" // USES MeshOps::nondimensionalize()
+#include "pylith/topology/Field.hh" // USES Field
+#include "pylith/topology/Fields.hh" // USES Fields
+#include "pylith/topology/VisitorMesh.hh" // USES VecVisitorMesh
+#include "pylith/topology/FieldQuery.hh" // USES FieldQuery
+#include "pylith/feassemble/AuxiliaryFactory.hh" // USES AuxiliaryFactory
+#include "pylith/problems/SolutionFactory.hh" // USES SolutionFactory
+#include "pylith/meshio/MeshIOAscii.hh" // USES MeshIOAscii
+#include "pylith/bc/DirichletUserFn.hh" // USES DirichletUserFn
+#include "pylith/utils/error.hh" // USES PYLITH_METHOD_BEGIN/END
+#include "pylith/utils/journals.hh" // pythia::journal
+
+#include "spatialdata/spatialdb/UserFunctionDB.hh" // USES UserFunctionDB
+#include "spatialdata/geocoords/CoordSys.hh" // USES CoordSys
+#include "spatialdata/spatialdb/GravityField.hh" // USES GravityField
+#include "spatialdata/units/Nondimensional.hh" // USES Nondimensional
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Setup testing data.
+void
+pylith::mmstests::TestPoroelasticity::setUp(void) {
+    MMSTest::setUp();
+
+    _material = new pylith::materials::Poroelasticity;CPPUNIT_ASSERT(_material);
+    _bcDisp = new pylith::bc::DirichletUserFn;CPPUNIT_ASSERT(_bcDisp);
+    _bcPres = new pylith::bc::DirichletUserFn;CPPUNIT_ASSERT(_bcPres);
+    _data = NULL;
+} // setUp
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Deallocate testing data.
+void
+pylith::mmstests::TestPoroelasticity::tearDown(void) {
+    delete _material;_material = NULL;
+    delete _bcDisp;_bcDisp = NULL;
+    delete _bcPres;_bcPres = NULL;
+    delete _data;_data = NULL;
+
+    MMSTest::tearDown();
+} // tearDown
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Initialize objects for test.
+void
+pylith::mmstests::TestPoroelasticity::_initialize(void) {
+    PYLITH_METHOD_BEGIN;
+
+    CPPUNIT_ASSERT(_mesh);
+    pylith::meshio::MeshIOAscii iohandler;
+    CPPUNIT_ASSERT(_data->meshFilename);
+    iohandler.filename(_data->meshFilename);
+    iohandler.read(_mesh);CPPUNIT_ASSERT(_mesh);
+
+    CPPUNIT_ASSERT_MESSAGE("Test mesh does not contain any cells.", _mesh->numCells() > 0);
+    CPPUNIT_ASSERT_MESSAGE("Test mesh does not contain any vertices.", _mesh->numVertices() > 0);
+
+    // Set up coordinates.
+    _mesh->setCoordSys(_data->cs);
+    CPPUNIT_ASSERT(_data->normalizer);
+    pylith::topology::MeshOps::nondimensionalize(_mesh, *_data->normalizer);
+
+    // Set up material
+    CPPUNIT_ASSERT(_material);
+    _material->setAuxiliaryFieldDB(_data->auxDB);
+
+    for (int i = 0; i < _data->numAuxSubfields; ++i) {
+        const pylith::topology::FieldBase::Discretization& info = _data->auxDiscretizations[i];
+        _material->setAuxiliarySubfieldDiscretization(_data->auxSubfields[i], info.basisOrder, info.quadOrder,
+                                                      _data->spaceDim, pylith::topology::FieldBase::DEFAULT_BASIS,
+                                                      info.isBasisContinuous, info.feSpace);
+    } // for
+
+    // Set up problem.
+    CPPUNIT_ASSERT(_problem);
+    CPPUNIT_ASSERT(_data->normalizer);
+    _problem->setNormalizer(*_data->normalizer);
+    _problem->setGravityField(_data->gravityField);
+    pylith::materials::Material* materials[1] = { _material };
+    _problem->setMaterials(materials, 1);
+    pylith::bc::BoundaryCondition* bcs[2] = { _bcDisp, _bcPres };
+    _problem->setBoundaryConditions(bcs, 2);
+    _problem->setStartTime(_data->startTime);
+    _problem->setEndTime(_data->endTime);
+    _problem->setInitialTimeStep(_data->timeStep);
+
+    // Set up solution field.
+    CPPUNIT_ASSERT( (!_data->isExplicit && 3 == _data->numSolnSubfields) ||
+                    (_data->isExplicit && 3 == _data->numSolnSubfields) );
+    CPPUNIT_ASSERT(_data->solnDiscretizations);
+
+    CPPUNIT_ASSERT(!_solution);
+    _solution = new pylith::topology::Field(*_mesh);CPPUNIT_ASSERT(_solution);
+    _solution->setLabel("solution");
+    pylith::problems::SolutionFactory factory(*_solution, *_data->normalizer);
+
+    if (_data->isExplicit) {
+        factory.addDisplacement(_data->solnDiscretizations[0]);
+        factory.addPressure(_data->solnDiscretizations[1]);
+        factory.addVelocity(_data->solnDiscretizations[2]);
+    } else {
+        factory.addDisplacement(_data->solnDiscretizations[0]);
+        factory.addPressure(_data->solnDiscretizations[1]);
+        factory.addTraceStrain(_data->solnDiscretizations[2]);
+    } // if else
+    _problem->setSolution(_solution);
+
+    pylith::testing::MMSTest::_initialize();
+
+    PYLITH_METHOD_END;
+} // _initialize
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Constructor
+pylith::mmstests::TestPoroelasticity_Data::TestPoroelasticity_Data(void) :
+    spaceDim(0),
+    meshFilename(NULL),
+    boundaryLabel(NULL),
+    cs(NULL),
+    gravityField(NULL),
+    normalizer(new spatialdata::units::Nondimensional),
+
+    startTime(0.0),
+    endTime(0.0),
+    timeStep(0.0),
+
+    numSolnSubfields(0),
+    solnDiscretizations(NULL),
+
+    numAuxSubfields(0),
+    auxSubfields(NULL),
+    auxDiscretizations(NULL),
+    auxDB(new spatialdata::spatialdb::UserFunctionDB),
+
+    isExplicit(false) {
+    CPPUNIT_ASSERT(normalizer);
+
+    CPPUNIT_ASSERT(auxDB);
+    auxDB->setLabel("auxiliary field spatial database");
+} // constructor
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Destructor
+pylith::mmstests::TestPoroelasticity_Data::~TestPoroelasticity_Data(void) {
+    delete cs;cs = NULL;
+    delete gravityField;gravityField = NULL;
+    delete normalizer;normalizer = NULL;
+    delete auxDB;auxDB = NULL;
+} // destructor
+
+
+// End of file
---
+++ tests/mmstests/poroelasticity/TestPoroelasticity.hh
@@ -0,0 +1,115 @@
+// -*- C++ -*-
+//
+// ----------------------------------------------------------------------
+//
+// Brad T. Aagaard, U.S. Geological Survey
+// Charles A. Williams, GNS Science
+// Matthew G. Knepley, University of Chicago
+//
+// This code was developed as part of the Computational Infrastructure
+// for Geodynamics (http://geodynamics.org).
+//
+// Copyright (c) 2010-2015 University of California, Davis
+//
+// See COPYING for license information.
+//
+// ----------------------------------------------------------------------
+//
+
+/**
+ * @file mmstets/poroelasticity/TestPoroelasticity.hh
+ *
+ * @brief C++ abstract base class for testing Poroelasticity for various rheologies.
+ */
+
+#if !defined(pylith_mmstests_testporoelasticity_hh)
+#define pylith_mmstests_testporoelasticity_hh
+
+#include "pylith/testing/MMSTest.hh" // ISA MMSTEST
+
+#include "pylith/materials/materialsfwd.hh" // forward declarations
+#include "pylith/bc/bcfwd.hh" // forward declarations
+#include "pylith/topology/topologyfwd.hh" // forward declarations
+#include "pylith/topology/Field.hh" // HASA FieldBase::Discretization
+
+#include "spatialdata/spatialdb/spatialdbfwd.hh" // HOLDSA UserFunctionDB
+#include "spatialdata/geocoords/geocoordsfwd.hh" // HOLDSA CoordSys
+#include "spatialdata/units/unitsfwd.hh" // HOLDSA Nondimensional
+
+namespace pylith {
+    namespace mmstests {
+        class TestPoroelasticity;
+
+        class TestPoroelasticity_Data; // test data
+    } // mmstets
+} // pylith
+
+/// C++ abstract base class for testing Poroelasticity with various rheologies.
+class pylith::mmstests::TestPoroelasticity : public pylith::testing::MMSTest {
+    // PUBLIC METHODS //////////////////////////////////////////////////////////////////////////////////////////////////
+public:
+
+    /// Setup testing data.
+    virtual
+    void setUp(void);
+
+    /// Deallocate testing data.
+    virtual
+    void tearDown(void);
+
+    // PROTECTED METHODS ///////////////////////////////////////////////////////////////////////////////////////////////
+protected:
+
+    /// Initialize objects for test.
+    void _initialize(void);
+
+    // PROTECTED MEMBERS ///////////////////////////////////////////////////////////////////////////////////////////////
+protected:
+
+    pylith::materials::Poroelasticity* _material; ///< Material for testing.
+    pylith::bc::DirichletUserFn* _bcDisp; ///< Dirichlet boundary condition, Displacement.
+    pylith::bc::DirichletUserFn* _bcPres; ///< Dirichlet boundary condition, Pressure.    
+    TestPoroelasticity_Data* _data; ///< Test parameters.
+
+}; // class TestPoroelasticity
+
+// =====================================================================================================================
+class pylith::mmstests::TestPoroelasticity_Data {
+    // PUBLIC METHODS //////////////////////////////////////////////////////////////////////////////////////////////////
+public:
+
+    /// Constructor
+    TestPoroelasticity_Data(void);
+
+    /// Destructor
+    ~TestPoroelasticity_Data(void);
+
+    // PUBLIC MEMBERS //////////////////////////////////////////////////////////////////////////////////////////////////
+public:
+
+    int spaceDim; ///< Spatial dimension of domain.
+    const char* meshFilename; ///< Name of file with ASCII mesh.
+    const char* boundaryLabel; ///< Group defining domain boundary.
+
+    spatialdata::geocoords::CoordSys* cs; ///< Coordinate system.
+    spatialdata::spatialdb::GravityField* gravityField; ///< Gravity field.
+    spatialdata::units::Nondimensional* normalizer; ///< Scales for nondimensionalization.
+
+    PylithReal startTime; ///< Start time for simulation.
+    PylithReal endTime; ///< End time for simulation.
+    PylithReal timeStep; ///< Time step in simulation.
+
+    int numSolnSubfields; ///< Number of solution fields.
+    pylith::topology::Field::Discretization* solnDiscretizations; ///< Discretizations for solution fields.
+
+    int numAuxSubfields; ///< Number of auxiliary subfields.
+    const char** auxSubfields; ///< Names of auxiliary subfields.
+    pylith::topology::Field::Discretization* auxDiscretizations; ///< Discretizations for auxiliary subfields.
+    spatialdata::spatialdb::UserFunctionDB* auxDB; ///< Spatial database with auxiliary field.
+
+    bool isExplicit; ///< True for explicit time stepping.
+}; // TestPoroelasticity_Data
+
+#endif // pylith_mmstests_testporoelasticity_hh
+
+// End of file
