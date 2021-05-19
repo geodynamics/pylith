b2eab0dcce47f2a71d8b4cba63006e354611fde5
Matthew G. Knepley <knepley@gmail.com>
2021-05-19 13:18:33 -0400

    MeshIO: Change to new Plex interface

--- libsrc/pylith/meshio/MeshBuilder.cc
+++ libsrc/pylith/meshio/MeshBuilder.cc
@@ -41,26 +41,28 @@ pylith::meshio::MeshBuilder::buildMesh(topology::Mesh* mesh,
                                        const int numCells,
                                        const int numCorners,
                                        const int meshDim,
-                                       const bool isParallel)
-{ // buildMesh
+                                       const bool isParallel) { // buildMesh
     PYLITH_METHOD_BEGIN;
 
     assert(mesh);
     assert(coordinates);
-    MPI_Comm comm  = mesh->comm();
-    PetscInt dim  = meshDim;
+    MPI_Comm comm = mesh->comm();
+    PetscInt dim = meshDim;
     PetscErrorCode err;
 
     { // Check to make sure every vertex is in at least one cell.
       // This is required by PETSc
         std::vector<bool> vertexInCell(numVertices, false);
         const int size = cells.size();
-        for (int i=0; i < size; ++i)
+        for (int i = 0; i < size; ++i) {
             vertexInCell[cells[i]] = true;
+        }
         int count = 0;
-        for (int i=0; i < numVertices; ++i)
-            if (!vertexInCell[i])
+        for (int i = 0; i < numVertices; ++i) {
+            if (!vertexInCell[i]) {
                 ++count;
+            }
+        }
         if (count > 0) {
             std::ostringstream msg;
             msg << "Mesh contains " << count << " vertices that are not in any cells.";
@@ -72,25 +74,26 @@ pylith::meshio::MeshBuilder::buildMesh(topology::Mesh* mesh,
     PetscDM dmMesh = NULL;
     PetscBool interpolate = PETSC_TRUE; /* interpolate = interpolate ? PETSC_TRUE : PETSC_FALSE; */
 
-    err = MPI_Bcast(&dim, 1, MPIU_INT, 0, comm); PYLITH_CHECK_ERROR(err);
-    err = MPI_Bcast(&spaceDim, 1, MPIU_INT, 0, comm); PYLITH_CHECK_ERROR(err);
+    err = MPI_Bcast(&dim, 1, MPIU_INT, 0, comm);PYLITH_CHECK_ERROR(err);
+    err = MPI_Bcast(&spaceDim, 1, MPIU_INT, 0, comm);PYLITH_CHECK_ERROR(err);
     const PetscInt bound = numCells*numCorners;
     for (PetscInt coff = 0; coff < bound; coff += numCorners) {
-      DMPolytopeType ct;
+        DMPolytopeType ct;
 
-      if (dim < 3) continue;
-      switch (numCorners) {
+        if (dim < 3) { continue;}
+        switch (numCorners) {
         case 4: ct = DM_POLYTOPE_TETRAHEDRON;break;
         case 6: ct = DM_POLYTOPE_TRI_PRISM;break;
         case 8: ct = DM_POLYTOPE_HEXAHEDRON;break;
         default: continue;
-      }
-      err = DMPlexInvertCell(ct, (int *) &cells[coff]); PYLITH_CHECK_ERROR(err);
+        }
+        err = DMPlexInvertCell(ct, (int *) &cells[coff]);PYLITH_CHECK_ERROR(err);
     }
-    err = DMPlexCreateFromCellList(comm, dim, numCells, numVertices, numCorners, interpolate, &cells[0], spaceDim, &(*coordinates)[0], &dmMesh); PYLITH_CHECK_ERROR(err);
+    err = DMPlexCreateFromCellListPetsc(comm, dim, numCells, numVertices, numCorners, interpolate, &cells[0], spaceDim, &(*coordinates)[0], &dmMesh);PYLITH_CHECK_ERROR(err);
     mesh->dmMesh(dmMesh);
 
     PYLITH_METHOD_END;
 } // buildMesh
+
 
 // End of file
