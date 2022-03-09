3c356f1d2c2730570892ed7d0252ae3cad7e80aa
Charles Williams <C.Williams at gns.cri.nz>
2022-03-10 10:03:01 +1300

    More cleanup.

--- tests/manual/3d/powerlaw/run_axialtract_n1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pylith axialtraction_powerlaw_n1_norefstate.cfg axialtraction_powerlaw_n1_norefstate_dt0.01_onecell_hex.cfg > axialtraction_powerlaw_n1_norefstate_dt0.01_onecell_hex.log 2>&1
-pylith axialtraction_powerlaw_n1_norefstate.cfg axialtraction_powerlaw_n1_norefstate_dt0.02_onecell_hex.cfg > axialtraction_powerlaw_n1_norefstate_dt0.02_onecell_hex.log 2>&1
-pylith axialtraction_powerlaw_n1_norefstate.cfg axialtraction_powerlaw_n1_norefstate_dt0.05_onecell_hex.cfg > axialtraction_powerlaw_n1_norefstate_dt0.05_onecell_hex.log 2>&1
-pylith axialtraction_powerlaw_n1_norefstate.cfg axialtraction_powerlaw_n1_norefstate_dt0.1_onecell_hex.cfg > axialtraction_powerlaw_n1_norefstate_dt0.1_onecell_hex.log 2>&1
-pylith axialtraction_powerlaw_n1_norefstate.cfg axialtraction_powerlaw_n1_norefstate_dt0.2_onecell_hex.cfg > axialtraction_powerlaw_n1_norefstate_dt0.2_onecell_hex.log 2>&1
