da1bf6e586cffc57390db5c1d3ddf1fa6d509cb0
Brad Aagaard <baagaard@usgs.gov>
2022-04-01 22:35:46 -0600

    Refactor 2d/powerlaw/run_tests.py (use class with methods).

--- tests/manual/2d/powerlaw/run_tests.py
+++ tests/manual/2d/powerlaw/run_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env nemesis
 
 # ----------------------------------------------------------------------
 #
@@ -14,16 +14,13 @@
 # See COPYING for license information.
 #
 # ----------------------------------------------------------------------
-#
-# @file tests/manual/2d/powerlaw/run_tests.py
-#
-# @brief Run power-law manual tests.
-#
 
-import numpy
-import subprocess
+import contextlib
+import io
 import importlib
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
+
+from pylith.apps.PyLithApp import PyLithApp
 
 import axialtraction_powerlaw_n1_gendb as gendb
 
@@ -40,91 +37,99 @@ p_youngs = p_mu*(3.0*p_lambda + 2.0*p_mu)/(p_lambda + p_mu)
 p_poissons = 0.5*p_lambda/(p_lambda + p_mu)
 
 # Uniform stress field (plane strain).
-T0 = -1.0e9
+p_t0 = -1.0e+9
 
-params = {'p_density': p_density,
-          'p_vs': p_vs,
-          'p_vp': p_vp,
-          'p_viscosity': p_viscosity,
-          'p_power_law_exponent': p_power_law_exponent,
-          'p_power_law_reference_strain_rate': p_power_law_reference_strain_rate,
-          'p_power_law_reference_stress': p_power_law_reference_stress,
-          'p_mu': p_mu,
-          'p_lambda': p_lambda,
-          'p_youngs': p_youngs,
-          'p_poissons': p_poissons,
-          'T0': T0}
+params = {
+    'p_density': p_density,
+    'p_vs': p_vs,
+    'p_vp': p_vp,
+    'p_viscosity': p_viscosity,
+    'p_power_law_exponent': p_power_law_exponent,
+    'p_power_law_reference_strain_rate': p_power_law_reference_strain_rate,
+    'p_power_law_reference_stress': p_power_law_reference_stress,
+    'p_mu': p_mu,
+    'p_lambda': p_lambda,
+    'p_youngs': p_youngs,
+    'p_poissons': p_poissons,
+    'T0': p_t0,
+    }
 
-allSims = ['axialtraction_powerlaw', 'axialtraction_powerlaw_n1', 'sheartraction_powerlaw']
-testJacobianArgs = ['--petsc.snes_test_jacobian', '--petsc.snes_test_jacobian_view']
-dt = [0.01, 0.05, 0.1, 0.5, 1.0, 5.0]
-numSteps = len(dt)
-simArgs = {'axialtraction_powerlaw': ['pylith', 'axialtraction_powerlaw.cfg'],
-           'axialtraction_powerlaw_n1': ['pylith', 'axialtraction_powerlaw_n1.cfg'],
-           'sheartraction_powerlaw': ['pylith', 'sheartraction_powerlaw.cfg']}
 
-def run_sims(sims, jacobianArgs):
-    """
-    Run simulations and plot results for power-law.
-    """
+class RunnerApp():
 
-    # Generate spatialdb for axial traction (n=1).
-    gendb.run(params)
+    SIMS = {
+        'axialtraction_powerlaw': ['axialtraction_powerlaw.cfg'],
+        'axialtraction_powerlaw_n1': ['axialtraction_powerlaw_n1.cfg'],
+        'sheartraction_powerlaw': ['sheartraction_powerlaw.cfg'],
+        }
+    TIMESTEPS = [0.01, 0.05, 0.1, 0.5, 1.0, 5.0]
 
-    # Create plot.
-    numSims = len(sims)
-    numRows = 4
-    useJacobian = True
-    if len(jacobianArgs) == 0:
-        numRows = 3
-        useJacobian = False
-    fig, axs = plt.subplots(numRows, numSims)
-    axs = axs.reshape(numRows, numSims)
-    simNum = 0
-
-    for sim in sims:
-        for stepNum in range(numSteps):
-            dtStr = repr(dt[stepNum])
-            baseName = sim + '_dt' + dtStr
-            logFile = baseName + '.log'
-            nameArg = '--problem.defaults.name=' + baseName
-            dumpArg = '--dump_parameters.filename=output/' + baseName + '-parameters.json'
-            progArg = '--problem.progress_monitor.filename=output/' + baseName + '-progress.txt'
-            stepArg = '--problem.initial_dt=' + dtStr + '*year'
-            metaArg = '--metadata.arguments=[' + simArgs[sim][-1] + ']'
-            runargs = simArgs[sim] + [nameArg, dumpArg, progArg, stepArg, metaArg] + jacobianArgs
-            f = open(logFile, 'w')
-            subprocess.run(runargs, stdout=f, stderr=f, check=True)
-            f.close()
-        plotModule = 'plot_' + sim
-        plot = importlib.import_module(plotModule)
-        if (sim == 'axialtraction_powerlaw_n1'):
-            plot.run(axs, simNum, params, dt, useJacobian)
+    def main(self):
+        """Application entry point.
+        """
+        args = self._parse_command_line()
+        if args.sim == 'all':
+            sims = self.SIMS
         else:
-            plot.run(axs, simNum, dt, useJacobian)
+            sims = {args.sim: self.SIMS[args.sim]}
 
-        simNum += 1
+        self._generate_db()
+        self._setup_plot(len(sims), args.test_jacobian)
+        self._run_sims(sims, args.test_jacobian)
+        self._show_plot()
 
-    plt.show()
+    def _generate_db(self):
+        gendb.run(params)
+
+    def _setup_plot(self, nsims, use_jacobian):
+        nrows = 4 if use_jacobian else 3
+        fig, axes = pyplot.subplots(nrows, nsims)
+        self.axes = axes.reshape(nrows, nsims)
+
+    def _run_sims(self, sims, test_jacobian):
+        ARGS_JACOBIAN = ['--petsc.snes_test_jacobian', '--petsc.snes_test_jacobian_view']
+        args_jacobian = ARGS_JACOBIAN if test_jacobian else []
+
+        for isim, sim in enumerate(sims.keys()):
+            for dt in self.TIMESTEPS:
+                sim_fullname = f"{sim}_dt{dt:04.2f}"
+                log_filename = f"{sim_fullname}.log"
+
+                args_sim = [
+                    f"--problem.defaults.name={sim_fullname}",
+                    f"--dump_parameters.filename=output/{sim_fullname}-parameters.json",
+                    f"--problem.progress_monitor.filename=output/{sim_fullname}-progress.txt",
+                    f"--problem.initial_dt={dt:04.2f}*year",
+                ]
+                pylith_args = sims[sim] + args_sim + args_jacobian
+                with contextlib.redirect_stdout(io.StringIO()) as sout:
+                    app = PyLithApp()
+                    app.run(argv=["pylith"] + pylith_args)
+                with open(log_filename, "w") as log:
+                    log.write(sout.getvalue())
+
+            plot_module = f"plot_{sim}"
+            plotter = importlib.import_module(plot_module)
+            if (sim == 'axialtraction_powerlaw_n1'):
+                plotter.run(self.axes, isim, params, dt, test_jacobian)
+            else:
+                plotter.run(self.axes, isim, dt, test_jacobian)
+
+    def _show_plot(self):
+        pyplot.show()
+
+    def _parse_command_line(self):
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--sim", action="store", dest="sim", default="all", choices=list(self.SIMS.keys()), help="Simulations to run")
+        parser.add_argument("--test_jacobian", action="store_true", dest="test_jacobian", default=False, help="Test PyLith Jacobian")
+
+        return parser.parse_args()
+
 
 # ======================================================================
 if __name__ == "__main__":
-    import argparse
+    RunnerApp().main()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--sims", action="store", dest="sims", default="all", choices=allSims, help="simulations to run")
-    parser.add_argument("-j", "--test_jacobians", action="store_true", dest="test_jacobians", default=False, help="test PyLith Jacobians")
 
-    args = parser.parse_args()
-    if args.sims == 'all':
-        sims = allSims
-    else:
-        sims = [args.sims]
-
-    jacobianArgs = []
-    if args.test_jacobians:
-        jacobianArgs = testJacobianArgs
-
-    run_sims(sims, jacobianArgs)
-        
 # End of file
