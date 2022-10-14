4269da2dd1a9e9c5ea58866ebc387a5675aa4507
Brad Aagaard <baagaard@usgs.gov>
2022-10-14 14:44:09 -0600

    Add Python script for uploading release assets for GitHub.
    Also update downloads script to use requests Python module.

---
+++ developer/upload_release_asset.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""Application for uploading release assets to GitHub repository.
+"""
+
+import argparse
+import requests
+import mimetypes
+import os
+
+class App:
+    BASE_URL = "https://api.github.com/repos/geodynamics/{package}/releases"
+
+    def set_token(self):
+        import getpass
+
+        token = getpass.getpass(prompt="Enter GitHub personal access token: ")
+        os.environ["GITHUB_TOKEN"] = token
+
+    def upload_file(self,
+        filename: str,
+        release: str,
+        package: str="pylith",
+        ):
+
+        self._get_token()
+        self._get_release_info(package, release)
+
+        mimetype = mimetypes.guess_type(filename)[0]
+        if mimetype is None:
+            raise IOError(f"Unknown mimetype for '{filename}'.")
+
+        headers = {
+            "Authorization": f"token {self.token}",
+            "Content-Type": mimetype,
+            "Accept": "application/vnd.github.v3+json",
+            }
+        params = (("name", filename),)
+        data = open(filename, "rb").read()
+        response = requests.post(self.upload_url, headers=headers, params=params, data=data)
+        if response.status_code == 201:
+            download_url = response.json()["browser_download_url"]
+            print(f"Upload complete. Download URL: {download_url}")
+        else:
+            print("Error while uploading file.")
+            print(response.text)
+
+    def _get_token(self):
+        if "GITHUB_TOKEN" not in os.environ:
+            raise IOError("Could not find GitHub token in environment.")
+        self.token = os.environ["GITHUB_TOKEN"]
+
+    def _get_release_info(self, package, tag):
+        url = self.BASE_URL.format(package=package)
+        headers = {"Authorization": f"token {self.token}"}
+
+        response = requests.get(url, headers=headers)
+        releases = response.json()
+        for release in releases:
+            if release["tag_name"] == tag:
+                self.release_id = release["id"]
+                self.upload_url = release["upload_url"].replace("{?name,label}","")
+                return
+        tags = "\n\t".join([release["tag_name"] for release in releases])
+        raise ValueError(
+            f"Could not find release '{tag}' in {package} GitHub repository.\n"
+            f"Releases:\n\t{tags}"
+            )
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--set-token", dest="set_token", action="store_true")
+    parser.add_argument("--upload", dest="upload", action="store")
+    parser.add_argument("--package", dest="package", action="store", default="pylith")
+    parser.add_argument("--release", dest="release", action="store", required=True)
+    args = parser.parse_args()
+
+    app = App()
+    if args.set_token:
+        app.set_token()
+    if args.upload:
+        app.upload_file(filename=args.upload, release=args.release, package=args.package)
+
+if __name__ == "__main__":
+    main()
