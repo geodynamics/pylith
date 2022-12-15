a39e66397d60fa774b0b25f888943cff464d32ef
Brad Aagaard <baagaard@usgs.gov>
2022-12-14 18:02:00 -0700

    Add full-scale test case with minimal fault in 2D.

---
+++ tests/fullscale/cornercases/faults-2d/TestTwoBlocks.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env nemesis
+#
+# ----------------------------------------------------------------------
+#
+# Brad T. Aagaard, U.S. Geological Survey
+# Charles A. Williams, GNS Science
+# Matthew G. Knepley, University at Buffalo
+#
+# This code was developed as part of the Computational Infrastructure
+# for Geodynamics (http://geodynamics.org).
+#
+# Copyright (c) 2010-2022 University of California, Davis
+#
+# See LICENSE.md for license information.
+#
+# ----------------------------------------------------------------------
+
+import unittest
+
+from pylith.testing.FullTestApp import (FullTestCase, Check)
+
+import meshes
+import twoblocks_soln
+
+
+# -------------------------------------------------------------------------------------------------
+class TestCase(FullTestCase):
+
+    def setUp(self):
+        defaults = {
+            "filename": "output/{name}-{mesh_entity}.h5",
+            "exact_soln": twoblocks_soln.AnalyticalSoln(),
+            "mesh": self.mesh,
+        }
+        self.checks = [
+            Check(
+                mesh_entities=["domain"],
+                vertex_fields=["displacement"],
+                defaults=defaults,
+            ),
+            Check(
+                mesh_entities=["elastic"],
+                filename="output/{name}-{mesh_entity}_info.h5",
+                cell_fields = ["density", "bulk_modulus", "shear_modulus"],
+                defaults=defaults,
+            ),
+            Check(
+                mesh_entities=["elastic"],
+                vertex_fields = ["displacement"],
+                cell_fields = ["cauchy_strain", "cauchy_stress"],
+                tolerance = 1.0e-4,
+                defaults=defaults,
+            ),
+            Check(
+                mesh_entities=["bc_xneg", "bc_xpos"],
+                filename="output/{name}-{mesh_entity}_info.h5",
+                vertex_fields=["initial_amplitude"],
+                defaults=defaults,
+            ),
+            Check(
+                mesh_entities=["bc_xneg", "bc_xpos"],
+                vertex_fields=["displacement"],
+                defaults=defaults,
+            ),
+        ]
+
+    def run_pylith(self, testName, args):
+        FullTestCase.run_pylith(self, testName, args)
+
+
+# -------------------------------------------------------------------------------------------------
+class TestQuadGmsh(TestCase):
+
+    def setUp(self):
+        self.name = "twoblocks_quad"
+        self.mesh = meshes.Quad()
+        super().setUp()
+
+        TestCase.run_pylith(self, self.name, ["twoblocks.cfg", "twoblocks_quad.cfg"])
+        return
+
+
+# -------------------------------------------------------------------------------------------------
+class TestTriGmsh(TestCase):
+
+    def setUp(self):
+        self.name = "twoblocks_tri"
+        self.mesh = meshes.Tri()
+        super().setUp()
+
+        TestCase.run_pylith(self, self.name, ["twoblocks.cfg", "twoblocks_tri.cfg"])
+        return
+
+
+# -------------------------------------------------------------------------------------------------
+def test_cases():
+    return [
+        TestQuadGmsh,
+        TestTriGmsh,
+    ]
+
+
+# -------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    FullTestCase.parse_args()
+
+    suite = unittest.TestSuite()
+    for test in test_cases():
+        suite.addTest(unittest.makeSuite(test))
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+# End of file
