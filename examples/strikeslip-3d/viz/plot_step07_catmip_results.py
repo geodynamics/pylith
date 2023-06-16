26fb3f827d6856d3392323d1872659aabed00858
Eric Lindsey <ericlindsey@users.noreply.github.com>
2023-06-16 15:58:43 -0600

    Update plot_step07 to show vectors

--- examples/strikeslip-3d/viz/plot_step07_catmip_results.py
+++ examples/strikeslip-3d/viz/plot_step07_catmip_results.py
@@ -12,31 +12,37 @@ import matplotlib.pyplot as plt
 import plotFaultFunctions
 
 FILENAME_impulses = "output/step05_greensfns-fault.h5"
+FILENAME_greensfns = "output/step05_greensfns-gps_stations.h5"
 FILENAME_observed = "output/step04_varslip-gps_stations.h5"
 CATMIP_results = "output/step07a_catmip-theta45.bin"
 show_plot=True # change to False if running without graphics
 
 ## Read the appropriate files
 
-# Station Observations (currently unused) 
-h5 = h5py.File(FILENAME_observed, "r")
-observed_coords = h5['geometry/vertices'][:] # Nstations x Ncomponents
-observed_displacement = h5['vertex_fields/displacement'][:,:,:].squeeze() # Nstations x Ncomponents
-h5.close()
+# Station Observations
+with h5py.File(FILENAME_observed, "r") as h5:
+    observed_coords = h5['geometry/vertices'][:] # Nstations x Ncomponents
+    observed_displacement = h5['vertex_fields/displacement'][:,:,:].squeeze() # Nstations x Ncomponents
 
-# Greens functions (impulse sources)
-h5 = h5py.File(FILENAME_impulses, "r")
-impulse_coords = h5['geometry/vertices'][:]
-impulse_slip = h5['vertex_fields/slip'][:,:,:].squeeze() # Nimpulses x Ntotalnodes x Ncomponents
-vertices = np.array(h5['geometry']['vertices'])
-cells = np.array(h5['viz']['topology']['cells'])
-h5.close()
-nimpulses = impulse_slip.shape[0]
+# Greens functions
+with h5py.File(FILENAME_greensfns, 'r') as h5:  # reads the hdf5 into a dictionary f
+    greens_fns = np.array(h5['vertex_fields']['displacement'])
 
 # un-interleave the greens functions
-# could add greens fns rotation matrix here too
+# could add a rotation matrix here too
+greens_fns_reordered = np.vstack((greens_fns[0::2,:,:],greens_fns[1::2,:,:]))
+
+# Impulse locations and components
+with h5py.File(FILENAME_impulses, "r") as h5:
+    impulse_coords = h5['geometry/vertices'][:]
+    impulse_slip = h5['vertex_fields/slip'][:,:,:].squeeze() # Nimpulses x Ntotalnodes x Ncomponents
+    vertices = np.array(h5['geometry']['vertices'])
+    cells = np.array(h5['viz']['topology']['cells'])
+    nimpulses = impulse_slip.shape[0]
+
+# un-interleave the impulses
+# could add a rotation matrix here too
 impulse_slip_reordered = np.vstack((impulse_slip[0::2,:,:],impulse_slip[1::2,:,:]))
-print(np.shape(impulse_slip_reordered))
 
 # CATMIP results output
 with open(CATMIP_results, "rb") as fin:
@@ -45,7 +51,6 @@ inversion_coefs = coefs.reshape((nimpulses,-1))
 
 # median
 median_coefs = np.median(inversion_coefs, axis=1)
-print(np.shape(median_coefs))
 
 # 95 % confidence interval
 conf_interval_coefs =  np.percentile(inversion_coefs, 97.5, axis=1) - np.percentile(inversion_coefs, 2.5, axis=1)
@@ -54,7 +59,7 @@ conf_interval_coefs =  np.percentile(inversion_coefs, 97.5, axis=1) - np.percent
 node_median_coefs = np.tensordot(median_coefs, impulse_slip_reordered, 1)
 node_conf_interval_coefs = np.tensordot(conf_interval_coefs, impulse_slip_reordered, 1)
 
-# Create the plot 
+# Plot median and confidence interval for each component
 fig, ax = plt.subplots(2, 2, figsize=(12,9))
 plotFaultFunctions.plot_fault_surface(vertices,cells,node_median_coefs[:,1],ax[0,0],'Slip component 1')
 plotFaultFunctions.plot_fault_surface(vertices,cells,node_median_coefs[:,2],ax[0,1],'Slip component 2')
@@ -66,5 +71,27 @@ fig.savefig(figureFilename)
 
 if show_plot:
     plt.show()
+else:
+    plt.close()
+
+# compute the predicted displacements
+median_pred_disp = np.tensordot(median_coefs, greens_fns_reordered, 1)
+
+# plot the gps data
+ptsize1=120
+ptsize2=40
+# scatter plot - observed z values
+zscatter = plt.scatter(observed_coords[:,0], observed_coords[:,1], ptsize1, observed_displacement[:,2], cmap = 'RdBu_r',edgecolors='k')
+# scatter plot - predicted z values
+zscatter = plt.scatter(observed_coords[:,0], observed_coords[:,1], ptsize2, median_pred_disp[:,2], cmap = 'RdBu_r',edgecolors='k')
+# colorbar for scatter plot
+cbar = plt.colorbar(zscatter,extend='both')
+cbar.ax.set_title('Z (m)')
+# quiver plot - observed displacements
+plt.quiver(observed_coords[:,0], observed_coords[:,1], observed_displacement[:,0], observed_displacement[:,1])
+# quiver plot - predicted displacements
+plt.quiver(observed_coords[:,0], observed_coords[:,1], median_pred_disp[:,0], median_pred_disp[:,1],color='r')
+
+plt.show()
 
 # End of file
