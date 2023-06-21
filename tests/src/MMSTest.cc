8821f3b5b9d3927666e5b33c4d170af911c8ccf2
Brad Aagaard <baagaard@usgs.gov>
2023-06-20 22:05:12 -0600

    Fix include directive.

--- tests/src/MMSTest.cc
+++ tests/src/MMSTest.cc
@@ -33,7 +33,7 @@
 #include "pylith/utils/journals.hh" // USES PYLITH_COMPONENT_*
 
 #include "catch2/catch_test_macros.hpp"
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "catch2/matchers/catch_matchers_floating_point.hpp"
 
 // ------------------------------------------------------------------------------------------------
 // Constructor.
