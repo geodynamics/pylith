92e65ad8ac8b7b54b26e0c5762409c22a691e173
Brad Aagaard <baagaard@usgs.gov>
2024-06-04 12:27:47 -0600

    FIX: Update bash script to generate example metadata.

--- docs/user/examples/update_synopsis.sh
+++ docs/user/examples/update_synopsis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-subdirs=("box-2d" "box-3d" "reverse-2d" "strikeslip-2d" "crustal-strikeslip-2d" "crustal-strikeslip-3d" "subduction-2d" "subduction-3d" "magma-2d")
+subdirs=("box-2d" "box-3d" "reverse-2d" "strikeslip-2d" "crustal-strikeslip-2d" "crustal-strikeslip-3d" "subduction-2d" "subduction-3d" "magma-2d" "poroelastic-outerrise-2d")
 
 for d in ${subdirs[*]}; do
     pushd $d && pylith_cfgsearch --display=features --output-format=markdown --path=../../../../examples/$d && popd
