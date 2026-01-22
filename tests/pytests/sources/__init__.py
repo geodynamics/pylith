26203b4cc923341cbe862353610a4a73e1ed047a
Robert Walker <rainbowseismic@gmail.com>
2026-01-22 20:49:34 +0000

    fixes to ensure CI tests work

--- tests/pytests/sources/__init__.py
+++ tests/pytests/sources/__init__.py
@@ -1,4 +1,2 @@
 from .TestSource import TestSource
-from .TestWellboreSource import TestWellboreSource
 from .TestMomentTensorForce import TestMomentTensorForce
-from .TestPointForce import TestPointForce
