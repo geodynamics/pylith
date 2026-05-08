f6e0865b69dc18961e0b01c0848ad96b274b04d0
Brad Aagaard <baagaard@usgs.gov>
2026-05-08 11:55:40 -0600

    DOCS: Update components directory.

--- docs/user/components/update_components.sh
+++ docs/user/components/update_components.sh
@@ -1,4 +1,4 @@
-~/src/cig/pythia/bin/pyre_doc_components.py --package=PyLith --src-path=$PYLITH_DIR/lib/python3.12/site-packages/pylith --output-path=./ --manual-toc=toc.json
+~/src/cig/pythia/bin/pyre_doc_components.py --package=PyLith --src-path=$PYLITH_DIR/lib/python$PYTHON_VERSION/site-packages/pylith --output-path=./ --manual-toc=toc.json
 cat index.md | sed -e "s#apps/index.md#implementations.md\\napps/index.md#g" > tmp.md && mv -f tmp.md index.md
 
 # Must edit PyLithApp.md and ConvertMeshApp.md manually.
